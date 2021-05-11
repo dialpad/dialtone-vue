836a0e3773474549aabf9da50c89e722a48195d1
Nikita <81983630+nikitadialpad@users.noreply.github.com>
2021-05-11 19:12:49 +0300

    resolve comments parameterization

--- components/link/link.test.js
+++ components/link/link.test.js
@@ -1,7 +1,7 @@
 import { assert } from 'chai';
 import { createLocalVue, mount } from '@vue/test-utils';
 import HsLink from './link.vue';
-import { LINK_MODIFIERS } from '../link';
+import { LINK_MODIFIERS, LINK_VALIDATION_CLASSES, LINK_CLASS, DANGER, SUCCESS, WARNING, MUTED } from '../link';
 
 // Constants
 const basePropsData = {
@@ -19,7 +19,7 @@ describe('HsTest Tests', function () {
 
   // Helpers
   const _setWrappers = () => {
-    nativeLink = wrapper.find('.d-link');
+    nativeLink = wrapper.find(`.${LINK_CLASS}`);
   };
 
   const _mountWrapper = () => {
@@ -61,54 +61,54 @@ describe('HsTest Tests', function () {
 
     describe('When type is danger', function () {
       beforeEach(async function () {
-        await wrapper.setProps({ kind: 'danger' });
+        await wrapper.setProps({ kind: DANGER });
         _setWrappers();
       });
 
       it('should render the native anchor', function () { assert.isTrue(nativeLink.exists()); });
-      it('should have danger class', function () { assert.isTrue(nativeLink.classes().includes('d-link--danger')); });
+      it('should have danger class', function () { assert.isTrue(nativeLink.classes().includes(LINK_VALIDATION_CLASSES.danger)); });
       it('should have danger inverted class', async function () {
         await wrapper.setProps({ inverted: true });
-        assert.isTrue(nativeLink.classes().includes('d-link--inverted-danger'));
+        assert.isTrue(nativeLink.classes().includes(LINK_MODIFIERS.invertedDanger));
       });
     });
 
     describe('When type is success', function () {
       beforeEach(async function () {
-        await wrapper.setProps({ kind: 'success', inverted: false });
+        await wrapper.setProps({ kind: SUCCESS, inverted: false });
         _setWrappers();
       });
 
-      it('should have success class', function () { assert.isTrue(nativeLink.classes().includes('d-link--success')); });
+      it('should have success class', function () { assert.isTrue(nativeLink.classes().includes(LINK_VALIDATION_CLASSES.success)); });
       it('shouldn\'t have success inverted class', async function () {
         await wrapper.setProps({ inverted: true });
-        assert.isFalse(nativeLink.classes().includes('d-link--inverted-success'));
+        assert.isFalse(nativeLink.classes().includes(`${LINK_MODIFIERS.inverted}-${SUCCESS}`));
       });
     });
 
     describe('When type is warning', function () {
       beforeEach(async function () {
-        await wrapper.setProps({ kind: 'warning', inverted: false });
+        await wrapper.setProps({ kind: WARNING, inverted: false });
         _setWrappers();
       });
 
-      it('should have warning class', function () { assert.isTrue(nativeLink.classes().includes('d-link--warning')); });
+      it('should have warning class', function () { assert.isTrue(nativeLink.classes().includes(LINK_VALIDATION_CLASSES.warning)); });
       it('shouldn\'t have warning inverted class', async function () {
         await wrapper.setProps({ inverted: true });
-        assert.isFalse(nativeLink.classes().includes('d-link--inverted-warning'));
+        assert.isFalse(nativeLink.classes().includes(`${LINK_MODIFIERS.inverted}-${WARNING}`));
       });
     });
 
     describe('When type is muted', function () {
       beforeEach(async function () {
-        await wrapper.setProps({ kind: 'muted', inverted: false });
+        await wrapper.setProps({ kind: MUTED, inverted: false });
         _setWrappers();
       });
 
-      it('should have muted class', function () { assert.isTrue(nativeLink.classes().includes('d-link--muted')); });
+      it('should have muted class', function () { assert.isTrue(nativeLink.classes().includes(LINK_VALIDATION_CLASSES.muted)); });
       it('shouldn\'t have muted inverted class', async function () {
         await wrapper.setProps({ inverted: true });
-        assert.isFalse(nativeLink.classes().includes('d-link--inverted-warning'));
+        assert.isFalse(nativeLink.classes().includes(`${LINK_MODIFIERS.inverted}-${WARNING}`));
       });
     });
 
@@ -118,10 +118,10 @@ describe('HsTest Tests', function () {
         _setWrappers();
       });
 
-      it('should have disabled class', function () { assert.isTrue(nativeLink.classes().includes('d-link--disabled')); });
+      it('should have disabled class', function () { assert.isTrue(nativeLink.classes().includes(LINK_MODIFIERS.disabled)); });
       it('should have disabled inverted class', async function () {
         await wrapper.setProps({ inverted: true });
-        assert.isTrue(nativeLink.classes().includes('d-link--inverted-disabled'));
+        assert.isTrue(nativeLink.classes().includes(LINK_MODIFIERS.invertedDisabled));
       });
     });
 
@@ -131,10 +131,10 @@ describe('HsTest Tests', function () {
         _setWrappers();
       });
 
-      it('should have inverted class', function () { assert.isTrue(nativeLink.classes().includes('d-link--inverted')); });
+      it('should have inverted class', function () { assert.isTrue(nativeLink.classes().includes(LINK_MODIFIERS.inverted)); });
       it('should have inverted danger class', async function () {
-        await wrapper.setProps({ kind: 'danger' });
-        assert.isTrue(nativeLink.classes().includes('d-link--inverted-danger'));
+        await wrapper.setProps({ kind: DANGER });
+        assert.isTrue(nativeLink.classes().includes(LINK_MODIFIERS.invertedDanger));
       });
     });
 
@@ -142,12 +142,12 @@ describe('HsTest Tests', function () {
       beforeEach(function () {
         _mountWrapper();
       });
-      it('should has correct modifier class', function () {
-        assert.strictEqual(wrapper.vm.linkModifierClassDisabled, '');
+      it('should has correct disabled modifier class', function () {
+        assert.isFalse(nativeLink.classes().includes(LINK_MODIFIERS.disabled));
       });
 
       it('should has correct inverted modifier class', function () {
-        assert.strictEqual(wrapper.vm.linkModifierClassInverted, '');
+        assert.isFalse(nativeLink.classes().includes(LINK_MODIFIERS.inverted));
       });
     });
 
@@ -157,7 +157,7 @@ describe('HsTest Tests', function () {
         _setWrappers();
       });
       it('should has correct modifier class', function () {
-        assert.strictEqual(wrapper.vm.linkModifierClassDisabled, LINK_MODIFIERS.disabled);
+        assert.isTrue(nativeLink.classes().includes(LINK_MODIFIERS.disabled));
       });
     });
 
@@ -167,18 +167,18 @@ describe('HsTest Tests', function () {
         _setWrappers();
       });
       it('should has correct modifier class', function () {
-        assert.strictEqual(wrapper.vm.linkModifierClassDisabled, LINK_MODIFIERS.invertedDisabled);
+        assert.isTrue(nativeLink.classes().includes(LINK_MODIFIERS.invertedDisabled));
       });
     });
 
     it('should has correct inverted modifier class in inverted case', async function () {
       await wrapper.setProps({ disabled: false, inverted: true });
-      assert.strictEqual(wrapper.vm.linkModifierClassInverted, LINK_MODIFIERS.inverted);
+      assert.isTrue(nativeLink.classes().includes(LINK_MODIFIERS.inverted));
     });
 
     it('should has correct inverted modifier class in danger and inverted case', async function () {
-      await wrapper.setProps({ kind: 'danger', inverted: true });
-      assert.strictEqual(wrapper.vm.linkModifierClassInverted, LINK_MODIFIERS.invertedDanger);
+      await wrapper.setProps({ kind: DANGER, inverted: true });
+      assert.isTrue(nativeLink.classes().includes(LINK_MODIFIERS.invertedDanger));
     });
   });
 });
