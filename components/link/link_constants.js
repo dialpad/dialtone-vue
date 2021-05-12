164ec41e41d7de3f1322afe7a7ffedbdde52e9ba
Nikita <81983630+nikitadialpad@users.noreply.github.com>
2021-05-12 19:14:28 +0300

    renaming

--- components/link/link_constants.js
+++ components/link/link_constants.js
@@ -1,3 +1,5 @@
+import { capitalize } from '../utils';
+
 export const DANGER = 'danger';
 export const WARNING = 'warning';
 export const SUCCESS = 'success';
@@ -6,7 +8,6 @@ export const DISABLED = 'disabled';
 export const INVERTED = 'inverted';
 export const LINK_CLASS = 'd-link';
 export const LINK_KINDS = ['', DANGER, WARNING, SUCCESS, MUTED];
-const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
 
 export const LINK_VALIDATION_CLASSES = {
   [DANGER]: `${LINK_CLASS}--${DANGER}`,
