4d3f7110bbff65c9ff7d4616c6ba00a0a29fa849
nikitadialpad <81983630+nikitadialpad@users.noreply.github.com>
2021-05-19 17:05:44 +0300

    resolve comments update export...

--- components/link/link_constants.js
+++ components/link/link_constants.js
@@ -8,7 +8,7 @@ export const DISABLED = 'disabled';
 export const INVERTED = 'inverted';
 export const LINK_VARIANTS = ['', DANGER, WARNING, SUCCESS, MUTED, INVERTED];
 
-export const LINK_VARIANT_CLASSES = {
+export const LINK_VARIANTS_CLASSES = {
   [DANGER]: `${LINK_CLASS}--${DANGER}`,
   [WARNING]: `${LINK_CLASS}--${WARNING}`,
   [SUCCESS]: `${LINK_CLASS}--${SUCCESS}`,
