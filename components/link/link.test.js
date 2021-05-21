b6766ba6a67d06818b52f3d36454a5697c375b85
nikitadialpad <81983630+nikitadialpad@users.noreply.github.com>
2021-05-21 14:08:33 +0300

    resolve comments

--- components/link/link.test.js
+++ components/link/link.test.js
@@ -23,8 +23,8 @@ describe('Dialtone Vue Link tests', function () {
   let nativeLink;
 
   // Environment
-  let propsData = basePropsData;
-  let slots = {};
+  const propsData = basePropsData;
+  const slots = { default: 'Slotted Link' };
 
   // Helpers
   const _setWrappers = () => {
@@ -35,108 +35,52 @@ describe('Dialtone Vue Link tests', function () {
     wrapper = shallowMount(DtLink, {
       propsData,
       slots,
-      localVue: this.localVue,
+      localVue: createLocalVue(),
     });
     _setWrappers();
   };
 
-  // Test Setup
-  before(function () {
-    this.localVue = createLocalVue();
-  });
-
-  beforeEach(function () {
-    propsData = basePropsData;
-    _mountWrapper();
-  });
+  const itBehavesLikeHasCorrectKindClass = kind => {
+    it('should have correct class', async function () {
+      await wrapper.setProps({ kind });
+      assert.isTrue(nativeLink.classes().includes(LINK_VARIANTS_CLASSES[kind]));
+    });
+  };
 
   // Test Teardown
-  afterEach(function () {
-    propsData = basePropsData;
-    slots = {};
-  });
+  afterEach(function () {});
+  after(function () {});
 
   describe('Presentation Tests', function () {
+    // Setup
+    _mountWrapper();
+
     it('should render the component', function () { assert.exists(wrapper, 'wrapper exists'); });
     it('should render the native anchor', function () { assert.isTrue(nativeLink.exists()); });
 
-    function itBehavesLikeHasCorrectKindClass (kind) {
-      it('should have correct class', async function () {
-        assert.isTrue(nativeLink.classes().includes(LINK_VARIANTS_CLASSES[kind]));
-      });
-    }
-
     describe('When the link has default state', function () {
-      beforeEach(function () {
-        _mountWrapper();
-      });
-
       it('should has correct disabled modifier class', function () {
         assert.isFalse(nativeLink.classes().includes(LINK_MODIFIER_CLASSES.disabled));
       });
     });
 
     describe('When a default slot is provided', function () {
-      beforeEach(function () {
-        slots = { default: 'Slotted Link' };
-        _mountWrapper();
-      });
-
-      it('should render the provided data', function () { assert.equal(nativeLink.text(), 'Slotted Link'); });
+      it('should render the provided data', function () { assert.equal(nativeLink.text(), slots.default); });
     });
 
-    describe('When kind is danger', function () {
-      beforeEach(async function () {
-        await wrapper.setProps({ kind: DANGER });
-        _setWrappers();
-      });
+    describe('When kind is danger', function () { itBehavesLikeHasCorrectKindClass(DANGER); });
 
-      itBehavesLikeHasCorrectKindClass(DANGER);
-    });
+    describe('When kind is inverted', function () { itBehavesLikeHasCorrectKindClass(INVERTED); });
 
-    describe('When kind is inverted', function () {
-      beforeEach(async function () {
-        await wrapper.setProps({ kind: INVERTED });
-        _setWrappers();
-      });
+    describe('When kind is success', function () { itBehavesLikeHasCorrectKindClass(SUCCESS); });
 
-      itBehavesLikeHasCorrectKindClass(INVERTED);
-    });
+    describe('When kind is warning', function () { itBehavesLikeHasCorrectKindClass(WARNING); });
 
-    describe('When kind is success', function () {
-      beforeEach(async function () {
-        await wrapper.setProps({ kind: SUCCESS });
-        _setWrappers();
-      });
-
-      itBehavesLikeHasCorrectKindClass(SUCCESS);
-    });
-
-    describe('When kind is warning', function () {
-      beforeEach(async function () {
-        await wrapper.setProps({ kind: WARNING });
-        _setWrappers();
-      });
-
-      itBehavesLikeHasCorrectKindClass(WARNING);
-    });
-
-    describe('When kind is muted', function () {
-      beforeEach(async function () {
-        await wrapper.setProps({ kind: MUTED });
-        _setWrappers();
-      });
-
-      itBehavesLikeHasCorrectKindClass(MUTED);
-    });
+    describe('When kind is muted', function () { itBehavesLikeHasCorrectKindClass(MUTED); });
 
     describe('When anchor is disabled', function () {
-      beforeEach(async function () {
+      it('should have correct class', async function () {
         await wrapper.setProps({ kind: '', disabled: true });
-        _setWrappers();
-      });
-
-      it('should have correct class', function () {
         assert.isTrue(nativeLink.classes().includes(LINK_MODIFIER_CLASSES[DISABLED]));
       });
     });
