1a383aa2b1229bf9333bcead8faf5a6ba2427e8a
José Silva <jose.silva@dialpad.com>
2021-05-27 12:42:35 -0300

    Add Badge component implementation

---
+++ components/badge/badge.vue
@@ -0,0 +1,44 @@
+<template>
+  <span
+    :class="badgeClass"
+    data-qa="dt-badge"
+  >
+    <!-- @slot Allows you to override the "text" prop. -->
+    <slot>
+      {{ text }}
+    </slot>
+  </span>
+</template>
+
+<script>
+import { BADGE_COLOR_MODIFIERS } from './badge_constants.js';
+
+export default {
+  name: 'DtBadge',
+
+  props: {
+    /**
+     * Text for the badge content
+     */
+    text: {
+      type: String,
+      default: '',
+    },
+
+    /**
+     * Color for the badge background
+     */
+    color: {
+      type: String,
+      default: 'default',
+      validator: (color) => Object.keys(BADGE_COLOR_MODIFIERS).includes(color),
+    },
+  },
+
+  computed: {
+    badgeClass () {
+      return ['d-badge', BADGE_COLOR_MODIFIERS[this.color]];
+    },
+  },
+};
+</script>
---
+++ components/badge/index.js
@@ -0,0 +1,1 @@
+export { default as DtBadge } from './badge.vue';
