36962e8ef47b81074570c5624bddfa55f88626ef
José Silva <jose.silva@dialpad.com>
2021-06-01 12:37:16 -0300

    Improve setup in order to execute _setWrappers once

--- components/badge/badge.test.js
+++ components/badge/badge.test.js
@@ -33,9 +33,6 @@ describe('Dialtone Vue Badge tests', function () {
   before(function () {
     this.localVue = createLocalVue();
   });
-  beforeEach(function () {
-    _setWrappers();
-  });
 
   // Teardown
   afterEach(function () {
@@ -56,7 +53,12 @@ describe('Dialtone Vue Badge tests', function () {
       });
     };
 
-    it('should render the component', function () { assert.exists(wrapper, 'wrapper exists'); });
+    describe('When the badge renders', function () {
+      // Test Setup
+      beforeEach(function () { _setWrappers(); });
+
+      it('should exist', function () { assert.exists(wrapper); });
+    });
 
     describe('When a text is provided via slot', function () {
       // Test Environment
@@ -76,39 +78,45 @@ describe('Dialtone Vue Badge tests', function () {
       const propText = 'Prop text';
 
       // Test Setup
-      beforeEach(async function () {
-        await wrapper.setProps({ text: propText });
+      beforeEach(function () {
+        propsData = { ...basePropsData, text: propText };
+        _setWrappers();
       });
 
       itBehavesLikeRendersText(propText);
     });
 
-    describe('When color is black-700', function () { itBehavesLikeHasCorrectColorClass('black-700'); });
+    describe('When a color is provided via prop', function () {
+      // Test Setup
+      beforeEach(function () { _setWrappers(); });
 
-    describe('When color is green-400', function () { itBehavesLikeHasCorrectColorClass('green-400'); });
+      describe('When color is black-700', function () { itBehavesLikeHasCorrectColorClass('black-700'); });
 
-    describe('When color is orange-400', function () { itBehavesLikeHasCorrectColorClass('orange-400'); });
+      describe('When color is green-400', function () { itBehavesLikeHasCorrectColorClass('green-400'); });
 
-    describe('When color is pink-300', function () { itBehavesLikeHasCorrectColorClass('pink-300'); });
+      describe('When color is orange-400', function () { itBehavesLikeHasCorrectColorClass('orange-400'); });
 
-    describe('When color is pink-500', function () { itBehavesLikeHasCorrectColorClass('pink-500'); });
+      describe('When color is pink-300', function () { itBehavesLikeHasCorrectColorClass('pink-300'); });
 
-    describe('When color is pink-600', function () { itBehavesLikeHasCorrectColorClass('pink-600'); });
+      describe('When color is pink-500', function () { itBehavesLikeHasCorrectColorClass('pink-500'); });
 
-    describe('When color is pink-700', function () { itBehavesLikeHasCorrectColorClass('pink-700'); });
+      describe('When color is pink-600', function () { itBehavesLikeHasCorrectColorClass('pink-600'); });
 
-    describe('When color is purple-100', function () { itBehavesLikeHasCorrectColorClass('purple-100'); });
+      describe('When color is pink-700', function () { itBehavesLikeHasCorrectColorClass('pink-700'); });
 
-    describe('When color is purple-300', function () { itBehavesLikeHasCorrectColorClass('purple-300'); });
+      describe('When color is purple-100', function () { itBehavesLikeHasCorrectColorClass('purple-100'); });
 
-    describe('When color is purple-500', function () { itBehavesLikeHasCorrectColorClass('purple-500'); });
+      describe('When color is purple-300', function () { itBehavesLikeHasCorrectColorClass('purple-300'); });
 
-    describe('When color is purple-700', function () { itBehavesLikeHasCorrectColorClass('purple-700'); });
+      describe('When color is purple-500', function () { itBehavesLikeHasCorrectColorClass('purple-500'); });
 
-    describe('When color is red-500', function () { itBehavesLikeHasCorrectColorClass('red-500'); });
+      describe('When color is purple-700', function () { itBehavesLikeHasCorrectColorClass('purple-700'); });
 
-    describe('When color is white', function () { itBehavesLikeHasCorrectColorClass('white'); });
+      describe('When color is red-500', function () { itBehavesLikeHasCorrectColorClass('red-500'); });
 
-    describe('When color is yellow-300', function () { itBehavesLikeHasCorrectColorClass('yellow-300'); });
+      describe('When color is white', function () { itBehavesLikeHasCorrectColorClass('white'); });
+
+      describe('When color is yellow-300', function () { itBehavesLikeHasCorrectColorClass('yellow-300'); });
+    });
   });
 });
