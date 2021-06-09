24106793f2091afbe30328e4c80ba408880d56d4
nikitadialpad <81983630+nikitadialpad@users.noreply.github.com>
2021-06-09 18:39:14 +0300

    updates

--- components/tabs/index.js
+++ components/tabs/index.js
@@ -1,2 +1,9 @@
 export { default as DtTabs } from './tabs.vue';
-export {} from './tabs_constants';
+export { default as DtTab } from './tab.vue';
+export { default as DtTabPanel } from './tab-panel.vue';
+export {
+  TABS_KIND_MODIFIERS,
+  TABS_SIZES,
+  TABS_IMPORTANCE_MODIFIERS,
+  TAB_IMPORTANCE_MODIFIERS,
+} from './tabs_constants';
