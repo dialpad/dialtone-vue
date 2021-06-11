81f7c4cdf882cd3c875585ab61d3a247069c5da5
Nikita <81983630+nikitadialpad@users.noreply.github.com>
2021-06-11 11:12:10 +0300

    Component : Tooltip. Implementation (#49)
    * generate default component
    
    * add basic integration of tooltip
    
    * add exports
    
    * add accessibility on hover and focus
    
    * clean up
    
    * clean up
    
    * update tooltip a11y
    
    * - clean up
    - resolve comments, clean up stories
    - hide tooltip on escape key press
    
    * change default hover to true
    
    * add docs
    
    * add docs
    
    * add comments
    
    * fix and comments
    
    * resolve comments
    
    * remove unnecessary style
    
    * remove spacing, clean up
    
    * resolve comments
    
    * upgrade dialtone version
    
    * remove 'd-ps-relative' duplicate
    
    * update focus logic
    
    * clean up class d-tooltip--show
    update mock css
    
    * update tooltip classes logic
    
    * add generic aria-describedby prop
    
    * voice over fixes
    
    * voice over integration update
    chrome and safari alignment
    
    * update doc
    
    * update doc
    
    * update a11y
    
    * update tooltip doc
    
    * Component: tooltip implementation vo issue cleanup (#71)
    
    * Updated the mounted method to set aria-describedby on the children of the anchor if the anchor's children are focusable.
    * Added role=tooltip to the tooltip
    
    * resolve comments
    
    * Update components/utils.js
    
    Co-authored-by: Ryan Lafferty <73855198+ryandialpad@users.noreply.github.com>
    
    * add comments
    refactor css classes
    
    * refactor css classes
    
    * add shouldHasHoverModifier
    
    * refactoring
    
    * refactoring
    
    * resolve comments
    
    * add absolute class for tooltip
    
    * renaming
    
    Co-authored-by: Ryan Lafferty <73855198+ryandialpad@users.noreply.github.com>

---
+++ components/tooltip/tooltip_default.story.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="d-fl-center d-fd-column d-pt64">
+    <div class="d-pt16">
+      <dt-tooltip
+        :message="message"
+        :arrow-direction="arrowDirection"
+        :show="show"
+        :inverted="inverted"
+        :hover="hover"
+      >
+        <template v-if="defaultSlot">
+          {{ defaultSlot }}
+        </template>
+        <template #anchor>
+          <dt-button importance="outlined">
+            {{ anchorSlot }}
+          </dt-button>
+        </template>
+      </dt-tooltip>
+    </div>
+  </div>
+</template>
+
+<script>
+import { DtTooltip } from './';
+import { DtButton } from '../button';
+
+export default {
+  name: 'DtTooltipDefault',
+  components: { DtTooltip, DtButton },
+};
+</script>
