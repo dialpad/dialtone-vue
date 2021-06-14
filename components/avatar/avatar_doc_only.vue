9fd338d5c49233531a69877315cf3fa0564ba853
José Silva <jose.silva@dialpad.com>
2021-06-14 12:56:35 -0300

    Add documentation for src and alt attributes

---
+++ components/avatar/avatar_doc_only.vue
@@ -0,0 +1,30 @@
+<script>
+import { DtAvatar } from './index';
+
+export default {
+  /*
+  * This component is only for documenting attrs in Storybook docs.
+  */
+  name: 'DtAvatarDocOnly',
+
+  extends: DtAvatar,
+
+  props: {
+    /**
+     * Native image src attribute. Overridden by default slot.
+     */
+    src: {
+      type: String,
+      default: '',
+    },
+
+    /**
+     * Native image alt attribute. Overridden by default slot.
+     */
+    alt: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
