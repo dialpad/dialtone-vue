03367401be5ba23b36ee426ef5ba05d2fc5a6878
nikitadialpad <81983630+nikitadialpad@users.noreply.github.com>
2021-06-16 12:31:08 +0300

    update tests

--- components/tooltip/tooltip.test.js
+++ components/tooltip/tooltip.test.js
@@ -11,31 +11,23 @@ describe('Dialtone Vue Tooltip tests', function () {
   let wrapper;
   let tooltipContainer;
   let tooltip;
+  let anchor;
+  let slots = { anchor: 'Anchor Slot' };
 
   // Helpers
   const _setWrappers = () => {
     tooltipContainer = wrapper.find('[data-qa="dt-tooltip-container"]');
     tooltip = wrapper.find('[data-qa="dt-tooltip"]');
+    anchor = wrapper.find('[data-qa="dt-tooltip-anchor"]');
   };
 
   const _mountWrapper = () => {
     wrapper = shallowMount(DtTooltip, {
       localVue: createLocalVue(),
+      slots,
     });
     _setWrappers();
   };
-
-  const itBehavesLikeHasCorrectDirectionClass = arrowDirection => {
-    it('should have correct class', async function () {
-      await wrapper.setProps({ arrowDirection });
-      assert.isTrue(tooltip.classes().includes(`d-tooltip__arrow--${arrowDirection}`));
-    });
-  };
-
-  const describeDirections = () => TOOLTIP_DIRECTION_MODIFIERS
-    .forEach(arrowDirection =>
-      describe(`When direction is ${arrowDirection}`,
-        function () { itBehavesLikeHasCorrectDirectionClass(arrowDirection); }));
 
   const mouseover = () => wrapper.trigger('mouseover');
   const focus = () => wrapper.trigger('focus.capture');
@@ -50,9 +42,71 @@ describe('Dialtone Vue Tooltip tests', function () {
     it('should render the container', function () { assert.isTrue(tooltipContainer.exists()); });
     it('should render the tooltip', function () { assert.isTrue(tooltip.exists()); });
   });
-  describeDirections();
+  describe('When an arrow direction is provided', function () {
+    TOOLTIP_DIRECTION_MODIFIERS.forEach(arrowDirection => describe(`When direction is ${arrowDirection}`, function () {
+      // Setup
+      beforeEach(async function () {
+        await wrapper.setProps({ arrowDirection });
+      });
 
-  describe('Events', function () {
+      it('should have correct arrow direction class', async function () {
+        assert.isTrue(tooltip.classes().includes(`d-tooltip__arrow--${arrowDirection}`));
+      });
+    }));
+  });
+  describe('Message provided via prop', function () {
+    it('should render the message', async function () {
+      await wrapper.setProps({ message: 'Message Prop' });
+      assert.equal(tooltip.text(), 'Message Prop');
+    });
+  });
+
+  describe('Message provided via slot', function () {
+    before(function () {
+      slots = { ...slots, default: 'Message Slot' };
+      _mountWrapper();
+    });
+    it('should render the message', async function () {
+      await wrapper.setProps({ message: 'Message Slot' });
+      assert.equal(tooltip.text(), 'Message Slot');
+    });
+  });
+
+  describe('Anchor slot', function () {
+    it('should render the anchor slot', async function () {
+      assert.equal(anchor.text(), 'Anchor Slot');
+    });
+  });
+
+  describe('Visibility props', function () {
+    it('should has inverted class', async function () {
+      await wrapper.setProps({ inverted: true });
+      assert.isTrue(tooltip.classes(TOOLTIP_KIND_MODIFIERS.inverted));
+    });
+  });
+
+  describe('Interactivity Tests', function () {
+    describe('Show state', function () {
+      it('should be visible', async function () {
+        await wrapper.setProps({ show: true, hover: false });
+        wrapper.vm.$nextTick(() => {
+          assert.isTrue(wrapper.vm.isTooltipVisible);
+        });
+      });
+      it('should be closed', async function () {
+        await wrapper.setProps({ show: false, hover: false });
+        wrapper.vm.$nextTick(() => {
+          assert.isFalse(wrapper.vm.isTooltipVisible);
+        });
+      });
+      it('should be invisible', async function () {
+        await wrapper.setProps({ show: true, hover: true });
+        wrapper.vm.$nextTick(() => {
+          assert.isFalse(wrapper.vm.isTooltipVisible);
+        });
+      });
+    });
+
     it('mouseover show tooltip', async function () {
       await mouseover();
       assert.isTrue(tooltip.classes().includes(TOOLTIP_KIND_MODIFIERS.show));
