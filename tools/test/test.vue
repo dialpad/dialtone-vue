f66164e1137994d6856222250755933f4514e649
ryandialpad <ryan.lafferty@dialpad.com>
2021-06-30 13:20:42 -0400

    Adding testing files to branch

---
+++ tools/test/test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="d-input-message--warning d-some-other-class-that-wont-be-matched">
+    Some text
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'SomeComponent',
+};
+</script>
+
+<style lang="less" scoped>
+  .some-local-class {
+    .d-some-class1();
+    .d-some-other-class-that-wont-be-matched();
+    .d-some-class2();
+  }
+</style>
