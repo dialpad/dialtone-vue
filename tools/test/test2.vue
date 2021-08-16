a1a7b1219b69516e18780def3e86c2ce8c8e51ad
ryandialpad <ryan.lafferty@dialpad.com>
2021-08-16 10:47:19 -0400

    Adding another test component to debug the migration script

---
+++ tools/test/test2.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="d-btn d-btn d-btn--outlined d-btn d-btn--inverted">
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SomeComponent',
+  data () {
+    return {
+      text: 'Some text',
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+  .some-local-class {
+    .d-btn(); .d-btn(); .d-btn--outlined();
+  }
+</style>
