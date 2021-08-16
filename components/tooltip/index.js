f14b8ae9d2475d54b489b04592257d428367ce72
nikitadialpad <81983630+nikitadialpad@users.noreply.github.com>
2021-08-16 17:47:08 +0300

    add import

--- components/tooltip/index.js
+++ components/tooltip/index.js
@@ -1,3 +1,3 @@
 // TODO for some reason storybook run doesn't fail if the line is commented
-// export { default as DtTooltip } from './tooltip.vue';
+export { default as DtTooltip } from './tooltip.vue';
 export { TOOLTIP_DIRECTION_MODIFIERS, TOOLTIP_KIND_MODIFIERS } from './tooltip_constants';
