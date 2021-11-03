df1cd7c11e3717d36b772ae14ce2d88e76ad07ca
José Silva <jose.silva@dialpad.com>
2021-11-03 15:11:32 -0300

    rollback to use default config for release commit author

--- release-local.config.js
+++ release-local.config.js
@@ -1,9 +1,3 @@
-// Set engbot as the author of the release commit
-process.env.GIT_AUTHOR_NAME = 'engbotdialpad';
-process.env.GIT_AUTHOR_EMAIL = 'engbot@dialpad.com';
-process.env.GIT_COMMITTER_NAME = 'engbotdialpad';
-process.env.GIT_COMMITTER_EMAIL = 'engbot@dialpad.com';
-
 /* eslint-disable no-template-curly-in-string */
 module.exports = {
   branches: [
