5d0b35e8f3da30456bb3a129919b17bd571a830a
Brad Paugh <brad.paugh@dialpad.com>
2022-01-06 15:37:57 -0800

    remove brackets

--- components/skeleton/skeleton-paragraph.vue
+++ components/skeleton/skeleton-paragraph.vue
@@ -26,7 +26,7 @@
 </template>
 
 <script>
-import { DtSkeletonText } from './skeleton-text';
+import DtSkeletonText from './skeleton-text';
 
 const validator = number => number !== '' && !Number.isNaN(Number(number));
 export default {
