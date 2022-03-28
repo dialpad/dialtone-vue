21a5e6ae2e461d1349c812e4d38b8261b5ddb0a0
Brad Paugh <brad.paugh@dialpad.com>
2022-03-28 13:37:46 -0700

    Update components/input/input.vue
    Co-authored-by: josedialpad <jose.silva@dialpad.com>

--- components/input/input.vue
+++ components/input/input.vue
@@ -444,7 +444,7 @@ export default {
     },
 
     calculateLength (value) {
-      if (value === null || typeof value !== 'string') {
+      if (typeof value !== 'string') {
         return 0;
       }
 
