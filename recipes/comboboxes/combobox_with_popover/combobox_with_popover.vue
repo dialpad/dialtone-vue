54ac7931d25984fe6dec8f5a9b72c859522098bd
Brad Paugh <brad.paugh@dialpad.com>
2022-04-19 16:46:19 -0700

    add more props, render example.

--- recipes/comboboxes/combobox_with_popover/combobox_with_popover.vue
+++ recipes/comboboxes/combobox_with_popover/combobox_with_popover.vue
@@ -1,14 +1,24 @@
 <template>
   <dt-combobox
     :show-list="showList"
+    :list-id="listId"
+    :on-beginning-of-list="onBeginningOfList"
+    :on-end-of-list="onEndOfList"
+    :list-aria-label="listAriaLabel"
+    @focusin="defaultToggleShow"
   >
-    <template #input>
+    <template
+      #input
+    >
       <slot name="input" />
     </template>
     <template #list="{ opened }">
       <dt-popover
         :open="true"
         :hide-on-click="false"
+        :max-height="maxHeight"
+        :max-width="maxWidth"
+        placement="bottom-start"
         padding="small"
         :modal="false"
         :auto-focus="false"
@@ -17,11 +27,19 @@
         <template #anchor>
           <div class="d-h0 d-vi-hidden" />
         </template>
+
+        <template #headerContent>
+          <slot name="header" />
+        </template>
+
+        <template #content>
+          <ul :class="['d-ps-relative', 'd-px0', DROPDOWN_PADDING_CLASSES[padding], listClass]">
+            <slot name="list" />
+          </ul>
+        </template>
+
         <template #footerContent>
           <slot name="footer" />
-        </template>
-        <template #content>
-          <slot name="content" />
         </template>
       </dt-popover>
     </template>
@@ -30,6 +48,10 @@
 
 <script>
 import { DtCombobox, DtPopover } from '@';
+import { getUniqueString } from '@/common/utils';
+import {
+  DROPDOWN_PADDING_CLASSES,
+} from '@/components/dropdown/dropdown_constants';
 import {} from './combobox_with_popover_constants.js';
 
 export default {
@@ -40,8 +62,6 @@ export default {
     DtPopover,
   },
 
-  mixins: [],
-
   /* inheritAttrs: false is generally an option we want to set on library
     components. This allows any attributes passed in that are not recognized
     as props to be passed down to another element or component using v-bind:$attrs
@@ -50,16 +70,88 @@ export default {
 
   props: {
     /**
+     * String to use for the list's aria-label.
+     */
+    listAriaLabel: {
+      type: String,
+      required: true,
+    },
+
+    /**
      * Determines when to show the list element and also controls the aria-expanded attribute.
      */
     showList: {
       type: Boolean,
       default: false,
     },
+
+    /**
+     * Sets an ID on the list element of the component. Used by several aria attributes
+     * as well as when deriving the IDs for each item.
+     */
+    listId: {
+      type: String,
+      default () { return getUniqueString(); },
+    },
+
+    /**
+     * Additional class for the wrapper list element.
+     */
+    listClass: {
+      type: [String, Array, Object],
+      default: '',
+    },
+
+    /**
+     * A method that will be called when the selection goes past the beginning of the list.
+     */
+    onBeginningOfList: {
+      type: Function,
+      default: null,
+    },
+
+    /**
+     * A method that will be called when the selection goes past the end of the list.
+     */
+    onEndOfList: {
+      type: Function,
+      default: null,
+    },
+
+    /**
+     * Determines maximum height for the popover before overflow.
+     * Possible units rem|px|em
+     */
+    maxHeight: {
+      type: String,
+      default: '',
+    },
+
+    /**
+     * Determines maximum width for the popover before overflow.
+     * Possible units rem|px|%|em
+     */
+    maxWidth: {
+      type: String,
+      default: '',
+    },
+
+    /**
+     * Vertical padding size around the list element.
+     */
+    padding: {
+      type: String,
+      default: 'small',
+      validator: (padding) => {
+        return Object.keys(DROPDOWN_PADDING_CLASSES).some((item) => item === padding);
+      },
+    },
   },
 
   data () {
-    return {};
+    return {
+      DROPDOWN_PADDING_CLASSES,
+    };
   },
 
   computed: {},
--- recipes/comboboxes/combobox_with_popover/combobox_with_popover_default.story.vue
+++ recipes/comboboxes/combobox_with_popover/combobox_with_popover_default.story.vue
@@ -1,35 +1,60 @@
 <template>
   <dt-recipe-combobox-with-popover
-    :show-list="showList"
+    list-aria-label="Example list items"
+    max-height="300px"
   >
     <template #input>
       <dt-input
         v-model="value"
+        placeholder="Start typing to show list"
       />
     </template>
-    <template #content>
-      <span
-        v-if="content"
-        v-html="content"
-      />
-      <template v-else>
-        <p>
-          I will be displayed in the popover!
-        </p>
-      </template>
+    <template #list>
+      <dt-list-item
+        v-for="item in items"
+        :key="item.id"
+        role="option"
+        navigation-type="arrow-keys"
+      >
+        {{ item.number }}
+        <template #right>
+          {{ item.type }}
+        </template>
+      </dt-list-item>
     </template>
   </dt-recipe-combobox-with-popover>
 </template>
 
 <script>
 import DtRecipeComboboxWithPopover from './combobox_with_popover';
-import { DtInput } from '@';
+import { DtInput, DtListItem } from '@';
 
 export default {
   name: 'DtRecipeComboboxWithPopoverDefault',
   components: {
     DtRecipeComboboxWithPopover,
     DtInput,
+    DtListItem,
+  },
+
+  data () {
+    return {
+      value: '',
+      items: [
+        { id: 'item1', number: '(732) 338-2720', type: 'MAINLINE' },
+        { id: 'item2', number: '(732) 338-2722', type: 'MAINLINE' },
+        { id: 'item3', number: '(732) 338-2725', type: 'MAINLINE' },
+        { id: 'item4', number: '(732) 338-2764', type: 'MAINLINE' },
+        { id: 'item5', number: '(732) 338-2784', type: 'MAINLINE' },
+        { id: 'item6', number: '(732) 338-2743', type: 'MAINLINE' },
+        { id: 'item7', number: '(732) 338-2728', type: 'MAINLINE' },
+        { id: 'item8', number: '(732) 338-2769', type: 'Other' },
+        { id: 'item9', number: '(732) 338-2723', type: 'MAINLINE' },
+        { id: 'item10', number: '(732) 338-2729', type: 'MAINLINE' },
+        { id: 'item11', number: '(732) 338-2489', type: 'MAINLINE' },
+        { id: 'item12', number: '(732) 338-2756', type: 'Other' },
+      ],
+    };
   },
 };
 </script>
