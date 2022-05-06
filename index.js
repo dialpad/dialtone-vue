7c9a4b030256b5c59cea66ec1f0900c88886481c
Brad Paugh <brad.paugh@dialpad.com>
2022-05-06 12:48:48 -0700

    fix casing

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ export * from './recipes/notices/top_banner_info';
 export * from './common/mixins';
 
 // Emoji
-export { default as EmojiJson } from './node_modules/emoji-toolkit/emoji.json';
+export { default as emojiJson } from './node_modules/emoji-toolkit/emoji.json';
 export * from './common/emoji';
 
 // Constants
