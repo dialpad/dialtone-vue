0aae39f21e7a9077216b050a85a9d376ced2f0cf
Brad Paugh <brad.paugh@dialpad.com>
2022-05-06 12:48:48 -0700

    fix casing

--- common/emoji.js
+++ common/emoji.js
@@ -1,9 +1,9 @@
-import EmojiToolkit from 'emoji-toolkit';
+import emojiToolkit from 'emoji-toolkit';
 
 export function setEmojiAssetUrl (url, fileExtension = '.svg') {
   if (!url.endsWith('/')) {
     url = url + '/';
   }
-  EmojiToolkit.imagePathPNG = url;
-  EmojiToolkit.fileExtension = fileExtension;
+  emojiToolkit.imagePathPNG = url;
+  emojiToolkit.fileExtension = fileExtension;
 }
