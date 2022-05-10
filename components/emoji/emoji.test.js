883820508871ed6b527780629b11a59f22fb92f9
Brad Paugh <brad.paugh@dialpad.com>
2022-05-10 09:33:24 -0700

    emoji component stub

---
+++ components/emoji/emoji.test.js
@@ -0,0 +1,94 @@
+// import { assert } from 'chai';
+// import { createLocalVue, shallowMount } from '@vue/test-utils';
+// import DtEmoji from './emoji.vue';
+
+// // Constants
+// const basePropsData = {};
+
+// describe('DtEmoji Tests', function () {
+//   // Wrappers
+//   let wrapper;
+//   let childContainer;
+
+//   // Environment
+//   let propsData = basePropsData;
+//   let attrs = {};
+//   let slots = {};
+//   let provide = {};
+
+//   // Helpers
+//   const _setChildWrappers = () => {
+//     childContainer = wrapper.find('[data-qa="child-container"]');
+//   };
+
+//   const _setWrappers = () => {
+//     wrapper = shallowMount(DtEmoji, {
+//       propsData,
+//       attrs,
+//       slots,
+//       provide,
+//       localVue: this.localVue,
+//     });
+//     _setChildWrappers();
+//   };
+
+//   // Setup
+//   before(function () {
+//     this.localVue = createLocalVue();
+//   });
+//   beforeEach(function () {});
+
+//   // Teardown
+//   afterEach(function () {
+//     propsData = basePropsData;
+//     attrs = {};
+//     slots = {};
+//     provide = {};
+//   });
+//   after(function () {});
+
+//   describe('Presentation Tests', function () {
+//     /*
+//      * Test(s) to ensure that the component is correctly rendering
+//      */
+
+//     describe('When a shortcode string is passed in', function () {
+//       it('renders the correct emoji', function () {
+
+//       });
+//       describe('When a prop changes to a new code', function () {
+//         it('should display the correct emoji with the new code', function () {
+
+//         });
+//       });
+//     });
+//     describe('When a unicode string is passed in', function () {
+//       it('renders the correct emoji', function () {
+
+//       });
+
+//       describe('When a prop changes to a new code', function () {
+//         it('should display the correct emoji with the new code', function () {
+
+//         });
+//       });
+//     });
+//     describe('When a unicode string and a shortcode string is passed in', function () {
+//       it('renders the emoji based on the unicode', function () {
+
+//       });
+//     });
+//   });
+
+//   describe('Accessibility Tests', function () {
+//     /*
+//      * Test(s) to ensure that the component is accessible
+//      */
+
+//     describe('When an emoji is rendered', function () {
+//       it('should have aria-label describing the emoji', function () {
+
+//       });
+//     });
+//   });
+// });
---
+++ components/emoji/emoji.vue
@@ -0,0 +1,60 @@
+<template>
+  <span v-html="emojiHtml" />
+</template>
+
+<script>
+export default {
+  name: 'DtEmoji',
+
+  props: {
+    shortcode: {
+      type: String,
+      default: null,
+    },
+
+    unicode: {
+      type: String,
+      default: null,
+    },
+  },
+
+  data () {
+    return {
+      emojiHtml: '',
+    };
+  },
+
+  watch: {
+    shortcode: {
+      handler: function () {
+        // run getEmojiHtml
+      },
+
+      immediate: true,
+    },
+
+    unicode: {
+      handler: function () {
+        // run getEmojiHtml
+      },
+
+      immediate: true,
+    },
+  },
+
+  methods: {
+    getCodeFromProps () {
+      // Check whether shortcode or unicode was passed in, if both, use unicode.
+      // return string
+    },
+
+    getEmojiHtml () {
+      // call getCodeFromProps
+      // run shortcode convert function if shortcode
+      // run unicode convert function if unicode
+      // if success: update emojiHtml data prop with resulting html
+      // if failure (not found): display an error icon.
+    },
+  },
+};
+</script>
