d995d5998d75a5c91cce7cb352c15128a8a5058e
Brad Paugh <brad.paugh@dialpad.com>
2022-05-13 13:19:20 -0700

    emoji component and tests

--- components/emoji/emoji.test.js
+++ components/emoji/emoji.test.js
@@ -1,94 +1,147 @@
-// import { assert } from 'chai';
-// import { createLocalVue, shallowMount } from '@vue/test-utils';
-// import DtEmoji from './emoji.vue';
+import { assert } from 'chai';
+import { createLocalVue, mount } from '@vue/test-utils';
+import DtEmoji from './emoji.vue';
+import emojiToolkit from 'emoji-toolkit';
 
-// // Constants
-// const basePropsData = {};
+emojiToolkit.imagePathPNG = 'https://mockstorage.com/emojis/';
+emojiToolkit.fileExtension = '.svg';
 
-// describe('DtEmoji Tests', function () {
-//   // Wrappers
-//   let wrapper;
-//   let childContainer;
+global.DOMParser = window.DOMParser;
 
-//   // Environment
-//   let propsData = basePropsData;
-//   let attrs = {};
-//   let slots = {};
-//   let provide = {};
+// Constants
+const basePropsData = {};
 
-//   // Helpers
-//   const _setChildWrappers = () => {
-//     childContainer = wrapper.find('[data-qa="child-container"]');
-//   };
+describe('DtEmoji Tests', function () {
+  // Wrappers
+  let wrapper;
+  let emoji;
 
-//   const _setWrappers = () => {
-//     wrapper = shallowMount(DtEmoji, {
-//       propsData,
-//       attrs,
-//       slots,
-//       provide,
-//       localVue: this.localVue,
-//     });
-//     _setChildWrappers();
-//   };
+  // Environment
+  let propsData = basePropsData;
+  let attrs = {};
+  let slots = {};
+  let provide = {};
 
-//   // Setup
-//   before(function () {
-//     this.localVue = createLocalVue();
-//   });
-//   beforeEach(function () {});
+  // Expected
+  const expectedSmileSrc = 'https://mockstorage.com/emojis/1f604.svg';
+  const expectedLaughingSrc = 'https://mockstorage.com/emojis/1f606.svg';
+  const expectedPointUpLight = 'https://mockstorage.com/emojis/261d-1f3fb.svg';
 
-//   // Teardown
-//   afterEach(function () {
-//     propsData = basePropsData;
-//     attrs = {};
-//     slots = {};
-//     provide = {};
-//   });
-//   after(function () {});
+  // Helpers
+  const _setChildWrappers = () => {
+    emoji = wrapper.find('img');
+  };
 
-//   describe('Presentation Tests', function () {
-//     /*
-//      * Test(s) to ensure that the component is correctly rendering
-//      */
+  const _setWrappers = () => {
+    wrapper = mount(DtEmoji, {
+      propsData,
+      attrs,
+      slots,
+      provide,
+      localVue: this.localVue,
+    });
+    _setChildWrappers();
+  };
 
-//     describe('When a shortcode string is passed in', function () {
-//       it('renders the correct emoji', function () {
+  // Setup
+  before(function () {
+    this.localVue = createLocalVue();
+  });
+  beforeEach(function () {});
 
-//       });
-//       describe('When a prop changes to a new code', function () {
-//         it('should display the correct emoji with the new code', function () {
+  // Teardown
+  afterEach(function () {
+    propsData = basePropsData;
+    attrs = {};
+    slots = {};
+    provide = {};
+  });
+  after(function () {});
 
-//         });
-//       });
-//     });
-//     describe('When a unicode string is passed in', function () {
-//       it('renders the correct emoji', function () {
+  describe('Presentation Tests', function () {
+    /*
+     * Test(s) to ensure that the component is correctly rendering
+     */
 
-//       });
+    describe('When a code string is passed in', function () {
+      beforeEach(function () {
+        propsData = {
+          code: ':smile:',
+        };
+        _setWrappers();
+      });
 
-//       describe('When a prop changes to a new code', function () {
-//         it('should display the correct emoji with the new code', function () {
+      it('renders the correct emoji', function () {
+        assert.strictEqual(emoji.attributes('src'), expectedSmileSrc);
+      });
+      describe('When a prop changes to a new code', function () {
+        beforeEach(async function () {
+          await wrapper.setProps({ code: ':laughing:' });
+        });
+        it('should display the correct emoji with the new code', function () {
+          assert.strictEqual(emoji.attributes('src'), expectedLaughingSrc);
+        });
+      });
 
-//         });
-//       });
-//     });
-//     describe('When a unicode string and a shortcode string is passed in', function () {
-//       it('renders the emoji based on the unicode', function () {
+      describe('When a prop changes to an invalid code', function () {
+        beforeEach(async function () {
+          await wrapper.setProps({ code: ':invalidcode:' });
+        });
+        it('should display a "not found" image', function () {
+          assert.strictEqual(emoji.attributes('title'), 'Invalid Emoji');
+        });
+      });
 
-//       });
-//     });
-//   });
+      describe('When the size changes', function () {
+        beforeEach(async function () {
+          await wrapper.setProps({ size: 'd-svg--size48' });
+        });
+        it('the correct class is set on the element', function () {
+          assert.isTrue(emoji.classes('d-svg--size48'));
+        });
+      });
+    });
 
-//   describe('Accessibility Tests', function () {
-//     /*
-//      * Test(s) to ensure that the component is accessible
-//      */
+    describe('When a skin tone emoji is passed in', function () {
+      beforeEach(function () {
+        propsData = {
+          code: ':point_up_tone1:',
+        };
+        _setWrappers();
+      });
+      it('renders the correct emoji', function () {
+        assert.strictEqual(emoji.attributes('src'), expectedPointUpLight);
+      });
+    });
 
-//     describe('When an emoji is rendered', function () {
-//       it('should have aria-label describing the emoji', function () {
+    describe('When an emoji unicode is passed in', function () {
+      beforeEach(function () {
+        propsData = {
+          code: '☝🏻',
+        };
+        _setWrappers();
+      });
+      it('renders the correct emoji', function () {
+        assert.strictEqual(emoji.attributes('src'), expectedPointUpLight);
+      });
+    });
+  });
 
-//       });
-//     });
-//   });
-// });
+  describe('Accessibility Tests', function () {
+    /*
+     * Test(s) to ensure that the component is accessible
+     */
+
+    describe('When an emoji is rendered', function () {
+      beforeEach(function () {
+        propsData = {
+          code: ':smile:',
+        };
+        _setWrappers();
+      });
+      it('should have aria-label describing the emoji', function () {
+        assert.strictEqual(emoji.attributes('aria-label'), 'grinning face with smiling eyes');
+      });
+    });
+  });
+});
--- components/emoji/emoji.vue
+++ components/emoji/emoji.vue
@@ -1,28 +1,66 @@
 <template>
-  <span v-html="emojiHtml" />
+  <img
+    :class="[emojiClass, size, emojiClassInternal]"
+    :aria-label="emojiAriaLabel"
+    :alt="emojiAlt"
+    :title="emojiTitle"
+    :src="emojiSrc"
+  >
 </template>
 
 <script>
+import { EMOJI_SIZES } from './emoji_constants.js';
+import emojiToolkit from 'emoji-toolkit';
+import emojiJson from '@/node_modules/emoji-toolkit/emoji.json';
+
 export default {
   name: 'DtEmoji',
 
   props: {
+    /**
+     * Supports shortcode ex: :smile: or unicode ex: 😄. Will display the resulting emoji.
+     * See https://emojipedia.org/joypixels/ for all supported shortcode/unicode.
+     */
     code: {
       type: String,
       default: null,
+    },
+
+    /**
+     * The size of the emoji. Can be any of the svg size utility
+     * classes from https://dialpad.design/utilities/svg/size
+     */
+    size: {
+      type: String,
+      default: EMOJI_SIZES.SIZE_20,
+      validator: (t) => Object.values(EMOJI_SIZES).includes(t),
+    },
+
+    /**
+     * Additional class for the emoji img.
+     * Can accept all of String, Object, and Array, i.e. has the
+     * same api as Vue's built-in handling of the class attribute.
+     */
+    emojiClass: {
+      type: [String, Object, Array],
+      default: '',
     },
   },
 
   data () {
     return {
-      emojiHtml: '',
+      emojiClassInternal: undefined,
+      emojiAlt: undefined,
+      emojiTitle: undefined,
+      emojiAriaLabel: undefined,
+      emojiSrc: undefined,
     };
   },
 
   watch: {
     code: {
       handler: function () {
-        // run getEmojiHtml
+        this.getEmojiHtml();
       },
 
       immediate: true,
@@ -30,17 +68,47 @@ export default {
   },
 
   methods: {
-    getCodeFromProps () {
-      // Check whether shortcode or unicode was passed in, if both, use unicode.
-      // return string
+    getEmojiHtml () {
+      const emojiHtml = emojiToolkit.toImage(this.code);
+      const imgElement = this.getImgFromEmojiHtml(emojiHtml);
+      if (!imgElement) { this.setInvalid(); return; }
+      this.emojiClassInternal = imgElement.getAttribute('class');
+      this.emojiAlt = imgElement.getAttribute('alt');
+      this.emojiTitle = imgElement.getAttribute('title');
+      const jsonKey = this.convertUnicodeToJsonKey(this.emojiAlt);
+      this.emojiAriaLabel = emojiJson[jsonKey].name;
+      this.emojiSrc = imgElement.getAttribute('src');
     },
 
-    getEmojiHtml () {
-      // call getCodeFromProps
-      // run shortcode convert function if shortcode
-      // run unicode convert function if unicode
-      // if success: update emojiHtml data prop with resulting html
-      // if failure (not found): display an error icon.
+    getImgFromEmojiHtml (emojiHtml) {
+      const parser = new DOMParser();
+      const htmlDoc = parser.parseFromString(emojiHtml, 'text/html');
+      const imgElement = htmlDoc.getElementsByTagName('img')[0];
+      return imgElement;
+    },
+
+    setInvalid () {
+      this.emojiClassInternal = undefined;
+      this.emojiAlt = undefined;
+      this.emojiTitle = 'Invalid Emoji';
+      this.emojiAriaLabel = 'Invalid Emoji';
+      this.emojiSrc = undefined;
+    },
+
+    // Takes in an emoji unicode character(s) and converts it to an emoji string in the format
+    // emoji.json expects as a key. There can be two unicode characters in an emoji if it contains a skin tone.
+    // The second unicode character denotes skin tone and is separated with a -
+    //
+    // Example:
+    // return value for smile emoji (no skin tone): 1f600
+    // return value for left facing fist (light skin tone): 1f91b-1f3fb
+    convertUnicodeToJsonKey (emoji) {
+      let key = '';
+      for (const codePoint of emoji) {
+        if (key !== '') { key = key + '-'; }
+        key = key + codePoint.codePointAt(0).toString(16);
+      }
+      return key;
     },
   },
 };
