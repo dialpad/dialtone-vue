b75e73142c9ffa82743905f2e4be75a98b54e915
Brad Paugh <brad.paugh@dialpad.com>
2022-05-18 10:18:13 -0700

    Update components/emoji/emoji.stories.js
    Co-authored-by: Jose Silva <jose.silva@dialpad.com>

--- components/emoji/emoji.stories.js
+++ components/emoji/emoji.stories.js
@@ -10,24 +10,6 @@ export const argsData = {
   code: ':smile:',
 };
 
-/**
- * example prop description decorator
- */
-
-/*
-  Controls
-  ========
-
-  Here we define any custom controls or control overrides for our components.
-
-  By default storybook will attempt to provide an appropriate control of the same name for each property in the
-  component as well as include any description provided using a prop decorator within your component (see above).
-
-  Storybook will also attempt to provide an appropriate control for each slot in the component as well as include any
-  description provided using a slot decorator within your component (see below).
-
-  <!-- @slot example slot decorator -->
-*/
 export const argTypesData = {
   size: {
     control: {
