8a5cb19d45493875f40c3c79241f12586eeb5a53
Jonathan Paugh <jonathan.paugh@dialpad.com>
2022-06-22 16:58:17 -0700

    feat(root-layout): migrate to vue 3

---
+++ components/root_layout/root_layout_default.story.vue
@@ -0,0 +1,40 @@
+<template>
+  <dt-root-layout
+    :sidebar-position="sidebarPosition"
+    :sidebar-width="sidebarWidth"
+    :header-height="headerHeight"
+    :footer-height="footerHeight"
+    :content-wrap-width-percent="contentWrapWidthPercent"
+  >
+    <template
+      v-if="header"
+      #header
+    >
+      <span v-html="header" />
+    </template>
+    <template
+      v-if="sidebar"
+      #sidebar
+    >
+      <span v-html="sidebar" />
+    </template>
+    <template v-if="defaultSlot">
+      <span v-html="defaultSlot" />
+    </template>
+    <template
+      v-if="footer"
+      #footer
+    >
+      <span v-html="footer" />
+    </template>
+  </dt-root-layout>
+</template>
+
+<script>
+import DtRootLayout from './root_layout';
+
+export default {
+  name: 'DtRootLayoutDefault',
+  components: { DtRootLayout },
+};
+</script>
