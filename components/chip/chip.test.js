837f46b7ecda10e2c93e7eb2df6689427086d6ab
Brad Paugh <brad.paugh@dialpad.com>
2022-06-24 13:32:16 -0700

    remove unncessary awaits

--- components/chip/chip.test.js
+++ components/chip/chip.test.js
@@ -25,22 +25,22 @@ describe('DtChip Tests', function () {
   let listeners;
 
   // Helpers
-  const _setChildWrappers = async () => {
-    chip = await wrapper.find('[data-qa="dt-chip"]');
-    icon = await wrapper.find('[data-qa="dt-chip-icon"]');
-    avatar = await wrapper.find('[data-qa="dt-chip-avatar"]');
-    label = await wrapper.find('[data-qa="dt-chip-label"]');
-    remove = await wrapper.find('[data-qa="dt-chip-close"]');
+  const _setChildWrappers = () => {
+    chip = wrapper.find('[data-qa="dt-chip"]');
+    icon = wrapper.find('[data-qa="dt-chip-icon"]');
+    avatar = wrapper.find('[data-qa="dt-chip-avatar"]');
+    label = wrapper.find('[data-qa="dt-chip-label"]');
+    remove = wrapper.find('[data-qa="dt-chip-close"]');
   };
 
-  const _setWrappers = async () => {
+  const _setWrappers = () => {
     wrapper = mount(DtChip, {
       propsData,
       slots,
       listeners,
       localVue: this.localVue,
     });
-    await _setChildWrappers();
+    _setChildWrappers();
   };
 
   // Setup
