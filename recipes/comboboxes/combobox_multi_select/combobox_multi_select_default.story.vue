064de746d785ca92915edf30f42acf85d7fb66bf
Brad Paugh <brad.paugh@dialpad.com>
2022-07-07 14:09:29 -0700

    prevent default so arrow key list items don't get focused on click

--- recipes/comboboxes/combobox_multi_select/combobox_multi_select_default.story.vue
+++ recipes/comboboxes/combobox_multi_select/combobox_multi_select_default.story.vue
@@ -79,8 +79,6 @@ export default {
     onComboboxSelect (i) {
       if (this.items[i]) {
         this.onSelect(i);
-        // Focus to input element
-        this.$refs.comboboxMultiSelect.$refs.input?.$refs.input?.focus();
 
         const item = this.items[i].value;
         if (this.selectedItems.includes(item)) {
