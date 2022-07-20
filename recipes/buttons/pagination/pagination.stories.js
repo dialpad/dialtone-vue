76d71cdb86790d6ac4609bfcb1954a91293d44b5
shefaligupta <shefali.gupta@dialpad.com>
2022-07-20 17:47:56 -0600

    feat: dt-pagination structure

---
+++ recipes/buttons/pagination/pagination.stories.js
@@ -0,0 +1,112 @@
+import { action } from '@storybook/addon-actions';
+import { createTemplateFromVueFile } from '@/common/storybook_utils';
+import DtRecipePagination from './pagination';
+import DtRecipePaginationMdx from './pagination.mdx';
+import DtRecipePaginationDefaultTemplate from './pagination_default.story.vue';
+import DtRecipePaginationVariantsTemplate from './pagination_variants.story.vue';
+
+// Default Prop Values
+export const argsData = {
+  some: 'prop',
+  onEvent: action('event'),
+};
+
+/**
+ * example prop description decorator
+ */
+
+/*
+  Controls
+  ========
+
+  Here we define any custom controls or control overrides for our components.
+
+  By default storybook will attempt to provide an appropriate control of the same name for each property in the
+  component as well as include any description provided using a prop decorator within your component (see above).
+
+  Storybook will also attempt to provide an appropriate control for each slot in the component as well as include any
+  description provided using a slot decorator within your component (see below).
+
+  <!-- @slot example slot decorator -->
+*/
+export const argTypesData = {
+  // Props
+  some: {
+    description: 'Describes the some prop',
+    table: {
+      category: 'props',
+      type: {
+        summary: 'string',
+      },
+    },
+    control: {
+      type: 'text',
+    },
+  },
+
+  // Slots
+  default: {
+    control: 'text',
+    table: {
+      type: {
+        summary: 'text/html',
+      },
+    },
+  },
+  /*
+    We use the following naming scheme `<SLOT_NAME>Slot` for slot controls to prevent conflicts with props that share
+    the same name. We provide the correct name of the slot using the name control attribute to ensure that the argument
+    table and description within the controls accurately reflects the correct names of our component's props and slots.
+  */
+  someSlot: {
+    name: 'some',
+    description: 'Slot for some',
+    control: 'text',
+    table: {
+      category: 'slots',
+      type: {
+        summary: 'text/html',
+      },
+    },
+  },
+
+  // Action Event Handlers
+  onEvent: {
+    table: {
+      disable: true,
+    },
+  },
+};
+
+// Story Collection
+export default {
+  title: 'Recipes/Buttons/Pagination',
+  component: DtRecipePagination,
+  args: argsData,
+  argTypes: argTypesData,
+  excludeStories: /.*Data$/,
+  parameters: {
+    docs: {
+      page: DtRecipePaginationMdx,
+    },
+  },
+};
+
+// Templates
+const DefaultTemplate = (args, { argTypes }) => createTemplateFromVueFile(
+  args,
+  argTypes,
+  DtRecipePaginationDefaultTemplate,
+);
+const VariantsTemplate = (args, { argTypes }) => createTemplateFromVueFile(
+  args,
+  argTypes,
+  DtRecipePaginationVariantsTemplate,
+);
+
+// Stories
+export const Default = DefaultTemplate.bind({});
+Default.args = {};
+
+export const Variants = VariantsTemplate.bind({});
+Variants.args = {};
---
+++ recipes/buttons/pagination/pagination.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="d-d-flex d-fd-row d-gg2">
+    <dt-button
+      class="d-h32 d-w32"
+      :importance="activePage > 1 ? 'clear' : 'primary'"
+      :disabled="activePage === 1"
+    >
+      <template slot="icon">
+        <icon-chevron-left />
+      </template>
+    </dt-button>
+    <dt-button
+      v-for="(item, index) in pages"
+      id="list"
+      :key="index"
+      :importance="activePage === index + 1 ? 'primary' : 'clear'"
+      class="d-h32 d-w32"
+      label-class="d-fs14"
+    >
+      {{ item }}
+    </dt-button>
+    <dt-button
+      class="d-h32 d-w32"
+      :disabled="activePage === totalPages"
+      :importance="activePage < totalPages ? 'clear' : 'primary'"
+    >
+      <template slot="icon">
+        <icon-chevron-right />
+      </template>
+    </dt-button>
+  </div>
+</template>
+
+<script>
+import { DtButton } from '@';
+import IconChevronLeft from '@dialpad/dialtone/lib/dist/vue/icons/IconChevronLeft';
+import IconChevronRight from '@dialpad/dialtone/lib/dist/vue/icons/IconChevronRight';
+
+export default {
+  name: 'DtRecipePagination',
+
+  components: {
+    DtButton,
+    IconChevronLeft,
+    IconChevronRight,
+  },
+
+  mixins: [],
+
+  /* inheritAttrs: false is generally an option we want to set on library
+    components. This allows any attributes passed in that are not recognized
+    as props to be passed down to another element or component using v-bind:$attrs
+    more info: https://vuejs.org/v2/api/#inheritAttrs */
+  // inheritAttrs: false,
+
+  props: {
+    /**
+     * Number of pages to be shown
+     */
+    totalPages: {
+      type: Number,
+      default: 5,
+    },
+
+    /**
+     * Selected/Active page
+     */
+    activePage: {
+      type: Number,
+      default: 1,
+    },
+  },
+
+  data () {
+    return {};
+  },
+
+  computed: {
+    pages () {
+      return Array(this.totalPages).fill().map((_, i) => i + 1);
+    },
+  },
+
+  watch: {},
+
+  methods: {},
+};
+</script>
+
+<style lang="less">
+
+</style>
