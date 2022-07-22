363d5690eae10f515a91cb6314c4aa0e68b98cd4
Matias Gea <matias.gea@dialpad.com>
2022-07-22 14:02:08 -0300

    Added callbar-button-with-popover component. Cleaned up some of the callbar-button.

--- recipes/buttons/callbar_button/callbar_button_variants.story.vue
+++ recipes/buttons/callbar_button/callbar_button_variants.story.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable max-lines -->
 <!-- Use this template story to allow the user control the component's props and slots -->
 <template>
   <div class="d-stack16">
@@ -5,104 +6,102 @@
       Call Bar Buttons
     </h3>
     <div class="d-d-flex d-flow16 d-fw-wrap">
-      <dt-recipe-call-bar-button>
-        <template #label>
-          Button
-        </template>
+      <dt-recipe-callbar-button>
+        Button
         <template #icon>
           <icon-recording />
         </template>
         <template #tooltip>
           Tooltip
         </template>
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
 
-      <dt-recipe-call-bar-button>
+      <dt-recipe-callbar-button>
+        No tooltip
         <template #icon>
           <icon-dialpad-glyph />
         </template>
-        No tooltip
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
 
-      <dt-recipe-call-bar-button>
+      <dt-recipe-callbar-button>
         Label only
         <template #tooltip>
           Tooltip
         </template>
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
 
-      <dt-recipe-call-bar-button>
+      <dt-recipe-callbar-button aria-label="mic on">
         <template #icon>
           <icon-mic-on />
         </template>
         <template #tooltip>
           Icon only
         </template>
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
 
-      <dt-recipe-call-bar-button disabled>
-        <template #label>
-          Disabled
-        </template>
+      <dt-recipe-callbar-button disabled>
+        Disabled
         <template #icon>
           <icon-mic-on />
         </template>
         <template #tooltip>
           Tooltip
         </template>
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
     </div>
 
     <h3>
       Active Call Bar Buttons
     </h3>
     <div class="d-d-flex d-flow16 d-fw-wrap">
-      <dt-recipe-call-bar-button active>
-        <template #label>
-          Button
-        </template>
+      <dt-recipe-callbar-button active>
+        Button
         <template #icon>
           <icon-recording />
         </template>
         <template #tooltip>
           Tooltip
         </template>
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
 
-      <dt-recipe-call-bar-button active>
+      <dt-recipe-callbar-button active>
+        No tooltip
         <template #icon>
           <icon-dialpad-glyph />
         </template>
-        No tooltip
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
 
-      <dt-recipe-call-bar-button active>
+      <dt-recipe-callbar-button active>
         Label only
         <template #tooltip>
           Tooltip
         </template>
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
 
-      <dt-recipe-call-bar-button active>
+      <dt-recipe-callbar-button
+        active
+        aria-label="mic on"
+      >
         <template #icon>
           <icon-mic-on />
         </template>
         <template #tooltip>
           Icon only
         </template>
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
 
-      <dt-recipe-call-bar-button active disabled>
-        <template #label>
-          Disabled
-        </template>
+      <dt-recipe-callbar-button
+        active
+        disabled
+      >
+        Disabled
         <template #icon>
           <icon-mic-on />
         </template>
         <template #tooltip>
           Tooltip
         </template>
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
     </div>
 
     <h3>
@@ -112,178 +111,214 @@
       Danger Call Bar Buttons look the same as regular buttons; they only differ on their active state.
     </div>
     <div class="d-d-flex d-flow16 d-fw-wrap">
-      <dt-recipe-call-bar-button danger>
-        <template #label>
-          Button
-        </template>
+      <dt-recipe-callbar-button danger>
+        Button
         <template #icon>
           <icon-recording />
         </template>
         <template #tooltip>
           Tooltip
         </template>
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
 
-      <dt-recipe-call-bar-button danger>
+      <dt-recipe-callbar-button danger>
+        No tooltip
         <template #icon>
           <icon-dialpad-glyph />
         </template>
-        No tooltip
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
 
-      <dt-recipe-call-bar-button danger>
+      <dt-recipe-callbar-button danger>
         Label only
         <template #tooltip>
           Tooltip
         </template>
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
 
-      <dt-recipe-call-bar-button danger>
+      <dt-recipe-callbar-button
+        danger
+        aria-label="mic on"
+      >
         <template #icon>
           <icon-mic-on />
         </template>
         <template #tooltip>
           Icon only
         </template>
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
 
-      <dt-recipe-call-bar-button disabled danger>
-        <template #label>
-          Disabled
-        </template>
+      <dt-recipe-callbar-button
+        disabled
+        danger
+      >
+        Disabled
         <template #icon>
           <icon-mic-on />
         </template>
         <template #tooltip>
           Tooltip
         </template>
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
     </div>
 
     <h3>
       Danger Active Call Bar Buttons
     </h3>
     <div class="d-d-flex d-flow16 d-fw-wrap">
-      <dt-recipe-call-bar-button active danger>
-        <template #label>
-          Button
-        </template>
+      <dt-recipe-callbar-button
+        active
+        danger
+      >
+        Button
         <template #icon>
           <icon-recording />
         </template>
         <template #tooltip>
           Tooltip
         </template>
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
 
-      <dt-recipe-call-bar-button active danger>
+      <dt-recipe-callbar-button
+        active
+        danger
+      >
+        No tooltip
         <template #icon>
           <icon-dialpad-glyph />
         </template>
-        No tooltip
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
 
-      <dt-recipe-call-bar-button active danger>
+      <dt-recipe-callbar-button
+        active
+        danger
+      >
         Label only
         <template #tooltip>
           Tooltip
         </template>
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
 
-      <dt-recipe-call-bar-button active danger>
+      <dt-recipe-callbar-button
+        active
+        danger
+        aria-label="mic on"
+      >
         <template #icon>
           <icon-mic-on />
         </template>
         <template #tooltip>
           Icon only
         </template>
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
 
-      <dt-recipe-call-bar-button active disabled danger>
-        <template #label>
-          Disabled
-        </template>
+      <dt-recipe-callbar-button
+        active
+        disabled
+        danger
+      >
+        Disabled
         <template #icon>
           <icon-mic-on />
         </template>
         <template #tooltip>
           Tooltip
         </template>
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
     </div>
-
 
     <h3>
       Rounded
     </h3>
     <div class="d-d-flex d-flow16 d-fw-wrap">
-      <dt-recipe-call-bar-button circle>
+      <dt-recipe-callbar-button
+        circle
+        aria-label="recording"
+      >
         <template #icon>
           <icon-recording />
         </template>
         <template #tooltip>
           Rounded button
         </template>
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
 
-      <dt-recipe-call-bar-button circle active>
+      <dt-recipe-callbar-button
+        circle
+        active
+        aria-label="person"
+      >
         <template #icon>
           <icon-person />
         </template>
         <template #tooltip>
           Rounded active
         </template>
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
 
-      <dt-recipe-call-bar-button circle danger>
+      <dt-recipe-callbar-button
+        circle
+        danger
+        aria-label="play"
+      >
         <template #icon>
           <icon-play />
         </template>
         <template #tooltip>
           Rounded danger
         </template>
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
 
-      <dt-recipe-call-bar-button circle active danger>
+      <dt-recipe-callbar-button
+        circle
+        active
+        danger
+        aria-label="star"
+      >
         <template #icon>
           <icon-star />
         </template>
         <template #tooltip>
           Rounded danger active
         </template>
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
 
-      <dt-recipe-call-bar-button circle disabled>
+      <dt-recipe-callbar-button
+        circle
+        disabled
+        aria-label="dialpad"
+      >
         <template #icon>
           <icon-dialpad-glyph />
         </template>
         <template #tooltip>
           Rounded button
         </template>
-      </dt-recipe-call-bar-button>
+      </dt-recipe-callbar-button>
     </div>
   </div>
 </template>
 
 <script>
-import DtRecipeCallBarButton from './call_bar_button';
+import DtRecipeCallbarButton from './callbar_button';
 import IconDialpadGlyph from '@dialpad/dialtone/lib/dist/vue/icons/IconDialpadGlyph';
 import IconMicOn from '@dialpad/dialtone/lib/dist/vue/icons/IconMicOn';
 import IconRecording from '@dialpad/dialtone/lib/dist/vue/icons/IconRecording';
 import IconPerson from '@dialpad/dialtone/lib/dist/vue/icons/IconPerson';
 import IconPlay from '@dialpad/dialtone/lib/dist/vue/icons/IconPlay';
 import IconStar from '@dialpad/dialtone/lib/dist/vue/icons/IconStar';
+import IconAi from '@dialpad/dialtone/lib/dist/vue/icons/IconAiGlyph.vue';
 
 export default {
-  name: 'DtRecipeCallBarButtonVariants',
+  name: 'DtRecipeCallbarButtonVariants',
   components: {
-    DtRecipeCallBarButton,
+    DtRecipeCallbarButton,
     IconDialpadGlyph,
     IconMicOn,
     IconRecording,
     IconPerson,
     IconPlay,
-    IconStar
+    IconStar,
+    IconAi,
   },
 };
 </script>
