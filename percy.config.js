deb248e91e375ce4947954a3d5eee4eae08beeac
Julio Ortega <julio.ortega@dialpad.com>
2022-07-26 19:16:17 -0500

    move config

--- percy.config.js
+++ percy.config.js
@@ -1,21 +1,5 @@
 module.exports = {
   version: 2,
-  storybook: {
-    include: [
-      'Visual Testing/Combobox With Popover: Default',
-      'Visual Testing/Dropdown: Default',
-      'Visual Testing/Popover: Default',
-      'Visual Testing/Toast: Default',
-      'Visual Testing/Tooltip: Default',
-      'Visual Testing/Inline Components: Default',
-      'Visual Testing/Modal: Default',
-      'Visual Testing/Modal: With Footer',
-      'Visual Testing/Modal: With Fixed Header Footer',
-      'Visual Testing/Modal: With Danger Style',
-      'Visual Testing/Modal: With Full Size',
-      'Visual Testing/Modal: With Custom Header And Content',
-    ],
-  },
   snapshot: {
     widths: [1280],
     minHeight: 2000,
