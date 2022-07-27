8249f84aa5fbd13284ec321b45397c0ea58b053b
shefaligupta <shefali.gupta@dialpad.com>
2022-07-27 12:58:57 -0600

    feat: dt-pagination change recipe to component

---
+++ components/pagination/pagination_default.story.vue
@@ -0,0 +1,13 @@
+<template>
+  <dt-pagination :total-pages="5" />
+</template>
+
+<script>
+import DtPagination from './pagination';
+
+export default {
+  name: 'DtRecipePaginationDefault',
+  components: { DtPagination },
+  methods: {},
+};
+</script>
