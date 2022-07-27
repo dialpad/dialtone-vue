2e5e096283c67deda6bc04b727d611c01e26a30a
Julio Ortega <julio.ortega@dialpad.com>
2022-07-27 15:28:04 -0500

    improve visual testing

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -72,20 +72,4 @@ export const parameters = {
       return src.slice(10, -12);
     }
   },
-  percy: {
-    include: [
-      'Visual Testing/Combobox With Popover: Default',
-      'Visual Testing/Dropdown: Default',
-      'Visual Testing/Popover: Default',
-      'Visual Testing/Toast: Default',
-      'Visual Testing/Tooltip: Default',
-      'Visual Testing/Inline Components: Default',
-      'Visual Testing/Modal: Default',
-      'Visual Testing/Modal: With Footer',
-      'Visual Testing/Modal: With Fixed Header Footer',
-      'Visual Testing/Modal: With Danger Style',
-      'Visual Testing/Modal: With Full Size',
-      'Visual Testing/Modal: With Custom Header And Content',
-    ],
-  }
 };
--- percy.config.js
+++ percy.config.js
@@ -1,7 +1,26 @@
 module.exports = {
   version: 2,
+  storybook: {
+    queryParams: {
+      viewMode: 'docs',
+    },
+    include: [
+      'Visual Testing/Combobox With Popover: Default',
+      'Visual Testing/Dropdown: Default',
+      'Visual Testing/Popover: Default',
+      'Visual Testing/Toast: Default',
+      'Visual Testing/Tooltip: Default',
+      'Visual Testing/Inline Components: Default',
+      'Visual Testing/Modal: Default',
+      'Visual Testing/Modal: With Footer',
+      'Visual Testing/Modal: With Fixed Header Footer',
+      'Visual Testing/Modal: With Danger Style',
+      'Visual Testing/Modal: With Full Size',
+      'Visual Testing/Modal: With Custom Header And Content',
+    ],
+  },
   snapshot: {
-    widths: [1280],
+    widths: [2048],
     minHeight: 2000,
   },
 };
