324ab144842692802c9a17e01844535780a22e76
shefaligupta <shefali.gupta@dialpad.com>
2022-07-27 19:34:27 -0600

    feat: dt-pagination update tests

--- components/pagination/pagination.test.js
+++ components/pagination/pagination.test.js
@@ -1,6 +1,7 @@
 import { assert } from 'chai';
 import { createLocalVue, mount } from '@vue/test-utils';
 import DtPagination from './pagination.vue';
+import { DtButton } from '@';
 
 // Constants
 const basePropsData = {
@@ -12,6 +13,7 @@ describe('DtPagination Tests', function () {
   let wrapper;
   let prev;
   let next;
+  let separators;
 
   // Environment
   let propsData = basePropsData;
@@ -22,6 +24,7 @@ describe('DtPagination Tests', function () {
   const _setChildWrappers = () => {
     prev = wrapper.find('[data-qa="dt-pagination-prev"]');
     next = wrapper.find('[data-qa="dt-pagination-next"]');
+    separators = wrapper.findAll('[data-qa="pagination-separator"]');
   };
 
   const _setWrappers = () => {
@@ -50,14 +53,62 @@ describe('DtPagination Tests', function () {
       beforeEach(function () {
         _setWrappers();
       });
+      it('should not render separator', function () { assert.isFalse(separators.exists()); });
       it('should render the component', function () { assert.exists(wrapper, 'wrapper exists'); });
       it('should render prev button', function () { assert.exists(prev, 'previous button exists'); });
+      it('should disable prev button', function () { assert.strictEqual(prev.element.disabled, true); });
       it('should render next button', function () { assert.exists(next, 'next button exists'); });
+      it('should enable next button', function () { assert.strictEqual(next.element.disabled, false); });
       it('prev button should have aria-label', function () {
         assert.equal(prev.attributes('aria-label'), 'previous');
       });
       it('next button should have aria-label', function () {
         assert.equal(next.attributes('aria-label'), 'next');
+      });
+    });
+
+    describe('When rendered with active page', function () {
+      beforeEach(async function () {
+        propsData = {
+          totalPages: 10,
+          activePage: 9,
+        };
+        await _setWrappers();
+      });
+      it('should render one separators', function () {
+        assert.exists(separators, 'separators exists');
+        assert.strictEqual(separators.length, 1);
+      });
+    });
+
+    describe('When rendered with more pages', function () {
+      beforeEach(async function () {
+        propsData = {
+          totalPages: 15,
+          activePage: 7,
+          maxVisible: 7,
+        };
+        await _setWrappers();
+      });
+      it('should render two separators', function () {
+        assert.exists(separators, 'separators exists');
+        assert.lengthOf(separators, 2);
+        // case when maxVisible is even - we round to the nearest odd when active page is in the mid-range
+        assert.lengthOf(wrapper.findAllComponents(DtButton), 9);
+      });
+    });
+
+    describe('When maxVisible is even', function () {
+      beforeEach(async function () {
+        propsData = {
+          totalPages: 15,
+          activePage: 7,
+          maxVisible: 6,
+        };
+        await _setWrappers();
+      });
+      it('should render less than maxVisible when active page is in mid range', function () {
+        assert.lengthOf(wrapper.findAllComponents(DtButton), 7);
       });
     });
   });
@@ -73,6 +124,14 @@ describe('DtPagination Tests', function () {
       });
       it('should emit change event', function () {
         assert.property(wrapper.emitted(), 'change');
+      });
+      it('should enable prev button', function () { assert.strictEqual(prev.element.disabled, false); });
+      it('should update page value', function () { assert.equal(wrapper.vm.currentPage, 2); });
+      it('should disable prev button and update page', async function () {
+        await prev.trigger('click');
+        assert.property(wrapper.emitted(), 'change');
+        assert.strictEqual(prev.element.disabled, true);
+        assert.equal(wrapper.vm.currentPage, 1);
       });
     });
   });
