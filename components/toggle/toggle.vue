1fcf7e3ee5f45888d13cbbecc395873195d88073
Jonathan Paugh <jonathan.paugh@dialpad.com>
2022-08-05 13:04:04 -0700

    docs: add model tags

--- components/toggle/toggle.vue
+++ components/toggle/toggle.vue
@@ -54,6 +54,9 @@ export default {
       default: false,
     },
 
+    /**
+     * @model checked
+     */
     checked: {
       type: Boolean,
       default: false,
@@ -70,7 +73,16 @@ export default {
     },
   },
 
-  emits: ['change'],
+  emits: [
+    /**
+     * Triggers on state change
+     *
+     * @event change
+     * @type {boolean}
+     * @model change
+     */
+    'change',
+  ],
 
   data () {
     return {
