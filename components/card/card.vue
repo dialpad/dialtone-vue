a3451e94e4fe4ff4a6b76baa15b602ef486ac41d
shefaligupta <shefali.gupta@dialpad.com>
2022-09-09 15:37:52 -0600

    review comments, update accessibility issues

--- components/card/card.vue
+++ components/card/card.vue
@@ -1,17 +1,26 @@
 <template>
   <div
-    class="d-card"
+    :class="[
+      'd-card',
+      containerClass,
+    ]"
     data-qa="dt-card"
   >
     <div
       v-if="$slots.header"
-      class="d-card__header"
+      :class="[
+        'd-card__header',
+        headerClass,
+      ]"
     >
       <slot name="header" />
     </div>
 
     <div
-      class="d-card__content"
+      :class="[
+        'd-card__content',
+        contentClass,
+      ]"
       :style="{ 'max-height': maxHeight }"
     >
       <slot name="content" />
@@ -19,7 +28,10 @@
 
     <div
       v-if="$slots.footer"
-      class="d-card__footer"
+      :class="[
+        'd-card__footer',
+        footerClass,
+      ]"
     >
       <slot name="footer" />
     </div>
@@ -37,6 +49,38 @@ export default {
     maxHeight: {
       type: String,
       default: null,
+    },
+
+    /**
+     * class for card container.
+     */
+    containerClass: {
+      type: [String, Array, Object],
+      default: '',
+    },
+
+    /**
+     * class for card content.
+     */
+    contentClass: {
+      type: [String, Array, Object],
+      default: '',
+    },
+
+    /**
+     * class for card header.
+     */
+    headerClass: {
+      type: [String, Array, Object],
+      default: '',
+    },
+
+    /**
+     * class for card footer.
+     */
+    footerClass: {
+      type: [String, Array, Object],
+      default: '',
     },
   },
 };
