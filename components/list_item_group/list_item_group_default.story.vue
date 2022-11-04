641bd5da8e7706b0da4f555e27afa1609fedc50f
Brad Paugh <brad.paugh@dialpad.com>
2022-11-04 13:44:42 -0700

    feat: list item group component

---
+++ components/list_item_group/list_item_group_default.story.vue
@@ -0,0 +1,36 @@
+<template>
+  <dt-list-item-group
+    :heading="heading"
+    :heading-class="headingClass"
+    :list-class="listClass"
+  >
+    <dt-list-item
+      class="d-pl8"
+      navigation-type="tab"
+    >
+      item1
+    </dt-list-item>
+    <dt-list-item
+      class="d-pl8"
+      navigation-type="tab"
+    >
+      item2
+    </dt-list-item>
+    <dt-list-item
+      class="d-pl8"
+      navigation-type="tab"
+    >
+      item3
+    </dt-list-item>
+  </dt-list-item-group>
+</template>
+
+<script>
+import DtListItemGroup from './list_item_group';
+import DtListItem from '../list_item/list_item';
+
+export default {
+  name: 'DtListItemGroupDefault',
+  components: { DtListItemGroup, DtListItem },
+};
+</script>
