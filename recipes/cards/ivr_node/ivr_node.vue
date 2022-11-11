fd6b9431ff1c82b456b69bb33e57b5104b8e60a9
shefaligupta <shefali.gupta@dialpad.com>
2022-11-10 17:24:21 -0700

    fix(dt-recipe-ivr-node): fix close sync for menu dropdown

--- recipes/cards/ivr_node/ivr_node.vue
+++ recipes/cards/ivr_node/ivr_node.vue
@@ -60,22 +60,23 @@
         <!-- node menu for actions like edit, copy, delete -->
         <dt-dropdown
           placement="bottom"
-          :open.sync="isOpen"
         >
           <template #anchor>
             <dt-button
               importance="clear"
               :aria-label="menuButtonAriaLabel"
-              @click.stop.prevent="openMenu"
             >
               <template #icon>
                 <icon-menu-vertical class="d-fc-black-900" />
               </template>
             </dt-button>
           </template>
-          <template #list>
+          <template #list="{ close }">
             <div class="d-w164">
-              <slot name="menuItems" />
+              <slot
+                name="menuItems"
+                :close="close"
+              />
             </div>
           </template>
         </dt-dropdown>
@@ -160,12 +161,6 @@ export default {
     'click',
   ],
 
-  data () {
-    return {
-      isOpen: false,
-    };
-  },
-
   computed: {
     nodeIcon () {
       return IVR_NODE_ICON_TYPES[this.nodeType];
@@ -174,12 +169,6 @@ export default {
     headerColor () {
       const { normal, selected } = IVR_NODE_COLOR_MAPPING[this.nodeType];
       return this.isSelected ? selected : normal;
-    },
-  },
-
-  methods: {
-    openMenu () {
-      this.isOpen = true;
     },
   },
 };
