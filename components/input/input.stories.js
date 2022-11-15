88485bfc109acee926b2faa272f39fae7eb69e3b
Julio Ortega <julio.ortega@dialpad.com>
2022-11-15 14:51:06 -0600

    refactor(input): icon component usage

--- components/input/input.stories.js
+++ components/input/input.stories.js
@@ -270,18 +270,18 @@ WithDescription.args = {
 
 export const WithLeftIcon = Template.bind({});
 WithLeftIcon.args = {
-  leftIcon: 'IconSend',
+  leftIcon: 'send',
 };
 
 export const WithRightIcon = Template.bind({});
 WithRightIcon.args = {
-  rightIcon: 'IconLock',
+  rightIcon: 'lock-filled',
 };
 
 export const WithBothIcons = Template.bind({});
 WithBothIcons.args = {
-  leftIcon: 'IconSend',
-  rightIcon: 'IconLock',
+  leftIcon: 'send',
+  rightIcon: 'lock-filled',
 };
 
 export const WithWarning = Template.bind({});
