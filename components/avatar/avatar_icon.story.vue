da90d4c007af0093ff320b63119d9b39e8206f71
Julio Ortega <julio.ortega@dialpad.com>
2022-11-16 14:09:28 -0600

    fix(avatar): update icon sizing

--- components/avatar/avatar_icon.story.vue
+++ components/avatar/avatar_icon.story.vue
@@ -4,16 +4,25 @@
     :size="size"
     :color="color"
   >
-    <dt-icon :name="defaultSlot" />
+    <dt-icon
+      :name="defaultSlot"
+      :size="iconSize"
+    />
   </dt-avatar>
 </template>
 
 <script>
 import DtAvatar from './avatar';
 import { DtIcon } from '@/components/icon';
+import { AVATAR_ICON_SIZES } from './avatar_constants';
 
 export default {
   name: 'DtAvatarDefault',
   components: { DtAvatar, DtIcon },
+  computed: {
+    iconSize () {
+      return AVATAR_ICON_SIZES[this.size];
+    },
+  },
 };
 </script>
