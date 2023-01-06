922f7c27b9468801564e14a024ef0a0aa7f11f91
Julio Ortega <julio.ortega@dialpad.com>
2023-01-06 17:50:45 -0600

    refactor

--- components/popover/popover.vue
+++ components/popover/popover.vue
@@ -8,7 +8,7 @@
         class="d-modal--transparent"
         :aria-hidden="modal && isOpen ? 'false' : 'true'"
         @click.prevent.stop
-        @wheel="(e) => (isOpen && modal) && e.preventDefault()"
+        @wheel.prevent.stop
       />
     </Teleport>
     <component
