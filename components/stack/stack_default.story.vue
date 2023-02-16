649db420ae6afb64b21cd5475913ff010d2fba5a
iropolo <ignacio.ropolo@dialpad.com>
2023-02-16 14:15:01 -0300

    feat(stack): new stack component

---
+++ components/stack/stack_default.story.vue
@@ -0,0 +1,26 @@
+<template>
+  <dt-stack
+    :direction="$attrs.direction"
+    :as="$attrs.as"
+    :gap="$attrs.gap"
+  >
+    <div class="d-bgc-magenta-100">
+      Stack item 1
+    </div>
+    <div class="d-bgc-magenta-100">
+      Stack item 2
+    </div>
+    <div class="d-bgc-magenta-100">
+      Stack item 3
+    </div>
+  </dt-stack>
+</template>
+
+<script>
+import DtStack from './stack';
+
+export default {
+  name: 'StackDefault',
+  components: { DtStack },
+};
+</script>
