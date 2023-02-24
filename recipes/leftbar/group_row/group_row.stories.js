82fba26f4379b9c33f1d184f3e35553cc788df9a
Julio Ortega <julio.ortega@dialpad.com>
2023-02-24 12:25:38 -0600

    feat(leftbar): add group row recipe

---
+++ recipes/leftbar/group_row/group_row.stories.js
@@ -0,0 +1,91 @@
+import { action } from '@storybook/addon-actions';
+import { createTemplateFromVueFile } from '@/common/storybook_utils';
+import DtRecipeGroupRow from './group_row';
+import DtRecipeGroupRowMdx from './group_row.mdx';
+import DtRecipeGroupRowDefaultTemplate from './group_row_default.story.vue';
+import DtRecipeGroupRowVariantsTemplate from './group_row_variants.story.vue';
+
+// Default Prop Values
+export const argsData = {
+  onClick: action('click'),
+};
+
+export const argTypesData = {
+  // Props
+  groupCount: {
+    defaultValue: 8,
+  },
+
+  names: {
+    defaultValue: 'Jaqueline Nackos, Lori Smith',
+  },
+
+  avatarInitials: {
+    defaultValue: 'JN',
+  },
+
+  avatarSrc: {
+    defaultValue: '/static/media/../components/avatar/person.png',
+  },
+
+  // Slots
+
+  // Action Event Handlers
+  onClick: {
+    table: {
+      disable: true,
+    },
+  },
+
+  click: {
+    description: 'Native click event',
+    table: {
+      type: { summary: 'event' },
+    },
+  },
+};
+
+const decorator = () => ({
+  template: '<div class="d-w512"><story /></div>',
+});
+
+// Story Collection
+export default {
+  title: 'Recipes/leftbar/Group Row',
+  component: DtRecipeGroupRow,
+  args: argsData,
+  argTypes: argTypesData,
+  decorators: [decorator],
+  excludeStories: /.*Data$/,
+  parameters: {
+    controls: {
+      sort: 'requiredFirst',
+    },
+    docs: {
+      page: DtRecipeGroupRowMdx,
+    },
+    options: {
+      showPanel: true,
+    },
+  },
+};
+
+// Templates
+const DefaultTemplate = (args, { argTypes }) => createTemplateFromVueFile(
+  args,
+  argTypes,
+  DtRecipeGroupRowDefaultTemplate,
+);
+const VariantsTemplate = (args, { argTypes }) => createTemplateFromVueFile(
+  args,
+  argTypes,
+  DtRecipeGroupRowVariantsTemplate,
+);
+
+// Stories
+export const Default = DefaultTemplate.bind({});
+Default.args = {};
+
+export const Variants = VariantsTemplate.bind({});
+Variants.args = {};
+Variants.parameters = { controls: { disable: true }, actions: { disable: true }, options: { showPanel: false } };
