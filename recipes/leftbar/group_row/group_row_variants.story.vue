5535526f2c1f88f50aff55a35cfca82215ebd4f1
Jose Silva <jose.silva@dialpad.com>
2023-02-28 13:36:36 -0300

    update to dt-stack in variants story

--- recipes/leftbar/group_row/group_row_variants.story.vue
+++ recipes/leftbar/group_row/group_row_variants.story.vue
@@ -1,72 +1,67 @@
 <template>
   <div class="d-w464">
     <dt-stack gap="600">
-      <div>
+      <dt-stack gap="400">
         <p class="d-fw-bold">
           Default behavior
         </p>
         <dt-recipe-group-row
-          class="d-pt8"
           avatar-initials="JN"
           :group-count="2"
           names="Jaqueline Nackos, Lori Smith"
           :avatar-src="$attrs.avatarSrc"
         />
-      </div>
+      </dt-stack>
 
-      <div>
+      <dt-stack gap="400">
         <p class="d-fw-bold">
           Ellipsed names
         </p>
         <dt-recipe-group-row
-          class="d-pt8"
           avatar-initials="JN"
           :group-count="4"
           names="Jaqueline Nackos, Lori Smith, Jaqueline Nackos, Lori Smith"
           :avatar-src="$attrs.avatarSrc"
         />
-      </div>
+      </dt-stack>
 
-      <div>
+      <dt-stack gap="400">
         <p class="d-fw-bold">
           With unread count
         </p>
         <dt-recipe-group-row
-          class="d-pt8"
           avatar-initials="JN"
           :group-count="2"
           names="Jaqueline Nackos, Lori Smith"
           :avatar-src="$attrs.avatarSrc"
           unread-count="1"
         />
-      </div>
+      </dt-stack>
 
-      <div>
+      <dt-stack gap="400">
         <p class="d-fw-bold">
           Selected
         </p>
         <dt-recipe-group-row
-          class="d-pt8"
           avatar-initials="JN"
           :group-count="2"
           names="Jaqueline Nackos, Lori Smith"
           :avatar-src="$attrs.avatarSrc"
           selected
         />
-      </div>
+      </dt-stack>
 
-      <div>
+      <dt-stack gap="400">
         <p class="d-fw-bold">
           With Initial as fallback when avatar image src cannot be found.
         </p>
         <dt-recipe-group-row
-          class="d-pt8"
           avatar-initials="JN"
           :group-count="2"
           names="Jaqueline Nackos, Lori Smith"
           avatar-src="/static/media/../components/avatar/404"
         />
-      </div>
+      </dt-stack>
     </dt-stack>
   </div>
 </template>
