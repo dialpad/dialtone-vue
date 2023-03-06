3ddc5d946ff140591a749272193522a2d9e9a2ce
Brad Paugh <brad.paugh@dialpad.com>
2023-03-05 23:54:23 -0800

    fix $attrs

--- recipes/leftbar/contact_row/contact_row_default.story.vue
+++ recipes/leftbar/contact_row/contact_row_default.story.vue
@@ -1,18 +1,18 @@
 <template>
   <dt-recipe-contact-row
-    :name="name"
-    :avatar-src="avatarSrc"
-    :avatar-presence="avatarPresence"
-    :avatar-seed="avatarSeed"
-    :presence-text="presenceText"
-    :unread-count="unreadCount"
-    :user-status="userStatus"
-    :has-unreads="hasUnreads"
-    :muted="muted"
-    :selected="selected"
-    :no-initials="noInitials"
-    @click="onClick"
-    @call="onCall"
+    :name="$attrs.name"
+    :avatar-src="$attrs.avatarSrc"
+    :avatar-presence="$attrs.avatarPresence"
+    :avatar-seed="$attrs.avatarSeed"
+    :presence-text="$attrs.presenceText"
+    :unread-count="$attrs.unreadCount"
+    :user-status="$attrs.userStatus"
+    :has-unreads="$attrs.hasUnreads"
+    :muted="$attrs.muted"
+    :selected="$attrs.selected"
+    :no-initials="$attrs.noInitials"
+    @click="$attrs.onClick"
+    @call="$attrs.onCall"
   />
 </template>
 
