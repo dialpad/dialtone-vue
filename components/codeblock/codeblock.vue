9e0a0372700797d9894fdff1d1506bbed178a8e6
Cormac Mollitor <cormac.mollitor@dialpad.com>
2023-03-22 15:59:39 -0700

    feat: adds codeblock component to dialtone vue

---
+++ components/codeblock/codeblock.vue
@@ -0,0 +1,20 @@
+<template>
+  <pre>
+    <code
+      class="d-d-block d-bgc-white d-ws-pre-wrap d-p8 d-my4 d-ba d-bc-black-500 d-bar8 d-fs-100 d-lh-200 d-fc-red-400"
+    >{{ text }}</code>
+  </pre>
+</template>
+
+<script>
+export default {
+  name: 'DtCodeblock',
+
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
