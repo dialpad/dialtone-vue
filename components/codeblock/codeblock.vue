5c7e8a0d7f2e196dbca053e2b9655beb2e965c48
Cormac Mollitor <cormac.mollitor@dialpad.com>
2023-03-24 15:57:02 -0700

    refactor: adds mono font class and fixes lint error

--- components/codeblock/codeblock.vue
+++ components/codeblock/codeblock.vue
@@ -1,7 +1,8 @@
 <template>
   <pre>
     <code
-      class="d-d-block d-bgc-secondary d-ws-pre-wrap d-p8 d-ba d-bc-subtle d-bar8 d-fs-200 d-lh-400 d-fc-secondary"
+      class="d-d-block d-bgc-secondary d-ws-pre-wrap d-p8 d-ba
+      d-bc-subtle d-bar8 d-fs-200 d-ff-mono d-lh-400 d-fc-secondary"
     >{{ text }}</code>
   </pre>
 </template>
