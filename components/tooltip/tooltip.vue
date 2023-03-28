fcc269852a9de0fdf22ba0f12bc510c3be698b28
Brad Paugh <brad.paugh@dialpad.com>
2023-03-28 16:55:19 -0700

    feat(tooltip): add 500ms default delay

--- components/tooltip/tooltip.vue
+++ components/tooltip/tooltip.vue
@@ -50,6 +50,7 @@ import {
   TOOLTIP_KIND_MODIFIERS,
   TOOLTIP_DIRECTIONS,
   TOOLTIP_STICKY_VALUES,
+  TOOLTIP_DELAY_MS,
 } from './tooltip_constants';
 import { getUniqueString, hasSlotContent } from '@/common/utils';
 import DtLazyShow from '../lazy_show/lazy_show';
@@ -186,10 +187,20 @@ export default {
 
     /**
      * Whether to apply transition on initial render in the content lazy show component.
+     * @values true, false
      */
     contentAppear: {
       type: Boolean,
       default: false,
+    },
+
+    /**
+     * Whether the tooltip will have a delay when being focused or moused over.
+     * @values true, false
+     */
+    delay: {
+      type: Boolean,
+      default: true,
     },
   },
 
@@ -214,6 +225,9 @@ export default {
       TOOLTIP_KIND_MODIFIERS,
       hasSlotContent,
       tip: null,
+
+      inTimer: null,
+      outTimer: null,
 
       // Internal state for whether the tooltip is shown. Changing the prop
       // will update this.
@@ -324,6 +338,17 @@ export default {
     },
 
     onEnterAnchor (e) {
+      if (this.delay) {
+        this.inTimer = setTimeout(function (event) {
+          return this.triggerShow(event);
+        }.bind(this, e), TOOLTIP_DELAY_MS);
+      } else {
+        return this.triggerShow(event);
+      }
+      clearTimeout(this.outTimer);
+    },
+
+    triggerShow (e) {
       if (e.type === 'focusin') {
         // only show tooltips on visible focus when triggered via focus.
         // when the user is using the mouse they only want tooltips to display
@@ -341,6 +366,15 @@ export default {
     },
 
     onLeaveAnchor () {
+      if (this.delay) {
+        this.outTimer = setTimeout(this.triggerHide, TOOLTIP_DELAY_MS);
+      } else {
+        return this.triggerHide();
+      }
+      clearTimeout(this.inTimer);
+    },
+
+    triggerHide () {
       if (this.show === null) this.isShown = false;
     },
 
