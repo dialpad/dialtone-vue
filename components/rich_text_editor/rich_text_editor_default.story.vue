5a05f314c8471beefa5a7e5d194aa5c2ca0626d8
Mika Paulasaari <mpaulasaari@dialpad.com>
2023-04-11 15:47:03 -0700

    feat(rich-text-editor): add rich text editor component

---
+++ components/rich_text_editor/rich_text_editor_default.story.vue
@@ -0,0 +1,36 @@
+<template>
+  <dt-rich-text-editor
+    v-model="inputValue"
+    :editable="editable"
+    :input-aria-label="inputAriaLabel"
+    @blur="onBlur"
+    @input="onInput"
+    @focus="onFocus"
+    @focusin="onFocusIn"
+    @focusout="onFocusOut"
+  />
+</template>
+
+<script>
+import DtRichTextEditor from './rich_text_editor';
+
+export default {
+  name: 'DtRichTextEditorDefault',
+
+  components: {
+    DtRichTextEditor,
+  },
+
+  data () {
+    return {
+      inputValue: this.value,
+    };
+  },
+
+  watch: {
+    value (val) {
+      this.inputValue = val;
+    },
+  },
+};
+</script>
