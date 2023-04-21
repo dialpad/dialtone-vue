7b18c094816b3f4e76db5e210f967a2c6e809cea
Julio Ortega <julio.ortega@dialpad.com>
2023-04-21 12:00:37 -0600

    rename unread_chip to unread_pill

--- recipes/leftbar/unread_chip/unread_chip_constants.js
+++
@@ -1,7 +0,0 @@
-export const UNREAD_CHIP_DIRECTIONS = ['up', 'down'];
-export const UNREAD_CHIP_KINDS = ['mentions', 'messages'];
-
-export default {
-  UNREAD_CHIP_DIRECTIONS,
-  UNREAD_CHIP_KINDS,
-};
