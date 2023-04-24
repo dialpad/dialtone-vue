7e01e207b27c77118e569cfc817621790ae35ef2
laurafirespotter <laura@firespotter.com>
2023-04-24 11:45:38 -0500

    feat(header): add setting menu button with update option

---
+++ recipes/header/settings_menu_button/settings_menu_button_variants.story.vue
@@ -0,0 +1,18 @@
+<template>
+  <dt-recipe-settings-menu-button
+    :update-available="true"
+  >
+    <template v-if="defaultSlot">
+      {{ defaultSlot }}
+    </template>
+  </dt-recipe-settings-menu-button>
+</template>
+
+<script>
+import DtRecipeSettingsMenuButton from './settings_menu_button';
+
+export default {
+  name: 'DtRecipeSettingsMenuButtonVariants',
+  components: { DtRecipeSettingsMenuButton },
+};
+</script>
