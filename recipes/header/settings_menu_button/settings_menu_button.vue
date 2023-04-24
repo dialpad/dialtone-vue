679b1709069f6f367037d794a3edef3e2402698a
laurafirespotter <laura@firespotter.com>
2023-04-24 11:45:38 -0500

    feat(header): add setting menu button with update option

---
+++ recipes/header/settings_menu_button/settings_menu_button.vue
@@ -0,0 +1,86 @@
+<template>
+  <dt-button
+    v-if="updateAvailable"
+    importance="outlined"
+    size="xs"
+    icon-position="right"
+    class="d-h32 d-bar-pill d-bgc-blue-200 d-bgo25 h:d-bgo50 d-bc-blue-500 d-bco25 d-fc-blue-500"
+  >
+    <slot />
+    <template #icon>
+      <dt-icon
+        name="more-vertical"
+        size="400"
+        class="d-bs-none d-fc-blue-500 d-ol-none d-svg--size20"
+      />
+    </template>
+  </dt-button>
+  <dt-button
+    v-else
+    importance="clear"
+    kind="inverted"
+    class="settings-menu-button d-p0 d-bar24 d-h32 d-w24 d-theme-topbar-fc"
+  >
+    <template #icon>
+      <dt-icon
+        name="more-vertical"
+        size="400"
+        class="d-bs-none d-ol-none d-svg--size20"
+      />
+    </template>
+  </dt-button>
+</template>
+
+<script>
+import DtButton from '@/components/button/button';
+import { DtIcon } from '@/components/icon';
+
+export default {
+  name: 'SettingsMenuButton',
+
+  components: {
+    DtButton,
+    DtIcon,
+  },
+
+  props: {
+    /**
+     * Determines whether the button should display the update state
+     * default is false
+     * @values true, false
+     */
+    updateAvailable: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  emits: [
+    /**
+     * Native click event
+     *
+     * @event click
+     * @type {PointerEvent | KeyboardEvent}
+     */
+    'click',
+  ],
+
+};
+</script>
+
+<style scoped lang="less">
+.d-btn:active {
+  --bgo: 85% !important;
+}
+
+.settings-menu-button {
+  &:hover {
+    background-color: hsla(var(--theme-topbar-color-hsl) / .05);
+  }
+
+  &:focus-visible {
+    color: var(--theme-topbar-color-hsl);
+    box-shadow: 0 0 0 var(--su2) hsla(var(--theme-sidebar-color-hsl) / .15);
+  }
+}
+</style>
---
+++ recipes/header/settings_menu_button/settings_menu_button_default.story.vue
@@ -0,0 +1,18 @@
+<template>
+  <dt-recipe-settings-menu-button
+    :update-available="updateAvailable"
+  >
+    <template v-if="defaultSlot">
+      {{ defaultSlot }}
+    </template>
+  </dt-recipe-settings-menu-button>
+</template>
+
+<script>
+import DtRecipeSettingsMenuButton from './settings_menu_button';
+
+export default {
+  name: 'DtRecipeSettingsMenuButtonDefault',
+  components: { DtRecipeSettingsMenuButton },
+};
+</script>
