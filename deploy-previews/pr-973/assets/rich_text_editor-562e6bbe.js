375497392affa084b823b59ae8b2ef4139374a73
braddialpad <braddialpad@users.noreply.github.com>
2023-05-10 21:18:23 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@3f7e04c83083146890dc22b20319375266dc16d5 🚀

---
+++ deploy-previews/pr-973/assets/rich_text_editor-562e6bbe.js
@@ -0,0 +1,24 @@
+import{j as t}from"./jsx-runtime-d2781c9b.js";import{M as r,S as a,C as c,a as l,b as h}from"./index-3dd5cdad.js";import{R as d,D as p}from"./rich_text_editor.stories-8b344477.js";import{u as i}from"./index-7524e354.js";import"./iframe-9cbb73c1.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-042e6b4d.js";import"./chunk-4NMOSTKD-938b085e.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-ac12cf05.js";import"./index-356e4a49.js";import"./chunk-OPEUWD42-a3b45fd8.js";import"./storybook_utils-35e0d11d.js";import"./vue.esm-bundler-2e3b72e0.js";import"./utils-9325de8f.js";import"./tippy.esm-65135068.js";import"./_plugin-vue_export-helper-c27b6911.js";function O(n={}){const{wrapper:o}=Object.assign({},i(),n.components);return o?t.jsx(o,Object.assign({},n,{children:t.jsx(s,{})})):s();function s(){const e=Object.assign({h1:"h1",p:"p",h2:"h2",h3:"h3",pre:"pre",code:"code"},i(),n.components);return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:d}),`
+`,t.jsx(e.h1,{id:"rich-text-editor",children:"Rich Text Editor"}),`
+`,t.jsx(a,{children:t.jsx(e.p,{children:`A rich text editor is a component that allows users to format text with a
+variety of styles. It enables also mixing text with custom components built as
+extensions, such as links and user mentions.`})}),`
+`,t.jsx("div",{class:"d-notice d-notice--info d-wmx100p",children:t.jsxs(e.p,{children:[t.jsx("b",{children:"NOTICE:"}),` This component is not a part of Dialtone-Vue core package and can
+only be used by other Dialtone components. To keep this code out of the
+Dialtone-vue core any Dialtone component that uses this should be built into
+their separate build targets.`]})}),`
+`,t.jsx(e.h2,{id:"base-style",children:"Base Style"}),`
+`,t.jsx(e.p,{children:`The editor itself is without any styling and the intention is to wrap it with
+another component, such as Message Input, that provides the UI.`}),`
+`,t.jsx(c,{children:t.jsx(l,{of:p})}),`
+`,t.jsx(e.h2,{id:"slots-props-and-events",children:"Slots, Props and Events"}),`
+`,t.jsx(h,{}),`
+`,t.jsx(e.h3,{id:"usage-internal-only",children:"Usage (internal only)"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-js",children:`import { DtRichTextEditor } from '@components/rich_text_editor';
+`})}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-jsx",children:`<dt-rich-text-editor
+  :v-model="text"
+  :input-aria-label="ariaLabel"
+/>
+`})})]})}}export{O as default};
+//# sourceMappingURL=rich_text_editor-562e6bbe.js.map
