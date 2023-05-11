af5467695d7af6d3c13d40eb333e9cd2fabb4d89
AlexLamDialpad <AlexLamDialpad@users.noreply.github.com>
2023-05-11 22:49:01 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@448adc1f56058673fdb795adbfef96f7549bbd59 🚀

---
+++ deploy-previews/pr-976/assets/time_pill-7323ba8f.js
@@ -0,0 +1,22 @@
+import{j as e}from"./jsx-runtime-679e915b.js";import{M as o,S as a,C as l,a as d,b as p}from"./index-74ebd2f6.js";import{T as m,D as c}from"./time_pill.stories-ec198044.js";import{u as r}from"./index-88e2cb5d.js";import"./iframe-c0f9e7d0.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-042e6b4d.js";import"./chunk-4NMOSTKD-938b085e.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-ac12cf05.js";import"./index-356e4a49.js";import"./storybook_utils-b781ced2.js";import"./vue.esm-eff6e72e.js";import"./_plugin-vue2_normalizer-2bbd088e.js";function E(i={}){const{wrapper:n}=Object.assign({},r(),i.components);return n?e.jsx(n,Object.assign({},i,{children:e.jsx(t,{})})):t();function t(){const s=Object.assign({h1:"h1",p:"p",h2:"h2",ul:"ul",li:"li",h3:"h3",pre:"pre",code:"code"},r(),i.components);return e.jsxs(e.Fragment,{children:[e.jsx(o,{of:m}),`
+`,e.jsx(s.h1,{id:"time-pill",children:"Time Pill"}),`
+`,e.jsx(a,{children:e.jsx(s.p,{children:"Used to divide feed items that are from different dates."})}),`
+`,e.jsx(s.h2,{id:"base-style",children:"Base Style"}),`
+`,e.jsx(s.p,{children:"Pass in a display value as dateTimeDisplay, and the actual date into dateTime."}),`
+`,e.jsxs(s.p,{children:[e.jsx("b",{children:"Examples"}),":"]}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsxs(s.li,{children:[`
+`,e.jsx(s.p,{children:"dateTimeDisplay: 'Today', 'Yesterday', 'Monday, September 13'"}),`
+`]}),`
+`,e.jsxs(s.li,{children:[`
+`,e.jsx(s.p,{children:"dateTime: '2022-09-13'"}),`
+`]}),`
+`]}),`
+`,e.jsx(l,{children:e.jsx(d,{of:c})}),`
+`,e.jsx(s.h2,{id:"slots-props--events",children:"Slots, Props & Events"}),`
+`,e.jsx(p,{}),`
+`,e.jsx(s.h2,{id:"usage",children:"Usage"}),`
+`,e.jsx(s.h3,{id:"import",children:"Import"}),`
+`,e.jsx(s.pre,{children:e.jsx(s.code,{className:"language-jsx",children:`import { DtRecipeTimePill } from '@dialpad/dialtone-vue';
+`})})]})}}export{E as default};
+//# sourceMappingURL=time_pill-7323ba8f.js.map
