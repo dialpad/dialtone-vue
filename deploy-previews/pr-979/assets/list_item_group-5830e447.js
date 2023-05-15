94df5a1ad2224f22cf24d72271d6ec21f6e6ec00
juliodialpad <juliodialpad@users.noreply.github.com>
2023-05-15 20:35:26 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@4efd6c05bbbbf1141af5adc8685f08d8d142492d 🚀

---
+++ deploy-previews/pr-979/assets/list_item_group-5830e447.js
@@ -0,0 +1,43 @@
+import{j as t}from"./jsx-runtime-7070528a.js";import{M as r,S as a,C as l,a as p,b as d}from"./index-9adc384e.js";import{L as m,D as h}from"./list_item_group.stories-344390f2.js";import{u as o}from"./index-6d05fad9.js";import"./iframe-c6ff29a4.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-042e6b4d.js";import"./chunk-4NMOSTKD-938b085e.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-ac12cf05.js";import"./index-356e4a49.js";import"./storybook_utils-58c64c5e.js";import"./vue.esm-eff6e72e.js";import"./list_item_group-8f35d4c7.js";import"./_plugin-vue2_normalizer-2bbd088e.js";import"./list_item-28076742.js";import"./icon-525487a4.js";import"./icon_constants-0085b0a1.js";function E(i={}){const{wrapper:n}=Object.assign({},o(),i.components);return n?t.jsx(n,Object.assign({},i,{children:t.jsx(s,{})})):s();function s(){const e=Object.assign({h1:"h1",p:"p",h2:"h2",h3:"h3",pre:"pre",code:"code"},o(),i.components);return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:m}),`
+`,t.jsx(e.h1,{id:"list-item-group",children:"List Item Group"}),`
+`,t.jsx(a,{children:t.jsx(e.p,{children:'The "List Item Group" component uses a non interactive heading which groups list items.'})}),`
+`,t.jsx(e.h2,{id:"base-style",children:"Base Style"}),`
+`,t.jsx(e.p,{children:`You should use this component when you have multiple list items you would like to group into different
+categories.`}),`
+`,t.jsx(e.p,{children:`The heading is unstyled by default. You will likely have to pass utility classes to the headingClass prop to make the
+heading look how you wish.`}),`
+`,t.jsx(l,{children:t.jsx(p,{of:h})}),`
+`,t.jsx(e.h2,{id:"slots-props--events",children:"Slots, Props & Events"}),`
+`,t.jsx(d,{}),`
+`,t.jsx(e.h2,{id:"usage",children:"Usage"}),`
+`,t.jsx(e.h3,{id:"import",children:"Import"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-jsx",children:`import { DtListItemGroup } from '@dialpad/dialtone-vue';
+`})}),`
+`,t.jsx(e.h3,{id:"usage-1",children:"Usage"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-jsx",children:`<dt-list-item-group
+  heading="Example Heading"
+  headingClass="d-fw-bold"
+>
+  <dt-list-item
+    navigation-type="tab"
+  >
+    item1
+  </dt-list-item>
+  <dt-list-item
+    navigation-type="tab"
+  >
+    item2
+  </dt-list-item>
+  <dt-list-item
+    navigation-type="tab"
+  >
+    item3
+  </dt-list-item>
+</dt-list-item-group>
+
+`})}),`
+`,t.jsx(e.h2,{id:"accessibility",children:"Accessibility"}),`
+`,t.jsx(e.p,{children:`The List Item Group does not implement arrow-keys keyboard navigation. You will however get arrow-keys
+keyboard navigation when using this within list based Dialtone components such as Dropdown or Combobox.`}),`
+`,t.jsx(e.p,{children:"The aria label for the List Item Group will be set by the content of the heading."})]})}}export{E as default};
+//# sourceMappingURL=list_item_group-5830e447.js.map
---
+++ deploy-previews/pr-979/assets/toast-39083b67.js
@@ -0,0 +1,8 @@
+import{j as t}from"./jsx-runtime-7070528a.js";import"./chunk-PCJTTTQV-22541bcb.js";import{M as e,C as s,a as i}from"./index-9adc384e.js";import{u as a}from"./index-6d05fad9.js";import"./iframe-c6ff29a4.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-042e6b4d.js";import"./chunk-4NMOSTKD-938b085e.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-ac12cf05.js";import"./index-356e4a49.js";function M(n={}){const{wrapper:r}=Object.assign({},a(),n.components);return r?t.jsx(r,Object.assign({},n,{children:t.jsx(o,{})})):o();function o(){return t.jsxs(t.Fragment,{children:[t.jsx(e,{title:"Visual Testing/Toast"}),`
+`,t.jsx(s,{children:t.jsx(i,{id:"visual-testing-toast--default"})}),`
+`,t.jsx(s,{children:t.jsx(i,{id:"visual-testing-toast--error"})}),`
+`,t.jsx(s,{children:t.jsx(i,{id:"visual-testing-toast--info"})}),`
+`,t.jsx(s,{children:t.jsx(i,{id:"visual-testing-toast--success"})}),`
+`,t.jsx(s,{children:t.jsx(i,{id:"visual-testing-toast--warning"})}),`
+`,t.jsx(s,{children:t.jsx(i,{id:"visual-testing-toast--important"})})]})}}export{M as default};
+//# sourceMappingURL=toast-39083b67.js.map
