33d330a959a9d3053282589fa712ce0d3f1f37c6
braddialpad <braddialpad@users.noreply.github.com>
2023-05-17 21:50:26 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@d7ef3ffe7a2d1d60d38b9c8f3d8884abcdf267f1 🚀

---
+++ deploy-previews/pr-988/assets/DocsRenderer-EYKKDMVH-2924abc6.js
@@ -0,0 +1,2 @@
+import{D as f,d as D}from"./chunk-PCJTTTQV-37adf1a9.js";import"./iframe-8a141fe9.js";import"../sb-preview/runtime.js";import"./index-4a8f232a.js";import"./_commonjsHelpers-042e6b4d.js";import"./chunk-4NMOSTKD-938b085e.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-ac12cf05.js";import"./index-356e4a49.js";export{f as DocsRenderer,D as defaultComponents};
+//# sourceMappingURL=DocsRenderer-EYKKDMVH-2924abc6.js.map
---
+++ deploy-previews/pr-988/assets/rich_text_editor-94082ae7.js
@@ -0,0 +1,24 @@
+import{j as e}from"./jsx-dev-runtime-f11fdc8c.js";import{M as o,S as r,C as l,a as u,b as m}from"./index-4a8f232a.js";import{R as a,D as c}from"./rich_text_editor.stories-423f2cd3.js";import{u as t}from"./index-81fd438a.js";import"./iframe-8a141fe9.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-042e6b4d.js";import"./chunk-4NMOSTKD-938b085e.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-ac12cf05.js";import"./index-356e4a49.js";import"./chunk-OPEUWD42-a3b45fd8.js";import"./storybook_utils-ed5693eb.js";import"./vue.esm-eff6e72e.js";import"./_plugin-vue2_normalizer-2bbd088e.js";function i(s){const n=Object.assign({h1:"h1",p:"p",h2:"h2",h3:"h3",pre:"pre",code:"code"},t(),s.components);return e.jsxDEV(e.Fragment,{children:[e.jsxDEV(o,{of:a},void 0,!1,{fileName:"<source.js>",lineNumber:5,columnNumber:1},this),`
+`,e.jsxDEV(n.h1,{id:"rich-text-editor",children:"Rich Text Editor"},void 0,!1,{fileName:"<source.js>",lineNumber:7,columnNumber:1},this),`
+`,e.jsxDEV(r,{children:e.jsxDEV(n.p,{children:`A rich text editor is a component that allows users to format text with a
+variety of styles. It enables also mixing text with custom components built as
+extensions, such as links and user mentions.`},void 0,!1,{fileName:"<source.js>",lineNumber:10,columnNumber:3},this)},void 0,!1,{fileName:"<source.js>",lineNumber:9,columnNumber:1},this),`
+`,e.jsxDEV("div",{class:"d-notice d-notice--info d-wmx100p",children:e.jsxDEV(n.p,{children:[e.jsxDEV("b",{children:"NOTICE:"},void 0,!1,{fileName:"<source.js>",lineNumber:16,columnNumber:1},this),` This component is not a part of Dialtone-Vue core package and can
+only be used by other Dialtone components. To keep this code out of the
+Dialtone-vue core any Dialtone component that uses this should be built into
+their separate build targets.`]},void 0,!0,{fileName:"<source.js>",lineNumber:16,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:15,columnNumber:1},this),`
+`,e.jsxDEV(n.h2,{id:"base-style",children:"Base Style"},void 0,!1,{fileName:"<source.js>",lineNumber:22,columnNumber:1},this),`
+`,e.jsxDEV(n.p,{children:`The editor itself is without any styling and the intention is to wrap it with
+another component, such as Message Input, that provides the UI.`},void 0,!1,{fileName:"<source.js>",lineNumber:24,columnNumber:1},this),`
+`,e.jsxDEV(l,{children:e.jsxDEV(u,{of:c},void 0,!1,{fileName:"<source.js>",lineNumber:28,columnNumber:3},this)},void 0,!1,{fileName:"<source.js>",lineNumber:27,columnNumber:1},this),`
+`,e.jsxDEV(n.h2,{id:"slots-props-and-events",children:"Slots, Props and Events"},void 0,!1,{fileName:"<source.js>",lineNumber:31,columnNumber:1},this),`
+`,e.jsxDEV(m,{},void 0,!1,{fileName:"<source.js>",lineNumber:33,columnNumber:1},this),`
+`,e.jsxDEV(n.h3,{id:"usage-internal-only",children:"Usage (internal only)"},void 0,!1,{fileName:"<source.js>",lineNumber:35,columnNumber:1},this),`
+`,e.jsxDEV(n.pre,{children:e.jsxDEV(n.code,{className:"language-js",children:`import { DtRichTextEditor } from '@components/rich_text_editor';
+`},void 0,!1,{fileName:"<source.js>",lineNumber:37,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:37,columnNumber:1},this),`
+`,e.jsxDEV(n.pre,{children:e.jsxDEV(n.code,{className:"language-jsx",children:`<dt-rich-text-editor
+  :v-model="text"
+  :input-aria-label="ariaLabel"
+/>
+`},void 0,!1,{fileName:"<source.js>",lineNumber:41,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:41,columnNumber:1},this)]},void 0,!0,{fileName:"<source.js>",lineNumber:1,columnNumber:1},this)}function M(s={}){const{wrapper:n}=Object.assign({},t(),s.components);return n?e.jsxDEV(n,Object.assign({},s,{children:e.jsxDEV(i,s,void 0,!1,{fileName:"<source.js>"},this)}),void 0,!1,{fileName:"<source.js>"},this):i(s)}export{M as default};
+//# sourceMappingURL=rich_text_editor-94082ae7.js.map
