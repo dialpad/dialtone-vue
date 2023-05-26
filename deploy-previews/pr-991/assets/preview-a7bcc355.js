2df1d150e987541b9bcc7936811d2b7be3695cb7
MikaDialpad <MikaDialpad@users.noreply.github.com>
2023-05-26 17:53:21 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@dabe249cf60e8edccc825f180277b615d56b5073 🚀

---
+++ deploy-previews/pr-991/assets/preview-a7bcc355.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-a6b03025.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-7ab88831.js"),["./DocsRenderer-EYKKDMVH-7ab88831.js","./chunk-PCJTTTQV-406ff403.js","./iframe-a6b03025.js","./index-f0440b7c.js","./_commonjsHelpers-725317a4.js","./chunk-4NMOSTKD-938b085e.js","./index-d475d2ea.js","./index-d37d4223.js","./index-aefc0af2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-a7bcc355.js.map
