6161df708b0c5073ce20092519057e0cba3c0549
juliodialpad <juliodialpad@users.noreply.github.com>
2023-06-08 00:34:20 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@eb2316d0573542255576d25f2c4079a3dfefd971 🚀

---
+++ deploy-previews/pr-1009/assets/group_row-075510aa.js
@@ -0,0 +1,31 @@
+import{j as e}from"./jsx-dev-runtime-fe73fd75.js";import{M as l,S as o,C as s,a as r,b as t}from"./index-b1d9b161.js";import{G as a,D as c,V as d}from"./group_row.stories-00a7ad26.js";import{u as m}from"./index-fa7871fd.js";import"./iframe-1aae5dc6.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./chunk-4NMOSTKD-938b085e.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-aefc0af2.js";import"./index-356e4a49.js";import"./chunk-OPEUWD42-a3b45fd8.js";import"./storybook_utils-be1af372.js";import"./vue.esm-eff6e72e.js";import"./general_row-6efbd758.js";import"./badge-cc939aa3.js";import"./icon-49f0abb3.js";import"./icon_constants-7577383f.js";import"./_plugin-vue2_normalizer-2bbd088e.js";import"./button-9102a84a.js";import"./link_constants-51338c78.js";import"./tooltip-0195acf0.js";import"./tippy_utils-e4c34e91.js";import"./lazy_show-0b3a0cf0.js";import"./emoji_text_wrapper-faad1d6a.js";import"./emoji-64227f3f.js";import"./emoji-1f0f2b27.js";import"./skeleton-28563e4f.js";import"./stack-dfcd045d.js";import"./person-13c0d6d2.js";function u(i){const n=Object.assign({h1:"h1",p:"p",h2:"h2",h3:"h3",pre:"pre",code:"code"},m(),i.components);return e.jsxDEV(e.Fragment,{children:[e.jsxDEV(l,{of:a},void 0,!1,{fileName:"<source.js>",lineNumber:5,columnNumber:1},this),`
+`,e.jsxDEV(n.h1,{id:"leftbar-group-row",children:"Leftbar Group Row"},void 0,!1,{fileName:"<source.js>",lineNumber:7,columnNumber:1},this),`
+`,e.jsxDEV(o,{children:e.jsxDEV(n.p,{children:"This component covers the group leftbar rows."},void 0,!1,{fileName:"<source.js>",lineNumber:10,columnNumber:3},this)},void 0,!1,{fileName:"<source.js>",lineNumber:9,columnNumber:1},this),`
+`,e.jsxDEV(n.h2,{id:"base-style",children:"Base Style"},void 0,!1,{fileName:"<source.js>",lineNumber:13,columnNumber:1},this),`
+`,e.jsxDEV(s,{children:e.jsxDEV(r,{of:c},void 0,!1,{fileName:"<source.js>",lineNumber:16,columnNumber:3},this)},void 0,!1,{fileName:"<source.js>",lineNumber:15,columnNumber:1},this),`
+`,e.jsxDEV(n.h2,{id:"variants",children:"Variants"},void 0,!1,{fileName:"<source.js>",lineNumber:19,columnNumber:1},this),`
+`,e.jsxDEV(s,{children:e.jsxDEV(r,{of:d},void 0,!1,{fileName:"<source.js>",lineNumber:22,columnNumber:3},this)},void 0,!1,{fileName:"<source.js>",lineNumber:21,columnNumber:1},this),`
+`,e.jsxDEV(n.h2,{id:"slots-props--events",children:"Slots, Props & Events"},void 0,!1,{fileName:"<source.js>",lineNumber:25,columnNumber:1},this),`
+`,e.jsxDEV(t,{},void 0,!1,{fileName:"<source.js>",lineNumber:27,columnNumber:1},this),`
+`,e.jsxDEV(n.h2,{id:"usage",children:"Usage"},void 0,!1,{fileName:"<source.js>",lineNumber:29,columnNumber:1},this),`
+`,e.jsxDEV(n.h3,{id:"import",children:"Import"},void 0,!1,{fileName:"<source.js>",lineNumber:31,columnNumber:1},this),`
+`,e.jsxDEV(n.pre,{children:e.jsxDEV(n.code,{className:"language-jsx",children:`import { DtRecipeGroupRow } from '@dialpad/dialtone-vue';
+`},void 0,!1,{fileName:"<source.js>",lineNumber:33,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:33,columnNumber:1},this),`
+`,e.jsxDEV(n.h3,{id:"default-behavior",children:"Default behavior"},void 0,!1,{fileName:"<source.js>",lineNumber:37,columnNumber:1},this),`
+`,e.jsxDEV(n.pre,{children:e.jsxDEV(n.code,{className:"language-html",children:`<dt-recipe-group-row
+  names="Jaqueline Nackos, Lori Smith"
+/>
+`},void 0,!1,{fileName:"<source.js>",lineNumber:39,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:39,columnNumber:1},this),`
+`,e.jsxDEV(n.h3,{id:"selected",children:"Selected"},void 0,!1,{fileName:"<source.js>",lineNumber:45,columnNumber:1},this),`
+`,e.jsxDEV(n.pre,{children:e.jsxDEV(n.code,{className:"language-html",children:`<dt-recipe-group-row
+  names="Jaqueline Nackos, Lori Smith"
+  :selected="true"
+/>
+`},void 0,!1,{fileName:"<source.js>",lineNumber:47,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:47,columnNumber:1},this),`
+`,e.jsxDEV(n.h3,{id:"with-unread-count",children:"With Unread Count"},void 0,!1,{fileName:"<source.js>",lineNumber:54,columnNumber:1},this),`
+`,e.jsxDEV(n.pre,{children:e.jsxDEV(n.code,{className:"language-html",children:`<dt-recipe-group-row
+  names="Jaqueline Nackos, Lori Smith"
+  :unread-count="10"
+/>
+`},void 0,!1,{fileName:"<source.js>",lineNumber:56,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:56,columnNumber:1},this)]},void 0,!0,{fileName:"<source.js>",lineNumber:1,columnNumber:1},this)}function T(i={}){const{wrapper:n}=Object.assign({},m(),i.components);return n?e.jsxDEV(n,Object.assign({},i,{children:e.jsxDEV(u,i,void 0,!1,{fileName:"<source.js>"},this)}),void 0,!1,{fileName:"<source.js>"},this):u(i)}export{T as default};
+//# sourceMappingURL=group_row-075510aa.js.map
---
+++ deploy-previews/pr-1009/assets/preview-7c23346c.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-1aae5dc6.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-22c3bfac.js"),["./DocsRenderer-EYKKDMVH-22c3bfac.js","./chunk-PCJTTTQV-a3bdd2a0.js","./iframe-1aae5dc6.js","./index-b1d9b161.js","./_commonjsHelpers-725317a4.js","./chunk-4NMOSTKD-938b085e.js","./index-d475d2ea.js","./index-d37d4223.js","./index-aefc0af2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-7c23346c.js.map
