3ab5925c4e73ad4efd48db3f1f860f0ca3da489e
juliodialpad <juliodialpad@users.noreply.github.com>
2023-06-08 20:36:47 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@59320bde88544eb80ecd8ea7d42ec11f440f6c49 🚀

---
+++ deploy-previews/pr-1011/assets/WithTooltip-FBT32F6Q-71336f98.js
@@ -0,0 +1,2 @@
+import{j as l,j as s,k as W}from"./index-630bbe6e.js";import"./iframe-3e08d24c.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./chunk-4NMOSTKD-938b085e.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-aefc0af2.js";import"./index-356e4a49.js";export{l as WithToolTipState,s as WithTooltip,W as WithTooltipPure};
+//# sourceMappingURL=WithTooltip-FBT32F6Q-71336f98.js.map
---
+++ deploy-previews/pr-1011/assets/pr_template-eea81417.js
@@ -0,0 +1,54 @@
+import{j as e}from"./jsx-dev-runtime-6f571cdf.js";import{M as r}from"./index-630bbe6e.js";import{u as a}from"./index-0dcbf45c.js";import"./iframe-3e08d24c.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./chunk-4NMOSTKD-938b085e.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-aefc0af2.js";import"./index-356e4a49.js";function s(n){const t=Object.assign({h1:"h1",pre:"pre",code:"code"},a(),n.components);return e.jsxDEV(e.Fragment,{children:[e.jsxDEV(r,{title:"Docs/Templates/Pull Request"},void 0,!1,{fileName:"<source.js>",lineNumber:3,columnNumber:1},this),`
+`,e.jsxDEV(t.h1,{id:"pull-request-template",children:"Pull Request Template"},void 0,!1,{fileName:"<source.js>",lineNumber:5,columnNumber:1},this),`
+`,e.jsxDEV(t.pre,{children:e.jsxDEV(t.code,{className:"language-md",children:`# PR Title
+
+<!--- Feel free to remove any unused sections -->
+
+## :hammer_and_wrench: Type Of Change
+
+<!--- Tick or place an \`x\` in all of the checkboxes that apply -->
+
+- [ ] Fix
+- [ ] Feature
+- [ ] Refactoring
+- [ ] Documentation
+
+## :book: Description
+
+<!--- Describe the changes -->
+
+## :bulb: Context
+
+<!--- Describe the purpose of the changes -->
+<!--- Why did we make these changes? -->
+<!--- What problem(s) do they solve? -->
+
+## :pencil: Checklist
+
+<!--- Tick or place an \`x\` in all of the checkboxes that apply -->
+<!--- Remove checkboxes that do not apply -->
+
+- [ ] I have updated library exports
+- [ ] I have reviewed my changes
+- [ ] I have added tests
+- [ ] I have added all relevant documentation
+- [ ] All tests are passing
+- [ ] All linters are passing
+- [ ] No accessibility issues reported
+- [ ] I have validated components with a screen reader
+- [ ] I have validated components keyboard navigation
+
+## :crystal_ball: Next Steps
+
+<!--- Describe any future changes that need to be made after merging the PR -->
+
+## :camera: Screenshots / GIFs
+
+<!--- Mandatory for any UI work -->
+<!--- Link any screenshots / GIFs below -->
+
+## :link: Sources
+
+<!--- Add any links to external reference material -->
+`},void 0,!1,{fileName:"<source.js>",lineNumber:7,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:7,columnNumber:1},this)]},void 0,!0,{fileName:"<source.js>",lineNumber:1,columnNumber:1},this)}function x(n={}){const{wrapper:t}=Object.assign({},a(),n.components);return t?e.jsxDEV(t,Object.assign({},n,{children:e.jsxDEV(s,n,void 0,!1,{fileName:"<source.js>"},this)}),void 0,!1,{fileName:"<source.js>"},this):s(n)}export{x as default};
+//# sourceMappingURL=pr_template-eea81417.js.map
