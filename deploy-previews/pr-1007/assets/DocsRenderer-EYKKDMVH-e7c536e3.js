77c943cc439830f303df726f24135202c58e3342
iropolo <iropolo@users.noreply.github.com>
2023-06-09 18:20:15 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@cf412424b792d6b1fb2b85c30a7d4b64eacffdd0 🚀

---
+++ deploy-previews/pr-1007/assets/DocsRenderer-EYKKDMVH-e7c536e3.js
@@ -0,0 +1,2 @@
+import{D as f,d as D}from"./chunk-PCJTTTQV-86cc8f8d.js";import"./iframe-b636b7b4.js";import"../sb-preview/runtime.js";import"./index-f3e70ef8.js";import"./_commonjsHelpers-725317a4.js";import"./chunk-4NMOSTKD-938b085e.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-aefc0af2.js";import"./index-356e4a49.js";export{f as DocsRenderer,D as defaultComponents};
+//# sourceMappingURL=DocsRenderer-EYKKDMVH-e7c536e3.js.map
---
+++ deploy-previews/pr-1007/assets/sfc_component_story_template-b6d721a0.js
@@ -0,0 +1,42 @@
+import{j as e}from"./jsx-dev-runtime-f7d8885d.js";import{M as r}from"./index-f3e70ef8.js";import{u as s}from"./index-d76e3c15.js";import"./iframe-b636b7b4.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./chunk-4NMOSTKD-938b085e.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-aefc0af2.js";import"./index-356e4a49.js";function o(n){const t=Object.assign({h1:"h1",pre:"pre",code:"code"},s(),n.components);return e.jsxDEV(e.Fragment,{children:[e.jsxDEV(r,{title:"Docs/Templates/SFC Component Story Template"},void 0,!1,{fileName:"<source.js>",lineNumber:3,columnNumber:1},this),`
+`,e.jsxDEV(t.h1,{id:"sfc-component-story-template",children:"SFC Component Story Template"},void 0,!1,{fileName:"<source.js>",lineNumber:5,columnNumber:1},this),`
+`,e.jsxDEV(t.pre,{children:e.jsxDEV(t.code,{className:"language-html",children:`<!-- Use this template story to allow the user control the component's props and slots -->
+<template>
+  <!--
+    We can bind the data that the user entered into the storybook controls to props by using a property of the same name
+    as the storybook control defined in the corresponding \`.story.js\` file.
+  -->
+  <dt-my-component
+    :my-prop="nameOfStorybookControl"
+  >
+    <!--
+      We can also bind any slot data that the user has entered into the storybook controls. In this example we
+      conditionally render slots using a custom storybook control defined in the corresponding \`.story.js\`.
+
+      The preferred naming scheme for storybook slot controls uses the following format \`<SLOT_NAME>Slot\`.
+
+      We use this storybook control naming scheme to prevent conflicts between controls for props and slots with the
+      same name.
+    -->
+    <template v-if="defaultSlot">
+      <span v-html="defaultSlot" />
+    </template>
+    <template
+      #some
+      v-if="someSlot"
+    >
+      <span v-html="someSlot" />
+    </template>
+  </dt-my-component>
+</template>
+
+<script>
+import DtMyComponent from './my_component.vue';
+
+export default {
+  name: 'MyComponentDefault',
+  components: { DtMyComponent },
+};
+<\/script>
+`},void 0,!1,{fileName:"<source.js>",lineNumber:7,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:7,columnNumber:1},this)]},void 0,!0,{fileName:"<source.js>",lineNumber:1,columnNumber:1},this)}function b(n={}){const{wrapper:t}=Object.assign({},s(),n.components);return t?e.jsxDEV(t,Object.assign({},n,{children:e.jsxDEV(o,n,void 0,!1,{fileName:"<source.js>"},this)}),void 0,!1,{fileName:"<source.js>"},this):o(n)}export{b as default};
+//# sourceMappingURL=sfc_component_story_template-b6d721a0.js.map
