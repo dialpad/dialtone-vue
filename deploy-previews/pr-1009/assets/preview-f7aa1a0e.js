2305ad37eb2f2f3f6095c027ba3bacc69e8eb764
juliodialpad <juliodialpad@users.noreply.github.com>
2023-06-09 17:52:28 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@9825917034391119f01f647382e3ed7379dbad58 🚀

--- deploy-previews/pr-1009/assets/preview-f7aa1a0e.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-b522ff02.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-dacd5e23.js"),["./DocsRenderer-EYKKDMVH-dacd5e23.js","./chunk-PCJTTTQV-6c63515e.js","./iframe-b522ff02.js","./index-4f59aaa9.js","./_commonjsHelpers-725317a4.js","./chunk-4NMOSTKD-938b085e.js","./index-d475d2ea.js","./index-d37d4223.js","./index-aefc0af2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-f7aa1a0e.js.map
---
+++ deploy-previews/pr-1009/assets/tooltip-fb4ba974.js
@@ -0,0 +1,100 @@
+import{j as e}from"./jsx-dev-runtime-f34c85b6.js";import{M as o,S as u,C as s,a as l,b as a}from"./index-b52af8fb.js";import"./tooltip-0195acf0.js";import{T as m,D as d,V as c,F as f}from"./tooltip.stories-09ef77d7.js";import{u as t}from"./index-770f9633.js";import"./iframe-412cafee.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./chunk-4NMOSTKD-938b085e.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-aefc0af2.js";import"./index-356e4a49.js";import"./tippy_utils-e4c34e91.js";import"./storybook_utils-be1af372.js";import"./vue.esm-eff6e72e.js";import"./lazy_show-0b3a0cf0.js";import"./_plugin-vue2_normalizer-2bbd088e.js";import"./button-9102a84a.js";import"./link_constants-51338c78.js";import"./chunk-OPEUWD42-a3b45fd8.js";function r(i){const n=Object.assign({h1:"h1",p:"p",h2:"h2",ol:"ol",li:"li",strong:"strong",a:"a",h3:"h3",code:"code",pre:"pre"},t(),i.components);return e.jsxDEV(e.Fragment,{children:[e.jsxDEV(o,{of:m},void 0,!1,{fileName:"<source.js>",lineNumber:6,columnNumber:1},this),`
+`,e.jsxDEV(n.h1,{id:"tooltip",children:"Tooltip"},void 0,!1,{fileName:"<source.js>",lineNumber:8,columnNumber:1},this),`
+`,e.jsxDEV(u,{children:e.jsxDEV(n.p,{children:`A tooltip is a floating label that briefly explains an action, function, or an element.
+Its content is exclusively text and shouldn't be vital information for users.
+If richer media is desired, consider using a popover instead.`},void 0,!1,{fileName:"<source.js>",lineNumber:11,columnNumber:5},this)},void 0,!1,{fileName:"<source.js>",lineNumber:10,columnNumber:1},this),`
+`,e.jsxDEV(n.h2,{id:"default",children:"Default"},void 0,!1,{fileName:"<source.js>",lineNumber:16,columnNumber:1},this),`
+`,e.jsxDEV(n.p,{children:`The tooltip, also known as infotip or hint, is a common graphical user interface element in which, when hovering over a
+screen element or component, a text box displays information about that element (such as a description of a button's
+function, or what an abbreviation stands for). The tooltip is displayed continuously as long as the user hovers over the
+element`},void 0,!1,{fileName:"<source.js>",lineNumber:18,columnNumber:1},this),`
+`,e.jsxDEV(n.p,{children:"A tooltip has two slots:"},void 0,!1,{fileName:"<source.js>",lineNumber:23,columnNumber:1},this),`
+`,e.jsxDEV(n.ol,{children:[`
+`,e.jsxDEV(n.li,{children:[e.jsxDEV(n.strong,{children:"the anchor"},void 0,!1,{fileName:"<source.js>",lineNumber:25,columnNumber:4},this)," required slot"]},void 0,!0,{fileName:"<source.js>",lineNumber:25,columnNumber:1},this),`
+`,e.jsxDEV(n.li,{children:[e.jsxDEV(n.strong,{children:"the default"},void 0,!1,{fileName:"<source.js>",lineNumber:26,columnNumber:4},this)," slot (which could be replaced with prop message)"]},void 0,!0,{fileName:"<source.js>",lineNumber:26,columnNumber:1},this),`
+`]},void 0,!0,{fileName:"<source.js>",lineNumber:25,columnNumber:1},this),`
+`,e.jsxDEV(s,{children:e.jsxDEV(l,{of:d},void 0,!1,{fileName:"<source.js>",lineNumber:29,columnNumber:5},this)},void 0,!1,{fileName:"<source.js>",lineNumber:28,columnNumber:1},this),`
+`,e.jsxDEV(n.h2,{id:"variants",children:"Variants"},void 0,!1,{fileName:"<source.js>",lineNumber:32,columnNumber:1},this),`
+`,e.jsxDEV(s,{children:e.jsxDEV(l,{of:c},void 0,!1,{fileName:"<source.js>",lineNumber:35,columnNumber:5},this)},void 0,!1,{fileName:"<source.js>",lineNumber:34,columnNumber:1},this),`
+`,e.jsxDEV(n.h2,{id:"flip",children:"Flip"},void 0,!1,{fileName:"<source.js>",lineNumber:38,columnNumber:1},this),`
+`,e.jsxDEV(s,{children:e.jsxDEV(l,{of:f},void 0,!1,{fileName:"<source.js>",lineNumber:41,columnNumber:5},this)},void 0,!1,{fileName:"<source.js>",lineNumber:40,columnNumber:1},this),`
+`,e.jsxDEV(n.h2,{id:"props--slots",children:"Props & Slots"},void 0,!1,{fileName:"<source.js>",lineNumber:44,columnNumber:1},this),`
+`,e.jsxDEV(a,{},void 0,!1,{fileName:"<source.js>",lineNumber:46,columnNumber:1},this),`
+`,e.jsxDEV(n.h2,{id:"accessibility",children:"Accessibility"},void 0,!1,{fileName:"<source.js>",lineNumber:48,columnNumber:1},this),`
+`,e.jsxDEV(n.p,{children:[`Reads out the tooltip content as a supplementary description for its trigger when the trigger is focused.
+See also `,e.jsxDEV(n.a,{href:"https://www.w3.org/TR/wai-aria-practices-1.1/#tooltip",target:"_blank",rel:"nofollow noopener noreferrer",children:"wai aria practices 1.1"},void 0,!1,{fileName:"<source.js>",lineNumber:51,columnNumber:10},this),"."]},void 0,!0,{fileName:"<source.js>",lineNumber:50,columnNumber:1},this),`
+`,e.jsxDEV(n.h3,{id:"general-info",children:"General info"},void 0,!1,{fileName:"<source.js>",lineNumber:53,columnNumber:1},this),`
+`,e.jsxDEV(n.ol,{children:[`
+`,e.jsxDEV(n.li,{children:[e.jsxDEV(n.strong,{children:"Twelve directions are offered:"},void 0,!1,{fileName:"<source.js>",lineNumber:55,columnNumber:4},this)," three on each face of the tooltip."]},void 0,!0,{fileName:"<source.js>",lineNumber:55,columnNumber:1},this),`
+`,e.jsxDEV(n.li,{children:[e.jsxDEV(n.strong,{children:"Inverted:"},void 0,!1,{fileName:"<source.js>",lineNumber:56,columnNumber:4},this)," Inverts styles to work on dark backgrounds."]},void 0,!0,{fileName:"<source.js>",lineNumber:56,columnNumber:1},this),`
+`]},void 0,!0,{fileName:"<source.js>",lineNumber:55,columnNumber:1},this),`
+`,e.jsxDEV(n.h3,{id:"anchor",children:"Anchor"},void 0,!1,{fileName:"<source.js>",lineNumber:58,columnNumber:1},this),`
+`,e.jsxDEV(n.p,{children:[`The anchor element that activates the tooltip should be fully accessible by keyboard. The easiest way to do this is by
+using an element like an `,e.jsxDEV(n.code,{children:"DtButton"},void 0,!1,{fileName:"<source.js>",lineNumber:61,columnNumber:26},this),` that is already accessible. When pressing the 'ESC' key in a focused tooltip,
+tooltip will be closed.`]},void 0,!0,{fileName:"<source.js>",lineNumber:60,columnNumber:1},this),`
+`,e.jsxDEV(n.p,{children:["There are some required ARIA attributes for the anchor element (such as ",e.jsxDEV(n.code,{children:"aria-hidden"},void 0,!1,{fileName:"<source.js>",lineNumber:64,columnNumber:73},this)," set based on ",e.jsxDEV(n.code,{children:"open"},void 0,!1,{fileName:"<source.js>",lineNumber:64,columnNumber:100},this),`).
+To make this as straightforward as possible, these ARIA attributes are passed
+with the correct values as the `,e.jsxDEV(n.code,{children:"attrs"},void 0,!1,{fileName:"<source.js>",lineNumber:66,columnNumber:32},this)," to the anchor slot. Applying them is as simple as using ",e.jsxDEV(n.code,{children:"v-bind"},void 0,!1,{fileName:"<source.js>",lineNumber:66,columnNumber:96},this),"."]},void 0,!0,{fileName:"<source.js>",lineNumber:64,columnNumber:1},this),`
+`,e.jsxDEV(n.h3,{id:"focus--keyboard",children:"Focus & Keyboard"},void 0,!1,{fileName:"<source.js>",lineNumber:68,columnNumber:1},this),`
+`,e.jsxDEV(n.p,{children:`Due to the different contexts in which a tooltip can be used, focus management and
+keyboard shortcut ESC is provided.
+You are encouraged to consult the ARIA documentation for the particular role`},void 0,!1,{fileName:"<source.js>",lineNumber:70,columnNumber:1},this),`
+`,e.jsxDEV(n.h2,{id:"usage",children:"Usage"},void 0,!1,{fileName:"<source.js>",lineNumber:74,columnNumber:1},this),`
+`,e.jsxDEV(n.h3,{id:"import",children:"Import"},void 0,!1,{fileName:"<source.js>",lineNumber:76,columnNumber:1},this),`
+`,e.jsxDEV(n.pre,{children:e.jsxDEV(n.code,{className:"language-jsx",children:`import { DtTooltip, TOOLTIP_TIPPY_DIRECTIONS } from '@dialpad/dialtone-vue';
+`},void 0,!1,{fileName:"<source.js>",lineNumber:78,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:78,columnNumber:1},this),`
+`,e.jsxDEV(n.h3,{id:"arrow-directions",children:"Arrow Directions"},void 0,!1,{fileName:"<source.js>",lineNumber:82,columnNumber:1},this),`
+`,e.jsxDEV(n.p,{children:`'top' is assigned by default.
+Twelve directions are offered: three on each face of the tooltip.`},void 0,!1,{fileName:"<source.js>",lineNumber:84,columnNumber:1},this),`
+`,e.jsxDEV(n.h3,{id:"fallback-placements",children:"Fallback Placements"},void 0,!1,{fileName:"<source.js>",lineNumber:87,columnNumber:1},this),`
+`,e.jsxDEV(n.p,{children:["The tooltip uses ",e.jsxDEV(n.a,{href:"https://atomiks.github.io/tippyjs/v6/headless-tippy/",target:"_blank",rel:"nofollow noopener noreferrer",children:"headless-tippy"},void 0,!1,{fileName:"<source.js>",lineNumber:89,columnNumber:18},this),` and
+`,e.jsxDEV(n.a,{href:"https://popper.js.org/docs/v2/modifiers/flip/",target:"_blank",rel:"nofollow noopener noreferrer",children:"popper"},void 0,!1,{fileName:"<source.js>",lineNumber:90,columnNumber:1},this),`, if the tooltip opens in a placement where it will
+be clipped, it will move to a new position. It will do this automatically by default, but if you want to
+manually specify which position it will move to in what order you can do so via the fallbackPlacements prop.`]},void 0,!0,{fileName:"<source.js>",lineNumber:89,columnNumber:1},this),`
+`,e.jsxDEV(n.h3,{id:"default-1",children:"Default"},void 0,!1,{fileName:"<source.js>",lineNumber:94,columnNumber:1},this),`
+`,e.jsxDEV(n.pre,{children:e.jsxDEV(n.code,{className:"language-html",children:`<dt-tooltip>
+    Message
+    <template #anchor>
+      Anchor
+    </template>
+</dt-tooltip>
+`},void 0,!1,{fileName:"<source.js>",lineNumber:96,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:96,columnNumber:1},this),`
+`,e.jsxDEV(n.h3,{id:"open-state",children:"Open state"},void 0,!1,{fileName:"<source.js>",lineNumber:105,columnNumber:1},this),`
+`,e.jsxDEV(n.pre,{children:e.jsxDEV(n.code,{className:"language-html",children:`<dt-tooltip
+    message="Message"
+    :show="true"
+>
+    <template #anchor>
+      <dt-button
+        importance="outlined"
+      >
+        Open default state
+      </dt-button>
+    </template>
+</dt-tooltip>
+`},void 0,!1,{fileName:"<source.js>",lineNumber:107,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:107,columnNumber:1},this),`
+`,e.jsxDEV(n.h3,{id:"top-start",children:"top-start"},void 0,!1,{fileName:"<source.js>",lineNumber:122,columnNumber:1},this),`
+`,e.jsxDEV(n.pre,{children:e.jsxDEV(n.code,{className:"language-html",children:`<dt-tooltip
+    placement="top-start"
+    message="Message"
+>
+    <template #anchor>
+        <dt-button>
+            top-start
+        </dt-button>
+    </template>
+</dt-tooltip>
+`},void 0,!1,{fileName:"<source.js>",lineNumber:124,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:124,columnNumber:1},this),`
+`,e.jsxDEV(n.h3,{id:"top",children:"top"},void 0,!1,{fileName:"<source.js>",lineNumber:137,columnNumber:1},this),`
+`,e.jsxDEV(n.pre,{children:e.jsxDEV(n.code,{className:"language-html",children:`<dt-tooltip
+    placement="top"
+    message="Message"
+    >
+    <template #anchor>
+        <dt-button importance="outlined" class="d-w128">
+            top
+        </dt-button>
+    </template>
+</dt-tooltip>
+`},void 0,!1,{fileName:"<source.js>",lineNumber:139,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:139,columnNumber:1},this)]},void 0,!0,{fileName:"<source.js>",lineNumber:1,columnNumber:1},this)}function R(i={}){const{wrapper:n}=Object.assign({},t(),i.components);return n?e.jsxDEV(n,Object.assign({},i,{children:e.jsxDEV(r,i,void 0,!1,{fileName:"<source.js>"},this)}),void 0,!1,{fileName:"<source.js>"},this):r(i)}export{R as default};
+//# sourceMappingURL=tooltip-fb4ba974.js.map
