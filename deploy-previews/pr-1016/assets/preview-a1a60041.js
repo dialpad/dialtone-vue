27fc8e624cbaf59f188308730a3858292fba952b
ramya-dp <ramya-dp@users.noreply.github.com>
2023-06-12 19:43:01 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@e2e7d42720cf26cab7e47e72a3813dc10c921bb2 🚀

---
+++ deploy-previews/pr-1016/assets/preview-a1a60041.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-4186d1f9.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-62474c88.js"),["./DocsRenderer-EYKKDMVH-62474c88.js","./chunk-PCJTTTQV-aa257ef3.js","./iframe-4186d1f9.js","./index-e2ade02d.js","./_commonjsHelpers-725317a4.js","./chunk-4NMOSTKD-938b085e.js","./index-d475d2ea.js","./index-d37d4223.js","./index-aefc0af2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-a1a60041.js.map
