70c446c6bf0f278f5648a020283147b60e916ead
iropolo <iropolo@users.noreply.github.com>
2023-06-15 13:31:14 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@cc1028b2204b55ab0648487ca98a471769ee0035 🚀

---
+++ deploy-previews/pr-1021/assets/components-67a75be8.js
@@ -0,0 +1,14 @@
+import{e as t}from"./index-6c10706d.js";const e=t.img`
+  max-width: 300px;
+  height: auto;
+`,i=t.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 5rem;
+`,a=t.img`
+  height: auto;
+  width: 300px;
+`;export{e as D,i as L,a};
+//# sourceMappingURL=components-67a75be8.js.map
