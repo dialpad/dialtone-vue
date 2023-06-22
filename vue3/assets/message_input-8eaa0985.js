994e7c107636497eecac6a69d77b694700207932
braddialpad <brad.paugh@dialpad.com>
2023-06-22 18:32:15 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@1e549845b843c4079879857da6136af402150cca 🚀

---
+++ vue3/assets/message_input-8eaa0985.js
@@ -0,0 +1,15 @@
+import{j as t}from"./jsx-runtime-80b9e200.js";import{M as n,S as p,C as a,b as m}from"./index-1822d621.js";import{D as c,a as h}from"./message_input.stories-67f93d70.js";import{u as s}from"./index-fa9f9789.js";import"./iframe-38b6d9d5.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./chunk-4NMOSTKD-938b085e.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-aefc0af2.js";import"./index-356e4a49.js";import"./chunk-OPEUWD42-a3b45fd8.js";import"./storybook_utils-521815dd.js";import"./vue.esm-bundler-66d1e5de.js";import"./utils-8eac0b7f.js";import"./rich_text_editor-07274056.js";import"./tippy.esm-e9da0aad.js";import"./_plugin-vue_export-helper-c27b6911.js";import"./button-47493803.js";import"./link_constants-51338c78.js";import"./icon-0d2949f0.js";import"./icon_constants-39899dec.js";import"./emoji_picker-da49b145.js";import"./input-e44648a3.js";import"./validation_messages-97f98f26.js";import"./input-e3f8f33d.js";import"./tab_group-f291df1a.js";import"./tab-138e38a4.js";import"./index-40c4ee4c.js";import"./tooltip-204d45a2.js";import"./tippy_utils-69cfdc19.js";import"./lazy_show-0f90120c.js";import"./popover-2bba58d1.js";import"./modal-f96ddab9.js";import"./sr_only_close_button-ebad008c.js";function Q(i={}){const{wrapper:o}=Object.assign({},s(),i.components);return o?t.jsx(o,Object.assign({},i,{children:t.jsx(r,{})})):r();function r(){const e=Object.assign({h1:"h1",p:"p",h2:"h2",h3:"h3",pre:"pre",code:"code"},s(),i.components);return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:c}),`
+`,t.jsx(e.h1,{id:"message-input-recipe",children:"Message Input Recipe"}),`
+`,t.jsx(p,{children:t.jsx(e.p,{children:"Message Input recipe wraps the Rich Text Editor for uses cases like conversations and writing notes etc."})}),`
+`,t.jsx(e.p,{children:`Message input recipe consists of the Rich text editor along with a container that houses the image picker,
+emoji picker and slots for helper texts and image carousels.
+It further has the logic to handle character limit logic and other message input functionality.`}),`
+`,t.jsx(e.h2,{id:"base-style",children:"Base Style"}),`
+`,t.jsx(a,{of:h}),`
+`,t.jsx(e.h2,{id:"slots-props--events",children:"Slots, Props & Events"}),`
+`,t.jsx(m,{}),`
+`,t.jsx(e.h2,{id:"usage",children:"Usage"}),`
+`,t.jsx(e.h3,{id:"import",children:"Import"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-jsx",children:`import { DtRecipeMessageInput } from '@dialpad/dialtone-vue';
+`})})]})}}export{Q as default};
+//# sourceMappingURL=message_input-8eaa0985.js.map
