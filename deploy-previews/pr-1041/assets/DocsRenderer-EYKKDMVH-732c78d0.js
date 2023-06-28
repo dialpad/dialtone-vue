1ad30e776855e275671322830e16675cfadfcf36
sanjeevdialpad <sanjeevdialpad@users.noreply.github.com>
2023-06-28 20:04:35 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@f007b33572bc9fc2c7448884ea6797775e2cd3ee 🚀

--- deploy-previews/pr-1041/assets/DocsRenderer-EYKKDMVH-732c78d0.js
+++
@@ -1,2 +0,0 @@
-import{D as f,d as D}from"./chunk-PCJTTTQV-a822949f.js";import"./iframe-1b191a65.js";import"../sb-preview/runtime.js";import"./index-cea9c5f7.js";import"./_commonjsHelpers-725317a4.js";import"./chunk-4NMOSTKD-938b085e.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-aefc0af2.js";import"./index-356e4a49.js";export{f as DocsRenderer,D as defaultComponents};
-//# sourceMappingURL=DocsRenderer-EYKKDMVH-732c78d0.js.map
---
+++ deploy-previews/pr-1041/assets/contact_row-eeda0de4.js
@@ -0,0 +1,62 @@
+import{j as e}from"./jsx-runtime-af3e744b.js";import{M as c,S as l,C as o,a as s,b as p}from"./index-daeffdd0.js";import{C as m,D as d,V as h}from"./contact_row.stories-6758e487.js";import{u as i}from"./index-17190709.js";import"./iframe-444a356a.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./chunk-4NMOSTKD-938b085e.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-aefc0af2.js";import"./index-356e4a49.js";import"./chunk-OPEUWD42-a3b45fd8.js";import"./storybook_utils-a2047fb2.js";import"./vue.esm-bundler-3bf5edd9.js";import"./utils-b704f328.js";import"./general_row-ec424639.js";import"./badge-5c6c9d78.js";import"./icon-28829c6f.js";import"./icon_constants-8c90fe6d.js";import"./_plugin-vue_export-helper-c27b6911.js";import"./button-581e5965.js";import"./link_constants-51338c78.js";import"./tooltip-925118bf.js";import"./tippy_utils-0155706c.js";import"./tippy.esm-65135068.js";import"./lazy_show-30f95b43.js";import"./emoji_text_wrapper-2a48067f.js";import"./emoji-bd7c8412.js";import"./emoji-8cc1c740.js";import"./index-40c4ee4c.js";import"./skeleton-38922610.js";import"./avatar-83f13b35.js";import"./presence-22277d28.js";import"./stack-990dc610.js";import"./person-13c0d6d2.js";function T(t={}){const{wrapper:a}=Object.assign({},i(),t.components);return a?e.jsx(a,Object.assign({},t,{children:e.jsx(r,{})})):r();function r(){const n=Object.assign({h1:"h1",p:"p",h2:"h2",h3:"h3",pre:"pre",code:"code"},i(),t.components);return e.jsxs(e.Fragment,{children:[e.jsx(c,{of:m}),`
+`,e.jsx(n.h1,{id:"leftbar-contact-row",children:"Leftbar Contact Row"}),`
+`,e.jsx(l,{children:e.jsx(n.p,{children:"Displays the row for one specific contact."})}),`
+`,e.jsx(n.h2,{id:"base-style",children:"Base Style"}),`
+`,e.jsx(o,{children:e.jsx(s,{of:d})}),`
+`,e.jsx(n.h2,{id:"variants",children:"Variants"}),`
+`,e.jsx(o,{children:e.jsx(s,{of:h})}),`
+`,e.jsx(n.h2,{id:"slots-props--events",children:"Slots, Props & Events"}),`
+`,e.jsx(p,{}),`
+`,e.jsx(n.h2,{id:"usage",children:"Usage"}),`
+`,e.jsx(n.h3,{id:"import",children:"Import"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`import { DtRecipeContactRow } from '@dialpad/dialtone-vue';
+`})}),`
+`,e.jsx(n.h3,{id:"default-behavior",children:"Default behavior"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-html",children:`<dt-recipe-group-row
+  name="Jaqueline Nackos"
+  avatar-src="/static/media/../common/assets/person.png"
+  call-button-tooltip="Call"
+/>
+`})}),`
+`,e.jsx(n.h3,{id:"with-user-status",children:"With user status"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-html",children:`<dt-recipe-contact-row
+  name="Jaqueline Nackos"
+  avatar-src="/static/media/../common/assets/person.png"
+  user-status="Good Morning! :smile:"
+  call-button-tooltip="Call"
+/>
+`})}),`
+`,e.jsx(n.h3,{id:"with-presence",children:"With presence"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-html",children:`<dt-recipe-contact-row
+  name="Jaqueline Nackos"
+  avatar-src="/static/media/../common/assets/person.png"
+  avatar-presence="away"
+  presence-text="Away"
+  call-button-tooltip="Call"
+/>
+`})}),`
+`,e.jsx(n.h3,{id:"with-unread-count",children:"With unread count"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-html",children:`<dt-recipe-contact-row
+  name="Jaqueline Nackos"
+  :avatar-src="defaultImage"
+  unread-count="5"
+  unread-count-tooltip="55 unread messages"
+  :has-unreads="true"
+  call-button-tooltip="Call"
+/>
+`})}),`
+`,e.jsx(n.h3,{id:"selected",children:"Selected"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-html",children:`<dt-recipe-contact-row
+  name="Jaqueline Nackos"
+  :avatar-src="defaultImage"
+  :selected="true"
+  call-button-tooltip="Call"
+/>
+`})}),`
+`,e.jsx(n.h3,{id:"with-initial",children:"With initial"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-html",children:`<dt-recipe-contact-row
+  name="Jaqueline Nackos"
+  call-button-tooltip="Call"
+/>
+`})})]})}}export{T as default};
+//# sourceMappingURL=contact_row-eeda0de4.js.map
