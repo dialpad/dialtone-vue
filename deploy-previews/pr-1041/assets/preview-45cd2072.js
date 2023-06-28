3d356879bb51c38b02f6bfdc3f970a90ffe28fff
sanjeevdialpad <sanjeevdialpad@users.noreply.github.com>
2023-06-28 19:38:04 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@bd867d47c73413b3b87c90ab74c4c5e47aaac9d1 🚀

---
+++ deploy-previews/pr-1041/assets/preview-45cd2072.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-1b191a65.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-732c78d0.js"),["./DocsRenderer-EYKKDMVH-732c78d0.js","./chunk-PCJTTTQV-a822949f.js","./iframe-1b191a65.js","./index-cea9c5f7.js","./_commonjsHelpers-725317a4.js","./chunk-4NMOSTKD-938b085e.js","./index-d475d2ea.js","./index-d37d4223.js","./index-aefc0af2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-45cd2072.js.map
