c343768a4a50689a949658958faa589b4d7b952b
Julio Ortega <julio.ortega@dialpad.com>
2023-06-30 11:57:42 -0300

    feat(item-layout): create component

---
+++ components/item_layout/item_layout_default.story.vue
@@ -0,0 +1,29 @@
+<template>
+  <dt-item-layout>
+    <template #left>
+      <span v-html="left" />
+    </template>
+    <span v-html="defaultSlot" />
+    <template #subtitle>
+      <span v-html="subtitle" />
+    </template>
+    <template #bottom>
+      <span v-html="bottom" />
+    </template>
+    <template #right>
+      <span v-html="right" />
+    </template>
+    <template #selected>
+      <span v-html="selected" />
+    </template>
+  </dt-item-layout>
+</template>
+
+<script>
+import DtItemLayout from './item_layout.vue';
+
+export default {
+  name: 'DtItemLayoutDefault',
+  components: { DtItemLayout },
+};
+</script>
