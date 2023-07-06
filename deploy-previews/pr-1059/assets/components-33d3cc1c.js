55e7239d4b5604ce9e12d0669f5b14ef0b3cea89
ramya-dp <ramya-dp@users.noreply.github.com>
2023-07-06 09:12:36 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@35b4f95ffa48af4c5348124e0a78021f6a537d5d 🚀

---
+++ deploy-previews/pr-1059/assets/components-33d3cc1c.js
@@ -0,0 +1,14 @@
+import{e as t}from"./index-c7036e0d.js";const e=t.img`
+  max-width: 300px;
+  height: auto;
+`,i=t.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 5rem;
+`,a=t.img`
+  height: auto;
+  width: 300px;
+`;export{e as D,i as L,a};
+//# sourceMappingURL=components-33d3cc1c.js.map
