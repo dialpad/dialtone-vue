cb3e724a3671123171f07d835906fdaebd6d5cdc
juliodialpad <juliodialpad@users.noreply.github.com>
2023-07-11 18:26:54 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@b444675379ce2136e9a4997450c7c20e88e4888e 🚀

---
+++ deploy-previews/pr-1065/assets/combobox_with_popover-65639106.js
@@ -0,0 +1,84 @@
+import{j as e}from"./jsx-dev-runtime-ed1035f9.js";import{M as t,S as l,C as r,a as m,b as u}from"./index-4c596ea9.js";import{C as c,D as a}from"./combobox_with_popover.stories-2a021782.js";import{u as o}from"./index-d6ae4816.js";import"./iframe-cf40c18e.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./chunk-4NMOSTKD-938b085e.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-aefc0af2.js";import"./index-356e4a49.js";import"./chunk-OPEUWD42-a3b45fd8.js";import"./storybook_utils-3dddf937.js";import"./vue.esm-bundler-8de31a68.js";import"./utils-d48fb0e8.js";import"./combobox_with_popover-9528bdc9.js";import"./combobox-8cd193cc.js";import"./keyboard_list_navigation-7bd5fd77.js";import"./list_item-673b49ce.js";import"./icon-9bb64099.js";import"./icon_constants-7a247d42.js";import"./_plugin-vue_export-helper-c27b6911.js";import"./item_layout-4e1173fd.js";import"./skeleton-e397fd7d.js";import"./popover-a4b003b1.js";import"./tippy_utils-e4c34e91.js";import"./lazy_show-b21d4dfb.js";import"./modal-f96ddab9.js";import"./button-83ae1ba6.js";import"./link_constants-51338c78.js";import"./sr_only_close_button-61524a7c.js";import"./dropdown_constants-d0cadac9.js";import"./input-920ac5b7.js";import"./validation_messages-6606b97f.js";import"./input-6ecbdc2b.js";import"./checkbox-9f1a5db1.js";function s(n){const i=Object.assign({h1:"h1",p:"p",h2:"h2",code:"code",a:"a",h3:"h3",pre:"pre"},o(),n.components);return e.jsxDEV(e.Fragment,{children:[e.jsxDEV(t,{of:c},void 0,!1,{fileName:"<source.js>",lineNumber:5,columnNumber:1},this),`
+`,e.jsxDEV(i.h1,{id:"combobox-with-popover",children:"Combobox With Popover"},void 0,!1,{fileName:"<source.js>",lineNumber:7,columnNumber:1},this),`
+`,e.jsxDEV(l,{children:e.jsxDEV(i.p,{children:"A combobox that renders the listbox inside a popover therefore the listbox is rendered at the root of the document."},void 0,!1,{fileName:"<source.js>",lineNumber:10,columnNumber:3},this)},void 0,!1,{fileName:"<source.js>",lineNumber:9,columnNumber:1},this),`
+`,e.jsxDEV(i.h2,{id:"base-style",children:"Base Style"},void 0,!1,{fileName:"<source.js>",lineNumber:13,columnNumber:1},this),`
+`,e.jsxDEV(i.p,{children:["The combobox contains the ",e.jsxDEV(i.code,{children:"input"},void 0,!1,{fileName:"<source.js>",lineNumber:15,columnNumber:27},this)," and ",e.jsxDEV(i.code,{children:"list"},void 0,!1,{fileName:"<source.js>",lineNumber:15,columnNumber:39},this)," slots specified in the ",e.jsxDEV(i.a,{href:"?path=/docs/components-combobox--default",children:"combobox documentation"},void 0,!1,{fileName:"<source.js>",lineNumber:15,columnNumber:69},this),`.
+Since it's using the popover content to render the list, it's also possible to add a header and footer to the popover
+as it's specified in the `,e.jsxDEV(i.a,{href:"?path=/docs/components-popover--default",children:"popover documentation"},void 0,!1,{fileName:"<source.js>",lineNumber:17,columnNumber:26},this),"."]},void 0,!0,{fileName:"<source.js>",lineNumber:15,columnNumber:1},this),`
+`,e.jsxDEV(r,{children:e.jsxDEV(m,{of:a},void 0,!1,{fileName:"<source.js>",lineNumber:20,columnNumber:3},this)},void 0,!1,{fileName:"<source.js>",lineNumber:19,columnNumber:1},this),`
+`,e.jsxDEV(i.h2,{id:"slots-props--events",children:"Slots, Props & Events"},void 0,!1,{fileName:"<source.js>",lineNumber:23,columnNumber:1},this),`
+`,e.jsxDEV(u,{},void 0,!1,{fileName:"<source.js>",lineNumber:25,columnNumber:1},this),`
+`,e.jsxDEV(i.h2,{id:"accessibility",children:"Accessibility"},void 0,!1,{fileName:"<source.js>",lineNumber:27,columnNumber:1},this),`
+`,e.jsxDEV(i.p,{children:["It is possible to include a screen reader visible only close button setting ",e.jsxDEV(i.code,{children:"visually-hidden-close"},void 0,!1,{fileName:"<source.js>",lineNumber:29,columnNumber:77},this),`
+and `,e.jsxDEV(i.code,{children:"visually-hidden-close-label"},void 0,!1,{fileName:"<source.js>",lineNumber:30,columnNumber:5},this)," props."]},void 0,!0,{fileName:"<source.js>",lineNumber:29,columnNumber:1},this),`
+`,e.jsxDEV(i.h2,{id:"usage",children:"Usage"},void 0,!1,{fileName:"<source.js>",lineNumber:32,columnNumber:1},this),`
+`,e.jsxDEV(i.h3,{id:"import",children:"Import"},void 0,!1,{fileName:"<source.js>",lineNumber:34,columnNumber:1},this),`
+`,e.jsxDEV(i.pre,{children:e.jsxDEV(i.code,{className:"language-jsx",children:`import { DtRecipeComboboxWithPopover } from '@dialpad/dialtone-vue';
+`},void 0,!1,{fileName:"<source.js>",lineNumber:36,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:36,columnNumber:1},this),`
+`,e.jsxDEV(i.h3,{id:"basic-usage",children:"Basic usage"},void 0,!1,{fileName:"<source.js>",lineNumber:40,columnNumber:1},this),`
+`,e.jsxDEV(i.pre,{children:e.jsxDEV(i.code,{className:"language-html",children:`<dt-recipe-combobox-with-popover
+  ref="comboboxWithPopover"
+  list-aria-label="Example list items"
+  @escape="onEscape"
+  @highlight="onHighlight"
+  @select="onSelect"
+>
+  <template #input="{ inputProps }">
+    <dt-input v-bind="inputProps" />
+  </template>
+  <template #list="{ listProps }">
+    <ul v-bind="listProps">
+      <dt-list-item
+        v-for="(item, i) in items"
+        :key="item.id"
+        navigation-type="arrow-keys"
+        role="option"
+        @click="onSelect(i)"
+      >
+        {{ item.name }}
+      </dt-list-item>
+    </ul>
+  </template>
+</dt-recipe-combobox-with-popover>
+`},void 0,!1,{fileName:"<source.js>",lineNumber:42,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:42,columnNumber:1},this),`
+`,e.jsxDEV(i.p,{children:["When not passing ",e.jsxDEV(i.code,{children:"showList"},void 0,!1,{fileName:"<source.js>",lineNumber:69,columnNumber:18},this),` and using the default combobox trigger on input focus,
+to close the list with the `,e.jsxDEV(i.code,{children:"select"},void 0,!1,{fileName:"<source.js>",lineNumber:70,columnNumber:28},this)," event or with the ",e.jsxDEV(i.code,{children:"click"},void 0,!1,{fileName:"<source.js>",lineNumber:70,columnNumber:55},this),` event in the list item,
+use the `,e.jsxDEV(i.code,{children:"closeComboboxList"},void 0,!1,{fileName:"<source.js>",lineNumber:71,columnNumber:9},this)," method:"]},void 0,!0,{fileName:"<source.js>",lineNumber:69,columnNumber:1},this),`
+`,e.jsxDEV(i.pre,{children:e.jsxDEV(i.code,{className:"language-jsx",children:`methods: {
+  onSelect (i) {
+    this.$refs.comboboxWithPopover.closeComboboxList();
+  },
+}
+`},void 0,!1,{fileName:"<source.js>",lineNumber:73,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:73,columnNumber:1},this),`
+`,e.jsxDEV(i.h3,{id:"with-header-and-footer",children:"With header and footer"},void 0,!1,{fileName:"<source.js>",lineNumber:81,columnNumber:1},this),`
+`,e.jsxDEV(i.pre,{children:e.jsxDEV(i.code,{className:"language-html",children:`<dt-recipe-combobox-with-popover
+  list-aria-label="Example list items"
+  @escape="onEscape"
+  @highlight="onHighlight"
+  @select="onSelect"
+>
+  <template #input="{ inputProps }">
+    <dt-input v-bind="inputProps" />
+  </template>
+  <template #list="{ listProps }">
+    <ul v-bind="listProps">
+      <dt-list-item
+        v-for="(item, i) in items"
+        :key="item.id"
+        navigation-type="arrow-keys"
+        role="option"
+        @click="onSelect(i)"
+      >
+        {{ item.name }}
+      </dt-list-item>
+    </ul>
+  </template>
+  <template #header>
+    <span>Header content</span>
+  </template>
+  <template #footer>
+     <span>Footer content</span>
+  </template>
+</dt-recipe-combobox-with-popover>
+`},void 0,!1,{fileName:"<source.js>",lineNumber:83,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:83,columnNumber:1},this)]},void 0,!0,{fileName:"<source.js>",lineNumber:1,columnNumber:1},this)}function J(n={}){const{wrapper:i}=Object.assign({},o(),n.components);return i?e.jsxDEV(i,Object.assign({},n,{children:e.jsxDEV(s,n,void 0,!1,{fileName:"<source.js>"},this)}),void 0,!1,{fileName:"<source.js>"},this):s(n)}export{J as default};
+//# sourceMappingURL=combobox_with_popover-65639106.js.map
---
+++ deploy-previews/pr-1065/assets/preview-072956c8.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-cf40c18e.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-f6087e58.js"),["./DocsRenderer-EYKKDMVH-f6087e58.js","./chunk-PCJTTTQV-753e6e4f.js","./iframe-cf40c18e.js","./index-4c596ea9.js","./_commonjsHelpers-725317a4.js","./chunk-4NMOSTKD-938b085e.js","./index-d475d2ea.js","./index-d37d4223.js","./index-aefc0af2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-072956c8.js.map
