3c5fe6d931da2567e151bb56b73f420025305773
braddialpad <brad.paugh@dialpad.com>
2023-07-14 20:46:33 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@df44d31445276da960e200b84e4ee47de7a39617 🚀

---
+++ assets/list_item_group-35dc1bd8.js
@@ -0,0 +1,43 @@
+import{j as t}from"./jsx-runtime-15a6d800.js";import{M as o,S as r,C as a,a as l,b as m}from"./index-4517e7df.js";import{L as d,D as p}from"./list_item_group.stories-e0d88d93.js";import{u as s}from"./index-983dc319.js";import"./iframe-2faeb3ad.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./chunk-4NMOSTKD-938b085e.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-aefc0af2.js";import"./index-356e4a49.js";import"./storybook_utils-6c24aced.js";import"./vue.esm-ba2ca387.js";import"./list_item_group-f7e31e31.js";import"./_plugin-vue2_normalizer-2bbd088e.js";import"./list_item-afde18d7.js";import"./icon-b42420a4.js";import"./icon_constants-80dce3e6.js";import"./item_layout-173b62c3.js";function n(i){const e=Object.assign({h1:"h1",p:"p",h2:"h2",h3:"h3",pre:"pre",code:"code"},s(),i.components);return t.jsxs(t.Fragment,{children:[t.jsx(o,{of:d}),`
+`,t.jsx(e.h1,{id:"list-item-group",children:"List Item Group"}),`
+`,t.jsx(r,{children:t.jsx(e.p,{children:'The "List Item Group" component uses a non interactive heading which groups list items.'})}),`
+`,t.jsx(e.h2,{id:"base-style",children:"Base Style"}),`
+`,t.jsx(e.p,{children:`You should use this component when you have multiple list items you would like to group into different
+categories.`}),`
+`,t.jsx(e.p,{children:`The heading is unstyled by default. You will likely have to pass utility classes to the headingClass prop to make the
+heading look how you wish.`}),`
+`,t.jsx(a,{children:t.jsx(l,{of:p})}),`
+`,t.jsx(e.h2,{id:"slots-props--events",children:"Slots, Props & Events"}),`
+`,t.jsx(m,{}),`
+`,t.jsx(e.h2,{id:"usage",children:"Usage"}),`
+`,t.jsx(e.h3,{id:"import",children:"Import"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-jsx",children:`import { DtListItemGroup } from '@dialpad/dialtone-vue';
+`})}),`
+`,t.jsx(e.h3,{id:"usage-1",children:"Usage"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-jsx",children:`<dt-list-item-group
+  heading="Example Heading"
+  headingClass="d-fw-bold"
+>
+  <dt-list-item
+    navigation-type="tab"
+  >
+    item1
+  </dt-list-item>
+  <dt-list-item
+    navigation-type="tab"
+  >
+    item2
+  </dt-list-item>
+  <dt-list-item
+    navigation-type="tab"
+  >
+    item3
+  </dt-list-item>
+</dt-list-item-group>
+
+`})}),`
+`,t.jsx(e.h2,{id:"accessibility",children:"Accessibility"}),`
+`,t.jsx(e.p,{children:`The List Item Group does not implement arrow-keys keyboard navigation. You will however get arrow-keys
+keyboard navigation when using this within list based Dialtone components such as Dropdown or Combobox.`}),`
+`,t.jsx(e.p,{children:"The aria label for the List Item Group will be set by the content of the heading."})]})}function E(i={}){const{wrapper:e}=Object.assign({},s(),i.components);return e?t.jsx(e,Object.assign({},i,{children:t.jsx(n,i)})):n(i)}export{E as default};
+//# sourceMappingURL=list_item_group-35dc1bd8.js.map
---
+++ assets/preview-92b9cb49.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-2faeb3ad.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-53b6c1b6.js"),["./DocsRenderer-EYKKDMVH-53b6c1b6.js","./chunk-PCJTTTQV-15dcc4e7.js","./iframe-2faeb3ad.js","./index-4517e7df.js","./_commonjsHelpers-725317a4.js","./chunk-4NMOSTKD-938b085e.js","./index-d475d2ea.js","./index-d37d4223.js","./index-aefc0af2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-92b9cb49.js.map
