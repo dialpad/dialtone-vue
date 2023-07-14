eaacef1b7521aef391abb2b0e1b91bbb3a03bd94
juliodialpad <juliodialpad@users.noreply.github.com>
2023-07-14 18:27:57 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@3b2e19280e43e2b0b830214990af2cf4b4cf6225 🚀

---
+++ deploy-previews/pr-1072/assets/preview-420154d9.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-fd9d0d0c.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-c89d9b62.js"),["./DocsRenderer-EYKKDMVH-c89d9b62.js","./chunk-PCJTTTQV-8644fd74.js","./iframe-fd9d0d0c.js","./index-aa86fe59.js","./_commonjsHelpers-725317a4.js","./chunk-4NMOSTKD-938b085e.js","./index-d475d2ea.js","./index-d37d4223.js","./index-aefc0af2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-420154d9.js.map
