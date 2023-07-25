83b0de02506479212f53043e12f843089da87c77
Julio Ortega <julio.ortega@dialpad.com>
2023-07-25 13:11:04 -0600

    feat(avatar): extract initials from full-name

--- components/avatar/avatar_default.story.vue
+++ components/avatar/avatar_default.story.vue
@@ -1,30 +1,27 @@
 <template>
   <dt-avatar
     :id="id"
+    :full-name="fullName"
+    :icon-name="iconName"
+    :image-src="imageSrc"
     :size="size"
     :presence="presence"
     :avatar-class="avatarClass"
     :canvas-class="canvasClass"
-    :initials="initials"
     :seed="seed"
     :group="group"
     :color="color"
     :overlay-icon="overlayIcon"
     :overlay-text="overlayText"
     :overlay-class="overlayClass"
-  >
-    <html-fragment
-      :html="defaultSlot"
-    />
-  </dt-avatar>
+  />
 </template>
 
 <script>
-import { htmlFragment } from '@/common/utils';
 import DtAvatar from './avatar.vue';
 
 export default {
   name: 'DtAvatarDefault',
-  components: { DtAvatar, htmlFragment },
+  components: { DtAvatar },
 };
 </script>
