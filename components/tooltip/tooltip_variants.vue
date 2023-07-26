2f48bd1e50332712ea6ebcb04a341fd9180e872e
Brad Paugh <brad.paugh@dialpad.com>
2023-07-26 13:34:28 -0700

    fix(tooltip): anchor display property issue

--- components/tooltip/tooltip_variants.vue
+++ components/tooltip/tooltip_variants.vue
@@ -33,6 +33,28 @@
         </dt-tooltip>
       </div>
     </div>
+    <div class="d-d-flex d-jc-center d-ai-center d-w100p">
+      <div>
+        Circle button tooltip
+      </div>
+      <dt-tooltip
+        class="d-ml4"
+        :transition="transition"
+        :message="localMessage"
+        :show="show"
+      >
+        <template #anchor>
+          <dt-button circle>
+            <template #icon>
+              <dt-icon
+                name="dp-phone"
+                size="300"
+              />
+            </template>
+          </dt-button>
+        </template>
+      </dt-tooltip>
+    </div>
     <div class="d-d-flex d-jc-center d-w100p">
       <!-- Text -->
       <dt-tooltip
@@ -92,6 +114,7 @@
 <script>
 import DtTooltip from './tooltip.vue';
 import { DtButton } from './../button';
+import { DtIcon } from './../icon';
 import { TOOLTIP_DIRECTIONS } from './tooltip_constants';
 
 function sliceIntoChunks (arr, chunkSize) {
@@ -105,7 +128,7 @@ function sliceIntoChunks (arr, chunkSize) {
 
 export default {
   name: 'TooltipVariants',
-  components: { DtTooltip, DtButton },
+  components: { DtTooltip, DtIcon, DtButton },
   data () {
     return {
       TOOLTIP_DIRECTIONS: sliceIntoChunks(this.$attrs.customDirections || TOOLTIP_DIRECTIONS, 3),
