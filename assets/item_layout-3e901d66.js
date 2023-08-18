c671ca1f7a133372fa7f0a0d1f26c9663dc102ed
braddialpad <brad.paugh@dialpad.com>
2023-08-18 17:25:59 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@d10fc71c4e8db701c664e2d6a4a904750315c4cd 🚀

---
+++ assets/item_layout-3e901d66.js
@@ -0,0 +1,13 @@
+import{j as t}from"./jsx-runtime-5b40978d.js";import{M as i,S as r,d as a,f as m}from"./index-d560cec7.js";import{D as p,a as d}from"./item_layout.stories-654ec75e.js";import{u as n}from"./index-745e8d1e.js";import"./iframe-71917256.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./_commonjsHelpers-725317a4.js";import"./index-d37d4223.js";import"./index-aefc0af2.js";import"./index-356e4a49.js";import"./storybook_utils-010d0d6a.js";import"./vue.esm-ba2ca387.js";import"./item_layout-173b62c3.js";import"./_plugin-vue2_normalizer-2bbd088e.js";function o(s){const e=Object.assign({h1:"h1",p:"p",code:"code",h2:"h2",h3:"h3",pre:"pre"},n(),s.components);return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:p}),`
+`,t.jsx(e.h1,{id:"dtitemlayout",children:"DtItemLayout"}),`
+`,t.jsx(r,{children:t.jsxs(e.p,{children:[`Custom layout to enable developer to use list-item like stack.
+It is used as base for `,t.jsx(e.code,{children:"dt-list-item"})," component"]})}),`
+`,t.jsx(e.h2,{id:"base-style",children:"Base Style"}),`
+`,t.jsx(a,{of:d}),`
+`,t.jsx(e.h2,{id:"slots-props--events",children:"Slots, Props & Events"}),`
+`,t.jsx(m,{}),`
+`,t.jsx(e.h2,{id:"usage",children:"Usage"}),`
+`,t.jsx(e.h3,{id:"import",children:"Import"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-jsx",children:`import { DtItemLayout } from '@dialpad/dialtone-vue';
+`})})]})}function S(s={}){const{wrapper:e}=Object.assign({},n(),s.components);return e?t.jsx(e,Object.assign({},s,{children:t.jsx(o,s)})):o(s)}export{S as default};
+//# sourceMappingURL=item_layout-3e901d66.js.map
--- assets/preview-5128c3dd.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-44faf968.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-15b135ee.js"),["./DocsRenderer-3PUGWF3O-15b135ee.js","./chunk-S4VUQJ4A-b641ca4d.js","./iframe-44faf968.js","./index-a7e5ec57.js","./index-d475d2ea.js","./_commonjsHelpers-725317a4.js","./index-d37d4223.js","./index-aefc0af2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-5128c3dd.js.map
