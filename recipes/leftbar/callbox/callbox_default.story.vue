ff8c3b96f0b28e2443335e7450935eea1b8e14af
Julio Ortega <julio.ortega@dialpad.com>
2023-08-25 17:51:19 -0600

    simplify default story

--- recipes/leftbar/callbox/callbox_default.story.vue
+++ recipes/leftbar/callbox/callbox_default.story.vue
@@ -7,66 +7,37 @@
     :title="title"
     :border-color="borderColor"
   >
-    <template #video>
+    <template
+      v-if="video"
+      #video
+    >
       <div
-        v-if="video"
         v-html="video"
       />
     </template>
-    <template #badge>
+    <template
+      v-if="badge"
+      #badge
+    >
       <span
-        v-if="badge"
         v-html="badge"
       />
     </template>
-    <template #subtitle>
+    <template
+      v-if="subtitle"
+      #subtitle
+    >
       <span
-        v-if="subtitle"
         v-html="subtitle"
       />
-      <div
-        v-else
-        class="d-d-flex d-ai-center d-flow4"
-      >
-        <dt-icon
-          name="share-screen"
-          size="200"
-        />
-        <span>06:01</span>
-      </div>
     </template>
-    <template #right>
+    <template
+      v-if="right"
+      #right
+    >
       <div
-        v-if="right"
         v-html="right"
       />
-      <div v-else>
-        <dt-button
-          aria-label="toggle dialpad ai"
-          circle
-          importance="clear"
-        >
-          <template #icon>
-            <dt-icon
-              name="dialpad-ai-color"
-              size="400"
-            />
-          </template>
-        </dt-button>
-        <dt-button
-          aria-label="hang call"
-          circle
-          importance="clear"
-        >
-          <template #icon>
-            <dt-icon
-              name="phone-hang-up"
-              class="d-fc-critical"
-              size="400"
-            />
-          </template>
-        </dt-button>
-      </div>
     </template>
     <template
       v-if="bottom"
@@ -79,11 +50,9 @@
 
 <script>
 import DtRecipeCallbox from './callbox.vue';
-import DtIcon from '@/components/icon/icon.vue';
-import DtButton from '@/components/button/button.vue';
 
 export default {
   name: 'DtRecipeCallboxDefault',
-  components: { DtButton, DtIcon, DtRecipeCallbox },
+  components: { DtRecipeCallbox },
 };
 </script>
