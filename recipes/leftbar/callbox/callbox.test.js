746f610629d7b004a005aa252d8c92b08a431aca
Julio Ortega <julio.ortega@dialpad.com>
2023-09-04 12:39:39 -0600

    style(callbox): add line break

--- recipes/leftbar/callbox/callbox.test.js
+++ recipes/leftbar/callbox/callbox.test.js
@@ -142,6 +142,7 @@ describe('DtRecipeCallbox Tests', () => {
       describe('When badge slot is provided', () => {
         it('Renders badge slot content', () => {
           mockSlots = { badge: MOCK_BADGE_SLOT_CONTENT };
+
           updateWrapper();
 
           expect(badgeSlot.text()).toBe(MOCK_BADGE_SLOT_CONTENT);
@@ -151,6 +152,7 @@ describe('DtRecipeCallbox Tests', () => {
       describe('When bottom slot is provided', () => {
         it('Renders bottom slot content', () => {
           mockSlots = { bottom: MOCK_BOTTOM_SLOT_CONTENT };
+
           updateWrapper();
 
           expect(bottomSlot.text()).toBe(MOCK_BOTTOM_SLOT_CONTENT);
@@ -160,6 +162,7 @@ describe('DtRecipeCallbox Tests', () => {
       describe('When right slot is provided', () => {
         it('Renders right slot content', () => {
           mockSlots = { right: MOCK_RIGHT_SLOT_CONTENT };
+
           updateWrapper();
 
           expect(rightSlot.text()).toBe(MOCK_RIGHT_SLOT_CONTENT);
@@ -169,6 +172,7 @@ describe('DtRecipeCallbox Tests', () => {
       describe('When subtitle slot is provided', () => {
         it('Renders subtitle slot content', () => {
           mockSlots = { subtitle: MOCK_SUBTITLE_SLOT_CONTENT };
+
           updateWrapper();
 
           expect(subtitleSlot.text()).toBe(MOCK_SUBTITLE_SLOT_CONTENT);
@@ -178,6 +182,7 @@ describe('DtRecipeCallbox Tests', () => {
       describe('When video slot is provided', () => {
         it('Renders video slot content', () => {
           mockSlots = { video: MOCK_VIDEO_SLOT_CONTENT };
+
           updateWrapper();
 
           expect(videoSlot.text()).toBe(MOCK_VIDEO_SLOT_CONTENT);
