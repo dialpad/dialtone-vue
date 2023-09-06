cea0d5765f830e1fc458b46e5c6ba72f86aa22ac
iropolo <ignacio.ropolo@dialpad.com>
2023-09-06 12:01:17 -0300

    test(combobox, datepicker, dropdown): new test template

--- components/dropdown/dropdown.test.js
+++ components/dropdown/dropdown.test.js
@@ -28,6 +28,7 @@ describe('DtDropdown Tests', () => {
   let listWrapper;
   let popover;
   let popoverContent;
+  let buttonExists;
 
   const updateWrapper = () => {
     wrapper = mount(DtDropdown, {
@@ -46,6 +47,7 @@ describe('DtDropdown Tests', () => {
     popover = wrapper.findComponent({ ref: 'popover' });
     popoverContent = popover.findComponent({ ref: 'content' });
     listWrapper = popoverContent.find('[data-qa="dt-dropdown-list-wrapper"]');
+    buttonExists = wrapper.findComponent(SrOnlyCloseButton);
   };
 
   beforeAll(() => {
@@ -80,9 +82,7 @@ describe('DtDropdown Tests', () => {
     });
 
     it('should not render the visually hidden close button', () => {
-      const buttonExists = wrapper.findComponent(SrOnlyCloseButton).exists();
-
-      expect(!buttonExists).toBe(true);
+      expect(buttonExists.exists()).toBe(false);
     });
 
     describe('When a list is provided', () => {
@@ -107,9 +107,7 @@ describe('DtDropdown Tests', () => {
       });
 
       it('should contain a visually hidden close button', () => {
-        const buttonExists = wrapper.findComponent(SrOnlyCloseButton).exists();
-
-        expect(buttonExists).toBe(true);
+        expect(buttonExists.exists()).toBe(true);
       });
 
       describe('When visuallyHiddenCloseLabel is null', () => {
