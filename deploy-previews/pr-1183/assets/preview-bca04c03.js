3be7bd2a61100b6666859cc20cdcf991737f4d39
SheldonDialpad <SheldonDialpad@users.noreply.github.com>
2023-09-15 23:16:40 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@3cf67da8a887e986645193df454fb9ddef6d388c 🚀

---
+++ deploy-previews/pr-1183/assets/preview-bca04c03.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-90ae5ebe.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-1b56446b.js"),["./DocsRenderer-3PUGWF3O-1b56446b.js","./chunk-S4VUQJ4A-2b2713d4.js","./iframe-90ae5ebe.js","./index-64929bc4.js","./index-d475d2ea.js","./_commonjsHelpers-725317a4.js","./index-d37d4223.js","./index-aefc0af2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-bca04c03.js.map
