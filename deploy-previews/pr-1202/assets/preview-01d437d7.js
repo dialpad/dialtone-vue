30cbad79d2f38a4fb04a02999be821f02c006596
iropolo <iropolo@users.noreply.github.com>
2023-09-22 14:10:51 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@c44c15f5d9d0ef36a4360d4a63cb213febc92a70 🚀

---
+++ deploy-previews/pr-1202/assets/preview-01d437d7.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-e6daf149.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-ae24da34.js"),["./DocsRenderer-3PUGWF3O-ae24da34.js","./chunk-S4VUQJ4A-c14463e2.js","./iframe-e6daf149.js","./index-0d4f528f.js","./_commonjsHelpers-725317a4.js","./index-d37d4223.js","./index-f073aaf1.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-01d437d7.js.map
