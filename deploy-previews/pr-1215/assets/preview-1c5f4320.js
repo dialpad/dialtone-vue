2778410fcf75c28318c616c429d59b6b375bfc0e
AlexLamDialpad <AlexLamDialpad@users.noreply.github.com>
2023-09-26 23:25:26 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@76955a4ed41581c6f423b1152969e7e731e5a1d8 🚀

--- deploy-previews/pr-1215/assets/preview-1c5f4320.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-f98f5a6f.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-38d1dc69.js"),["./DocsRenderer-3PUGWF3O-38d1dc69.js","./chunk-S4VUQJ4A-237c9933.js","./iframe-f98f5a6f.js","./index-2b69acc2.js","./_commonjsHelpers-725317a4.js","./index-d37d4223.js","./index-cba466d5.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-1c5f4320.js.map
