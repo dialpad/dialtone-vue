0c547e44d825794bc37d5b98bad76fefafd656b1
juliodialpad <juliodialpad@users.noreply.github.com>
2023-09-27 20:08:19 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@bd41a9d53c950ca441497d63b4b93e70a36d4820 🚀

---
+++ deploy-previews/pr-1219/assets/feed_item_pill-03abb818.js
@@ -0,0 +1,56 @@
+import{j as e}from"./jsx-dev-runtime-5ac8d10b.js";import{M as s,b as l,C as n,d as t,e as u}from"./index-cf7f82df.js";import{F as a,D as p,A as d,N as c}from"./feed_item_pill.stories-09f151f1.js";import{u as o}from"./index-afe1bb8d.js";import"./iframe-1f5328c4.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./index-d37d4223.js";import"./index-f073aaf1.js";import"./index-356e4a49.js";import"./storybook_utils-1fcbdb2e.js";import"./vue.esm-bundler-735640fd.js";import"./utils-4a13a80a.js";import"./avatar-6c7cf17e.js";import"./presence-4f451321.js";import"./_plugin-vue_export-helper-c27b6911.js";import"./icon-d93ddec9.js";import"./icon_constants-635d9bbe.js";import"./badge-9602f04e.js";import"./banner-126dd783.js";import"./notice-ed8a7782.js";import"./button-4346d584.js";import"./link_constants-8ea95ec9.js";import"./sr_only_close_button-bed1eac4.js";import"./modal-f96ddab9.js";import"./breadcrumbs-6690387d.js";import"./link-1107d035.js";import"./button_group-0884e399.js";import"./card-0785a2ab.js";import"./codeblock-4acbf3ff.js";import"./combobox-525e60a7.js";import"./keyboard_list_navigation-7bd5fd77.js";import"./list_item-ed24eecf.js";import"./item_layout-f83ccb15.js";import"./skeleton-722196b6.js";import"./collapsible-7b2bbb23.js";import"./lazy_show-4d85a9f6.js";import"./dropdown_separator-4cb1e667.js";import"./popover-043690ee.js";import"./tippy_utils-89aa4eb5.js";import"./dropdown_constants-d0cadac9.js";import"./image_viewer-70f0a55e.js";import"./input-32357f8c.js";import"./validation_messages-f712c9b1.js";import"./input-6baddef5.js";import"./input_group-acb638b4.js";import"./modal-188e223a.js";import"./list_item_group-1cbb60e2.js";import"./pagination-efcd7b4f.js";import"./radio-6595d462.js";import"./radio_constants-4f6325c5.js";import"./radio_group-aa355ba3.js";import"./tab_group-96ead068.js";import"./tab-898a13a4.js";import"./tab_panel-10206c9f.js";import"./checkbox-49510162.js";import"./checkbox_group-c4b46032.js";import"./chip-0c13c881.js";import"./select_menu-47664ba0.js";import"./toast-f2db3c00.js";import"./toggle-15eec1bd.js";import"./tooltip-d084916b.js";import"./keyboard_shortcut-41acc3c1.js";import"./root_layout-f22b4343.js";import"./stack-4bf0ac33.js";import"./DtScroller-b9b5562a.js";import"./DtEmojiPicker-16093e7c.js";import"./index-40c4ee4c.js";import"./DtDatepicker-cac9007c.js";import"./combobox_with_popover-3f482beb.js";import"./combobox_multi_select-741f4691.js";import"./callbar_button-391c7dbc.js";import"./callbar_button_with_popover-b13d53ba.js";import"./contact_info-df81e5c4.js";import"./top_banner_info-31abd17e.js";import"./ivr_node-e2659a3a.js";import"./grouped_chip-4ffa4dac.js";import"./settings_menu_button-03088888.js";import"./general_row-e966a969.js";import"./emoji_text_wrapper-1225bf3c.js";import"./emoji-c68ed563.js";import"./emoji-399fff3d.js";import"./group_row-694d7521.js";import"./contact_row-e0175e6c.js";import"./unread_pill-2461b868.js";import"./feed_item_row-b7ce877c.js";import"./time_pill-0fa2995b.js";import"./emoji_row-699f4c5c.js";import"./callbox-b1709257.js";function m(r){const i=Object.assign({h1:"h1",p:"p",h2:"h2",h3:"h3",pre:"pre",code:"code"},o(),r.components);return e.jsxDEV(e.Fragment,{children:[e.jsxDEV(s,{of:a},void 0,!1,{fileName:"<source.js>",lineNumber:5,columnNumber:1},this),`
+`,e.jsxDEV(i.h1,{id:"feed-item-pill",children:"Feed Item Pill"},void 0,!1,{fileName:"<source.js>",lineNumber:7,columnNumber:1},this),`
+`,e.jsxDEV(l,{children:e.jsxDEV(i.p,{children:"A generic layout for different Feed Items"},void 0,!1,{fileName:"<source.js>",lineNumber:10,columnNumber:3},this)},void 0,!1,{fileName:"<source.js>",lineNumber:9,columnNumber:1},this),`
+`,e.jsxDEV(i.h2,{id:"base-style",children:"Base Style"},void 0,!1,{fileName:"<source.js>",lineNumber:13,columnNumber:1},this),`
+`,e.jsxDEV(n,{children:e.jsxDEV(t,{of:p},void 0,!1,{fileName:"<source.js>",lineNumber:16,columnNumber:3},this)},void 0,!1,{fileName:"<source.js>",lineNumber:15,columnNumber:1},this),`
+`,e.jsxDEV(i.h2,{id:"variants",children:"Variants"},void 0,!1,{fileName:"<source.js>",lineNumber:19,columnNumber:1},this),`
+`,e.jsxDEV(i.p,{children:"Can provide a border style via borderColor"},void 0,!1,{fileName:"<source.js>",lineNumber:21,columnNumber:1},this),`
+`,e.jsxDEV(n,{children:e.jsxDEV(t,{of:d},void 0,!1,{fileName:"<source.js>",lineNumber:23,columnNumber:3},this)},void 0,!1,{fileName:"<source.js>",lineNumber:22,columnNumber:1},this),`
+`,e.jsxDEV(i.p,{children:"Can disable the toggleable behaviour (Cursor is reset, no changing of icon on hover, clicking does not toggle the expandable)"},void 0,!1,{fileName:"<source.js>",lineNumber:26,columnNumber:1},this),`
+`,e.jsxDEV(n,{children:e.jsxDEV(t,{of:c},void 0,!1,{fileName:"<source.js>",lineNumber:28,columnNumber:3},this)},void 0,!1,{fileName:"<source.js>",lineNumber:27,columnNumber:1},this),`
+`,e.jsxDEV(i.h2,{id:"slots-props--events",children:"Slots, Props & Events"},void 0,!1,{fileName:"<source.js>",lineNumber:31,columnNumber:1},this),`
+`,e.jsxDEV(u,{},void 0,!1,{fileName:"<source.js>",lineNumber:33,columnNumber:1},this),`
+`,e.jsxDEV(i.h2,{id:"usage",children:"Usage"},void 0,!1,{fileName:"<source.js>",lineNumber:35,columnNumber:1},this),`
+`,e.jsxDEV(i.h3,{id:"import",children:"Import"},void 0,!1,{fileName:"<source.js>",lineNumber:37,columnNumber:1},this),`
+`,e.jsxDEV(i.pre,{children:e.jsxDEV(i.code,{className:"language-jsx",children:`import { DtRecipeFeedItemPill } from '@dialpad/dialtone-vue';
+`},void 0,!1,{fileName:"<source.js>",lineNumber:39,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:39,columnNumber:1},this),`
+`,e.jsxDEV(i.h3,{id:"example",children:"Example"},void 0,!1,{fileName:"<source.js>",lineNumber:43,columnNumber:1},this),`
+`,e.jsxDEV(i.pre,{children:e.jsxDEV(i.code,{className:"language-vue",children:`  <dt-recipe-feed-item-pill
+    :icon-name="iconName"
+    :title="title"
+    :wrapper-class="wrapperClass"
+    :button-class="buttonClass"
+    :border-color="borderColor"
+    :aria-label="ariaLabel"
+    :toggleable="toggleable"
+  >
+    <template #subtitle>
+      Subtitle
+    </template>
+    <template #bottom>
+      <span class="d-fc-tertiary d-fs-100">Bottom</span>
+    </template>
+    <template #right>
+      <div class="d-pr16">
+        <dt-button
+          kind="muted"
+          importance="clear"
+          @click.stop=""
+        >
+          <template slot="icon">
+            <dt-icon
+              name="external-link"
+              size="200"
+            />
+          </template>
+        </dt-button>
+      </div>
+    </template>
+    <template #content>
+      <span class="d-p16">
+        Content
+      </span>
+    </template>
+  </dt-recipe-feed-item-pill>
+`},void 0,!1,{fileName:"<source.js>",lineNumber:45,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:45,columnNumber:1},this)]},void 0,!0,{fileName:"<source.js>",lineNumber:1,columnNumber:1},this)}function Qe(r={}){const{wrapper:i}=Object.assign({},o(),r.components);return i?e.jsxDEV(i,Object.assign({},r,{children:e.jsxDEV(m,r,void 0,!1,{fileName:"<source.js>"},this)}),void 0,!1,{fileName:"<source.js>"},this):m(r)}export{Qe as default};
+//# sourceMappingURL=feed_item_pill-03abb818.js.map
