9a94d3fe404689fd2cab85a21f792d699dbb5874
AlexLamDialpad <AlexLamDialpad@users.noreply.github.com>
2023-09-28 21:43:49 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@74acb76ee088a6dd88f1f4cd4cf05fc78c0455c0 🚀

---
+++ deploy-previews/pr-1224/assets/preview-713dcbdd.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-107a5deb.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-a0d273f3.js"),["./DocsRenderer-3PUGWF3O-a0d273f3.js","./chunk-S4VUQJ4A-e6d7c8c7.js","./iframe-107a5deb.js","./index-dd8541c2.js","./_commonjsHelpers-725317a4.js","./index-d37d4223.js","./index-f073aaf1.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-713dcbdd.js.map
