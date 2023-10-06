8d310cf9caf4ea3d452a524f1a8e1b9b35b76b54
Brad Paugh <brad.paugh@dialpad.com>
2023-10-06 12:14:07 -0700

    docs

--- components/root_layout/root_layout.vue
+++ components/root_layout/root_layout.vue
@@ -7,7 +7,7 @@
       :class="['d-root-layout__header', { 'd-root-layout__header--sticky': headerSticky }, headerClass]"
       data-qa="dt-root-layout-header"
     >
-      <!-- @slot Slot for header content be sure to set a height on the element inside this
+      <!-- @slot Slot for header content, be sure to set a height on the element inside this
         if you want a fixed height. -->
       <slot name="header" />
     </header>
@@ -21,7 +21,7 @@
         :class="['d-root-layout__sidebar', { 'd-root-layout__sidebar--sticky': fixed }, sidebarClass]"
         data-qa="dt-root-layout-sidebar"
       >
-        <!-- @slot Slot for the sidebar -->
+        <!-- @slot Slot for sidebar content, be sure to set a width on the element within this. -->
         <slot name="sidebar" />
       </aside>
       <main
@@ -38,7 +38,7 @@
       :class="['d-root-layout__footer', footerClass]"
       data-qa="dt-root-layout-footer"
     >
-      <!-- @slot Slot for footer content be sure to set a height on the element inside this
+      <!-- @slot Slot for footer content, be sure to set a height on the element inside this
         if you want a fixed height. -->
       <slot name="footer" />
     </footer>
