7638488ba901f344db86d53999d45c938e4f67a1
Brad Paugh <brad.paugh@dialpad.com>
2023-10-16 13:27:18 -0700

    fix wrapper display

--- components/radio/radio.vue
+++ components/radio/radio.vue
@@ -1,5 +1,5 @@
 <template>
-  <label>
+  <label class="dt-radio-wrapper">
     <div :class="['d-radio-group', { 'd-radio-group--disabled': internalDisabled }]">
       <div class="d-radio__input">
         <input
@@ -157,3 +157,11 @@ export default {
   },
 };
 </script>
+
+<style>
+/* our html structure between dialtone and dialtone-vue is vastly different for checkbox/radio right now.. setting a
+local style here until we can get this fixed up. */
+.dt-radio-wrapper {
+  display: block;
+}
+</style>
