6986919a57d2ee32fb352802b27cfca8cd4e34e1
braddialpad <braddialpad@users.noreply.github.com>
2023-10-16 18:43:52 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@2e56035ccc73b0973580697f1bbf3ab18b6f0d23 🚀

---
+++ deploy-previews/pr-1260/assets/preview-c1728d5c.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-14179d37.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-49a02c15.js"),["./DocsRenderer-3PUGWF3O-49a02c15.js","./chunk-S4VUQJ4A-45e2c484.js","./iframe-14179d37.js","./index-7c47571d.js","./_commonjsHelpers-725317a4.js","./index-d37d4223.js","./index-f073aaf1.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-c1728d5c.js.map
