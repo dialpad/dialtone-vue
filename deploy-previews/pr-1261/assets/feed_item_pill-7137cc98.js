fbb15c4860cc62963357178f796aa34dfaa69132
fabiangarga <fabiangarga@users.noreply.github.com>
2023-10-23 18:35:13 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@f7fa608fa68171c14837eaa1f2c88ee097fd19c6 🚀

---
+++ deploy-previews/pr-1261/assets/feed_item_pill-7137cc98.js
@@ -0,0 +1,53 @@
+import{j as e}from"./jsx-dev-runtime-a02b4d74.js";import{M as s,b as l,C as r,d as m,e as p}from"./index-9671df88.js";import{F as u,D as a,V as d}from"./feed_item_pill.stories-f7aed262.js";import{u as o}from"./index-126bd2ea.js";import"./iframe-f922c4e1.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./index-d37d4223.js";import"./index-cba466d5.js";import"./index-356e4a49.js";import"./storybook_utils-3f71f290.js";import"./utils-c7895956.js";import"./vue.esm-eff6e72e.js";import"./avatar-2a773abc.js";import"./presence-d4f38341.js";import"./_plugin-vue2_normalizer-2bbd088e.js";import"./icon-8e9c060c.js";import"./icon_constants-8bec38b7.js";import"./badge-cc137183.js";import"./banner-caa26849.js";import"./notice-569d3b77.js";import"./button-f9ebe2a3.js";import"./link_constants-8ea95ec9.js";import"./sr_only_close_button-f3f539d4.js";import"./modal-600bd46a.js";import"./breadcrumbs-4a8ffdff.js";import"./breadcrumb_item-30fa3087.js";import"./link-82bdd562.js";import"./button_group-875af4af.js";import"./card-5e4a62ed.js";import"./codeblock-6f598051.js";import"./combobox-febb10d3.js";import"./keyboard_list_navigation-8c116ee6.js";import"./list_item-29645faf.js";import"./item_layout-499a0a91.js";import"./skeleton-e82601aa.js";import"./collapsible-15bd8222.js";import"./lazy_show-0b3a0cf0.js";import"./dropdown_separator-64129a78.js";import"./popover-4e498420.js";import"./tippy_utils-e4c34e91.js";import"./index.esm-be2d025a.js";import"./dropdown_constants-d0cadac9.js";import"./image_viewer-3eaf40dc.js";import"./input-64dcfdfa.js";import"./validation_messages-0d9f2537.js";import"./input-7972dc04.js";import"./input_group-382f5d74.js";import"./modal-5e78ca86.js";import"./list_item_group-0c89eb8e.js";import"./pagination-e70887d2.js";import"./radio-60110e37.js";import"./radio_constants-4f6325c5.js";import"./radio_group-747fac1b.js";import"./tab_group-85f26400.js";import"./tab-740f17dc.js";import"./tab_panel-72292c29.js";import"./checkbox-0df75fc1.js";import"./checkbox_group-7f31a683.js";import"./chip-66cc3403.js";import"./select_menu-9c849631.js";import"./toast-3b7def67.js";import"./toggle-087cb428.js";import"./tooltip-9d43b8db.js";import"./keyboard_shortcut-3197fce8.js";import"./root_layout-a238945e.js";import"./stack-b8a925c0.js";import"./DtDatepicker-8eb96ee1.js";import"./combobox_with_popover-8386e852.js";import"./combobox_multi_select-a859b42e.js";import"./callbar_button-5edf719d.js";import"./callbar_button_with_popover-d8f95d0c.js";import"./contact_info-cc3f24af.js";import"./top_banner_info-371feffc.js";import"./ivr_node-eda38202.js";import"./grouped_chip-1192e8f6.js";import"./settings_menu_button-5cb8caba.js";import"./general_row-82edb133.js";import"./emoji_text_wrapper-d79ea48c.js";import"./emoji-5a514b4f.js";import"./emoji-e75a4e8c.js";import"./group_row-d941dca9.js";import"./contact_row-89e4238f.js";import"./unread_pill-6e3d8c8a.js";import"./feed_item_row-463582e1.js";import"./time_pill-a3dd4c1b.js";import"./emoji_row-e7c2b412.js";import"./callbox-d5fa6a23.js";function n(t){const i=Object.assign({h1:"h1",p:"p",h2:"h2",h3:"h3",pre:"pre",code:"code"},o(),t.components);return e.jsxDEV(e.Fragment,{children:[e.jsxDEV(s,{of:u},void 0,!1,{fileName:"<source.js>",lineNumber:5,columnNumber:1},this),`
+`,e.jsxDEV(i.h1,{id:"feed-item-pill",children:"Feed Item Pill"},void 0,!1,{fileName:"<source.js>",lineNumber:7,columnNumber:1},this),`
+`,e.jsxDEV(l,{children:e.jsxDEV(i.p,{children:"A generic layout for different Feed Items"},void 0,!1,{fileName:"<source.js>",lineNumber:10,columnNumber:3},this)},void 0,!1,{fileName:"<source.js>",lineNumber:9,columnNumber:1},this),`
+`,e.jsxDEV(i.h2,{id:"base-style",children:"Base Style"},void 0,!1,{fileName:"<source.js>",lineNumber:13,columnNumber:1},this),`
+`,e.jsxDEV(r,{children:e.jsxDEV(m,{of:a},void 0,!1,{fileName:"<source.js>",lineNumber:16,columnNumber:3},this)},void 0,!1,{fileName:"<source.js>",lineNumber:15,columnNumber:1},this),`
+`,e.jsxDEV(i.h2,{id:"variants",children:"Variants"},void 0,!1,{fileName:"<source.js>",lineNumber:19,columnNumber:1},this),`
+`,e.jsxDEV(r,{children:e.jsxDEV(m,{of:d},void 0,!1,{fileName:"<source.js>",lineNumber:22,columnNumber:3},this)},void 0,!1,{fileName:"<source.js>",lineNumber:21,columnNumber:1},this),`
+`,e.jsxDEV(i.h2,{id:"slots-props--events",children:"Slots, Props & Events"},void 0,!1,{fileName:"<source.js>",lineNumber:25,columnNumber:1},this),`
+`,e.jsxDEV(p,{},void 0,!1,{fileName:"<source.js>",lineNumber:27,columnNumber:1},this),`
+`,e.jsxDEV(i.h2,{id:"usage",children:"Usage"},void 0,!1,{fileName:"<source.js>",lineNumber:29,columnNumber:1},this),`
+`,e.jsxDEV(i.h3,{id:"import",children:"Import"},void 0,!1,{fileName:"<source.js>",lineNumber:31,columnNumber:1},this),`
+`,e.jsxDEV(i.pre,{children:e.jsxDEV(i.code,{className:"language-jsx",children:`import { DtRecipeFeedItemPill } from '@dialpad/dialtone-vue';
+`},void 0,!1,{fileName:"<source.js>",lineNumber:33,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:33,columnNumber:1},this),`
+`,e.jsxDEV(i.h3,{id:"example",children:"Example"},void 0,!1,{fileName:"<source.js>",lineNumber:37,columnNumber:1},this),`
+`,e.jsxDEV(i.pre,{children:e.jsxDEV(i.code,{className:"language-vue",children:`  <dt-recipe-feed-item-pill
+    :icon-name="iconName"
+    :title="title"
+    :wrapper-class="wrapperClass"
+    :button-class="buttonClass"
+    :border-color="borderColor"
+    :aria-label="ariaLabel"
+    :toggleable="toggleable"
+  >
+    <template #subtitle>
+      Subtitle
+    </template>
+    <template #bottom>
+      <span class="d-fc-tertiary d-fs-100">Bottom</span>
+    </template>
+    <template #right>
+      <div class="d-pr16">
+        <dt-button
+          kind="muted"
+          importance="clear"
+          @click.stop=""
+        >
+          <template slot="icon">
+            <dt-icon
+              name="external-link"
+              size="200"
+            />
+          </template>
+        </dt-button>
+      </div>
+    </template>
+    <template #content>
+      <span class="d-p16">
+        Content
+      </span>
+    </template>
+  </dt-recipe-feed-item-pill>
+`},void 0,!1,{fileName:"<source.js>",lineNumber:39,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:39,columnNumber:1},this)]},void 0,!0,{fileName:"<source.js>",lineNumber:1,columnNumber:1},this)}function Ke(t={}){const{wrapper:i}=Object.assign({},o(),t.components);return i?e.jsxDEV(i,Object.assign({},t,{children:e.jsxDEV(n,t,void 0,!1,{fileName:"<source.js>"},this)}),void 0,!1,{fileName:"<source.js>"},this):n(t)}export{Ke as default};
+//# sourceMappingURL=feed_item_pill-7137cc98.js.map
--- deploy-previews/pr-1261/assets/preview-47cb2075.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-95d117b8.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-5553d441.js"),["./DocsRenderer-3PUGWF3O-5553d441.js","./chunk-S4VUQJ4A-dea1c250.js","./iframe-95d117b8.js","./index-94f227b4.js","./_commonjsHelpers-725317a4.js","./index-d37d4223.js","./index-cba466d5.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-47cb2075.js.map
