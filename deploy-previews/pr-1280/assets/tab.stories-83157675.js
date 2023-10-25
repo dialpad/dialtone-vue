817ea1b91d07d20278befbf645e67640ebd14db6
sanjeevdialpad <sanjeevdialpad@users.noreply.github.com>
2023-10-25 17:40:35 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@95c9b8398a6ebd46179c29baaf1dd0ba7e3afb75 🚀

---
+++ deploy-previews/pr-1280/assets/tab.stories-83157675.js
@@ -0,0 +1,24 @@
+import{D as r}from"./tab-d4512b7b.js";import{a}from"./chunk-AY7I2SME-5eb1ab46.js";import"./button-a4e86900.js";import"./vue.esm-bundler-5f4c5b12.js";import"./utils-eec883fe.js";import"./link_constants-8ea95ec9.js";import"./_plugin-vue_export-helper-c27b6911.js";const p={id:"1",panelId:"1",selected:!0},c={},D={title:"Components/Tabs",component:r,args:p,argTypes:c,excludeStories:/.*Data$/,parameters:{a11y:{config:{rules:[{id:"aria-required-parent",enabled:!1}]}}}},e={render:(i,{argTypes:o})=>({components:{DtTab:r},props:Object.keys(o),provide:{setFocus:a("setFocus"),changeContentPanel:a("changeContentPanel"),groupContext:{selected:"",disabled:!1}},template:`<DtTab v-bind="$props">
+                Tab
+               </DtTab>`})};var t,n,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: (args, {
+    argTypes
+  }) => ({
+    components: {
+      DtTab
+    },
+    props: Object.keys(argTypes),
+    provide: {
+      setFocus: action('setFocus'),
+      changeContentPanel: action('changeContentPanel'),
+      groupContext: {
+        selected: '',
+        disabled: false
+      }
+    },
+    template: \`<DtTab v-bind="$props">
+                Tab
+               </DtTab>\`
+  })
+}`,...(s=(n=e.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const y=["argsData","argTypesData","Tab"];export{e as Tab,y as __namedExportsOrder,c as argTypesData,p as argsData,D as default};
+//# sourceMappingURL=tab.stories-83157675.js.map
