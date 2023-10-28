22fd20459752c5c81e04c90ba1b6611da54be1f5
braddialpad <braddialpad@users.noreply.github.com>
2023-10-28 00:05:08 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@70c84440392b2623599c7d381d43117f796c79fb 🚀

---
+++ deploy-previews/pr-1286/assets/WithTooltip-J57HCPYA-9ff25f8b.js
@@ -0,0 +1,2 @@
+import{W as T,W as e,a as h}from"./index-a6e7972a.js";import"./iframe-1bbec7b6.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./index-d37d4223.js";import"./index-f073aaf1.js";import"./index-356e4a49.js";export{T as WithToolTipState,e as WithTooltip,h as WithTooltipPure};
+//# sourceMappingURL=WithTooltip-J57HCPYA-9ff25f8b.js.map
---
+++ deploy-previews/pr-1286/assets/banner.stories-3d94c823.js
@@ -0,0 +1,64 @@
+import{D as G}from"./banner-b934255d.js";import{D as Y}from"./button-cf0b13d0.js";import{D as Z}from"./icon-c9b7700c.js";import{t as k,o as b,p as h,d as $,v as C,x as m,w as n,y as g,D as x,z as ee,e as B,k as I}from"./vue.esm-bundler-807df745.js";import{_ as re}from"./_plugin-vue_export-helper-c27b6911.js";import{c as ae}from"./storybook_utils-5e2f75ca.js";import{b as D,c as u}from"./notice.stories-e2eda4eb.js";const J={name:"BannerDefault",components:{DtBanner:G,DtButton:Y,DtIcon:Z},data(){return{displayBanner:this.$attrs.show}},computed:{shouldInvertButton(){return this.$attrs.kind==="base"||this.$attrs.kind==="error"||this.$attrs.kind==="info"},isInverted(){return this.$attrs.important&&this.shouldInvertButton},buttonKind(){return this.isInverted?"inverted":"muted"},linkClass(){return this.isInverted?"d-link--inverted":"d-link--muted"},buttonCloseProps(){return{...this.$attrs.closeButtonProps,kind:this.buttonKind,ariaLabel:"Close"}}}},ne=["innerHTML"],te={key:1},se=["innerHTML"];function oe(e,c,ce,pe,p,f){const y=k("dt-button"),Q=k("dt-icon"),X=k("dt-banner");return b(),h("div",null,[$(m(y,{onClick:c[0]||(c[0]=v=>p.displayBanner=!0)},{default:n(()=>[g(" Click to show! ")]),_:1},512),[[C,!p.displayBanner]]),$(m(X,{kind:e.$attrs.kind,title:e.$attrs.title,"title-id":e.$attrs.titleId,"content-id":e.$attrs.contentId,important:e.$attrs.important,pinned:e.$attrs.pinned,"hide-close":e.$attrs.hideClose,"close-button-props":f.buttonCloseProps,class:I({"d-ps-sticky":e.$attrs.show}),"hide-icon":e.$attrs.hideIcon,"background-image":e.$attrs.backgroundImage,"background-size":e.$attrs.backgroundSize,"dialog-class":e.$attrs.dialogClass,"visually-hidden-close":e.$attrs.visuallyHiddenClose,"visually-hidden-close-label":e.$attrs.visuallyHiddenCloseLabel,onClose:c[1]||(c[1]=v=>{p.displayBanner=!1,e.$attrs.onClose(v)})},x({default:n(()=>[e.defaultSlot?(b(),h("span",{key:0,innerHTML:e.defaultSlot},null,8,ne)):(b(),h("span",te,[g(" Message body with "),B("a",{href:"#",class:I(["d-link",f.linkClass])},"a link",2),g(". ")]))]),_:2},[e.$attrs.action?{name:"action",fn:n(()=>[m(y,{kind:f.buttonKind,importance:"outlined",onClick:e.$attrs.onClick},{default:n(()=>[g(ee(e.$attrs.action),1)]),_:1},8,["kind","onClick"])]),key:"0"}:void 0,e.$attrs.icon?{name:"icon",fn:n(()=>[m(Q,{name:e.$attrs.icon},null,8,["name"])]),key:"1"}:void 0,e.$attrs.titleOverride?{name:"titleOverride",fn:n(()=>[B("span",{innerHTML:e.$attrs.titleOverride},null,8,se)]),key:"2"}:void 0]),1032,["kind","title","title-id","content-id","important","pinned","hide-close","close-button-props","class","hide-icon","background-image","background-size","dialog-class","visually-hidden-close","visually-hidden-close-label"]),[[C,p.displayBanner]])])}const ie=re(J,[["render",oe],["__file","/home/runner/work/dialtone-vue/dialtone-vue/components/banner/banner_default.story.vue"]]);J.__docgenInfo={displayName:"BannerDefault",exportName:"default",description:"",tags:{},sourceFiles:["/home/runner/work/dialtone-vue/dialtone-vue/components/banner/banner_default.story.vue"]};const de=""+new URL("dialpad-gradient-fc68f20e.png",import.meta.url).href;u.role={table:{disable:!0}};u.titleId={table:{defaultValue:{summary:"generated unique ID"}}};u.contentId={table:{defaultValue:{summary:"generated unique ID"}}};D.visuallyHiddenCloseLabel="Close Banner";const le={title:"Components/Banner",component:G,args:D,argTypes:u,excludeStories:/.Data$/},a=e=>ae(e,ie),r={render:a,args:{title:"Optional title",action:"Action",kind:"base"},parameters:{percy:{args:{show:!0}}}},t={render:a,args:{...r.args,kind:"error"},parameters:r.parameters},s={render:a,args:{...r.args,kind:"info"},parameters:r.parameters},o={render:a,args:{...r.args,kind:"success"},parameters:r.parameters},i={render:a,args:{...r.args,kind:"warning"},parameters:r.parameters},d={render:a,args:{...r.args,pinned:!0},parameters:r.parameters},l={render:a,args:{...r.args,backgroundImage:de,backgroundSize:"contain",title:"",action:"",hideIcon:!0,dialogClass:"d-fc-neutral-white",default:"The power of Dialpad. On your desktop"},parameters:r.parameters};var S,T,w;r.parameters={...r.parameters,docs:{...(S=r.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    title: 'Optional title',
+    action: 'Action',
+    kind: 'base'
+  },
+  parameters: {
+    percy: {
+      args: {
+        show: true
+      }
+    }
+  }
+}`,...(w=(T=r.parameters)==null?void 0:T.docs)==null?void 0:w.source}}};var _,O,z;t.parameters={...t.parameters,docs:{...(_=t.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    ...Default.args,
+    kind: 'error'
+  },
+  parameters: Default.parameters
+}`,...(z=(O=t.parameters)==null?void 0:O.docs)==null?void 0:z.source}}};var L,H,P;s.parameters={...s.parameters,docs:{...(L=s.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    ...Default.args,
+    kind: 'info'
+  },
+  parameters: Default.parameters
+}`,...(P=(H=s.parameters)==null?void 0:H.docs)==null?void 0:P.source}}};var M,V,E;o.parameters={...o.parameters,docs:{...(M=o.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    ...Default.args,
+    kind: 'success'
+  },
+  parameters: Default.parameters
+}`,...(E=(V=o.parameters)==null?void 0:V.docs)==null?void 0:E.source}}};var N,F,K;i.parameters={...i.parameters,docs:{...(N=i.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    ...Default.args,
+    kind: 'warning'
+  },
+  parameters: Default.parameters
+}`,...(K=(F=i.parameters)==null?void 0:F.docs)==null?void 0:K.source}}};var W,j,q;d.parameters={...d.parameters,docs:{...(W=d.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    ...Default.args,
+    pinned: true
+  },
+  parameters: Default.parameters
+}`,...(q=(j=d.parameters)==null?void 0:j.docs)==null?void 0:q.source}}};var A,R,U;l.parameters={...l.parameters,docs:{...(A=l.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    ...Default.args,
+    backgroundImage,
+    backgroundSize: 'contain',
+    title: '',
+    action: '',
+    hideIcon: true,
+    dialogClass: 'd-fc-neutral-white',
+    default: 'The power of Dialpad. On your desktop'
+  },
+  parameters: Default.parameters
+}`,...(U=(R=l.parameters)==null?void 0:R.docs)==null?void 0:U.source}}};const ue=["argsData","argTypesData","Default","Error","Info","Success","Warning","Pinned","CustomBackground"],ye=Object.freeze(Object.defineProperty({__proto__:null,CustomBackground:l,Default:r,Error:t,Info:s,Pinned:d,Success:o,Warning:i,__namedExportsOrder:ue,argTypesData:u,argsData:D,default:le},Symbol.toStringTag,{value:"Module"}));export{ye as B,r as D,t as E,s as I,d as P,o as S,i as W};
+//# sourceMappingURL=banner.stories-3d94c823.js.map
