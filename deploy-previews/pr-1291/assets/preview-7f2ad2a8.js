1cb09f2305d7b712f79bd896922b890203c112d9
ninamarina <ninamarina@users.noreply.github.com>
2023-11-02 18:30:30 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@7b9837d3a3011f297d42a1db9842aaa7a9a48175 🚀

---
+++ deploy-previews/pr-1291/assets/preview-7f2ad2a8.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-7c3395a1.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-84c7b183.js"),["./DocsRenderer-3PUGWF3O-84c7b183.js","./chunk-S4VUQJ4A-c92ea0bf.js","./iframe-7c3395a1.js","./index-96c2e0f5.js","./_commonjsHelpers-725317a4.js","./index-d37d4223.js","./index-f073aaf1.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-7f2ad2a8.js.map
