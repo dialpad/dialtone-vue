7f1d3c0c690093985b8edec8b13481e8e2f8d88a
juliodialpad <juliodialpad@users.noreply.github.com>
2023-11-03 18:09:11 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@92d048a70dbfa2e6373fde0e0c8686d7a24a30b3 🚀

---
+++ deploy-previews/pr-1295/assets/feed_item_row-96b293a0.js
@@ -0,0 +1,53 @@
+import{j as e}from"./jsx-dev-runtime-9a668480.js";import{M as o,b as l,C as s,d as r,e as u}from"./index-6bec374c.js";import{F as a,D as d,V as c}from"./feed_item_row.stories-735edd7e.js";import{u as m}from"./index-85bf0bab.js";import"./iframe-2057b6e8.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./index-d37d4223.js";import"./index-f073aaf1.js";import"./index-356e4a49.js";import"./chunk-AY7I2SME-5eb1ab46.js";import"./storybook_utils-43ef8c35.js";import"./feed_item_row-9cbe94fc.js";import"./avatar-0038ee3e.js";import"./utils-a917dac3.js";import"./vue.esm-bundler-4299d4fa.js";import"./presence-e2ffcf2f.js";import"./_plugin-vue_export-helper-c27b6911.js";import"./icon-d97b2f98.js";import"./icon_constants-e5cbd7a1.js";import"./lazy_show-37284601.js";import"./list_item-e21d9a07.js";import"./item_layout-16ef4dd3.js";import"./emoji_row-ced9824c.js";import"./button-193f2ca3.js";import"./link_constants-8ea95ec9.js";import"./tooltip-0833e2ff.js";import"./tippy_utils-89aa4eb5.js";import"./emoji-9cfd9564.js";import"./emoji-87314816.js";import"./index-40c4ee4c.js";import"./skeleton-64d050fa.js";import"./emoji_text_wrapper-46231b2f.js";import"./stack-abb58067.js";import"./stack_constants-4f71ebff.js";import"./image_viewer-27bccccf.js";import"./modal-f96ddab9.js";import"./fry-269daf43.js";function t(i){const n=Object.assign({h1:"h1",p:"p",h2:"h2",ul:"ul",li:"li",strong:"strong",h3:"h3",pre:"pre",code:"code"},m(),i.components);return e.jsxDEV(e.Fragment,{children:[e.jsxDEV(o,{of:a},void 0,!1,{fileName:"<source.js>",lineNumber:5,columnNumber:1},this),`
+`,e.jsxDEV(n.h1,{id:"feed-item-row",children:"Feed Item Row"},void 0,!1,{fileName:"<source.js>",lineNumber:7,columnNumber:1},this),`
+`,e.jsxDEV(l,{children:e.jsxDEV(n.p,{children:"Feed Item Row Recipe represent individual feed row in a feed list."},void 0,!1,{fileName:"<source.js>",lineNumber:10,columnNumber:3},this)},void 0,!1,{fileName:"<source.js>",lineNumber:9,columnNumber:1},this),`
+`,e.jsxDEV(n.h2,{id:"base-style",children:"Base Style"},void 0,!1,{fileName:"<source.js>",lineNumber:13,columnNumber:1},this),`
+`,e.jsxDEV(n.p,{children:`A feed item row provides slots for avatar, action menu, threads and reactions as well as header (display name & time)
+and the content which can be either a message or a rich media.`},void 0,!1,{fileName:"<source.js>",lineNumber:15,columnNumber:1},this),`
+`,e.jsxDEV(n.p,{children:"The default feed item row contains 4 slots:"},void 0,!1,{fileName:"<source.js>",lineNumber:18,columnNumber:1},this),`
+`,e.jsxDEV(n.ul,{children:[`
+`,e.jsxDEV(n.li,{children:[e.jsxDEV(n.strong,{children:"default"},void 0,!1,{fileName:"<source.js>",lineNumber:20,columnNumber:3},this)," slot - the content whether it be text or the rich media card"]},void 0,!0,{fileName:"<source.js>",lineNumber:20,columnNumber:1},this),`
+`,e.jsxDEV(n.li,{children:[e.jsxDEV(n.strong,{children:"threading"},void 0,!1,{fileName:"<source.js>",lineNumber:21,columnNumber:3},this)," slot - the threading row component which is below the reactions slot"]},void 0,!0,{fileName:"<source.js>",lineNumber:21,columnNumber:1},this),`
+`,e.jsxDEV(n.li,{children:[e.jsxDEV(n.strong,{children:"menu"},void 0,!1,{fileName:"<source.js>",lineNumber:22,columnNumber:3},this)," slot - the actions menu for current feed row. Floats towards the right and shows when isActive is true."]},void 0,!0,{fileName:"<source.js>",lineNumber:22,columnNumber:1},this),`
+`,e.jsxDEV(n.li,{children:[e.jsxDEV(n.strong,{children:"reactions"},void 0,!1,{fileName:"<source.js>",lineNumber:23,columnNumber:3},this)," slot - the emoji reactions list component below the content slot."]},void 0,!0,{fileName:"<source.js>",lineNumber:23,columnNumber:1},this),`
+`]},void 0,!0,{fileName:"<source.js>",lineNumber:20,columnNumber:1},this),`
+`,e.jsxDEV(s,{children:e.jsxDEV(r,{of:d},void 0,!1,{fileName:"<source.js>",lineNumber:26,columnNumber:3},this)},void 0,!1,{fileName:"<source.js>",lineNumber:25,columnNumber:1},this),`
+`,e.jsxDEV(n.h2,{id:"variants",children:"Variants"},void 0,!1,{fileName:"<source.js>",lineNumber:29,columnNumber:1},this),`
+`,e.jsxDEV(s,{children:e.jsxDEV(r,{of:c},void 0,!1,{fileName:"<source.js>",lineNumber:32,columnNumber:3},this)},void 0,!1,{fileName:"<source.js>",lineNumber:31,columnNumber:1},this),`
+`,`
+`,e.jsxDEV(n.h2,{id:"slots-props--events",children:"Slots, Props & Events"},void 0,!1,{fileName:"<source.js>",lineNumber:37,columnNumber:1},this),`
+`,e.jsxDEV(u,{},void 0,!1,{fileName:"<source.js>",lineNumber:39,columnNumber:1},this),`
+`,e.jsxDEV(n.h2,{id:"usage",children:"Usage"},void 0,!1,{fileName:"<source.js>",lineNumber:41,columnNumber:1},this),`
+`,e.jsxDEV(n.h3,{id:"import",children:"Import"},void 0,!1,{fileName:"<source.js>",lineNumber:43,columnNumber:1},this),`
+`,e.jsxDEV(n.pre,{children:e.jsxDEV(n.code,{className:"language-jsx",children:`import { DtRecipeFeedItemRow } from '@dialpad/dialtone-vue';
+`},void 0,!1,{fileName:"<source.js>",lineNumber:45,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:45,columnNumber:1},this),`
+`,e.jsxDEV(n.h3,{id:"default-feed-item-row",children:"Default Feed Item row"},void 0,!1,{fileName:"<source.js>",lineNumber:49,columnNumber:1},this),`
+`,e.jsxDEV(n.pre,{children:e.jsxDEV(n.code,{className:"language-vue",children:`<dt-recipe-feed-item-row
+  :show-header="showHeader"
+  :avatar-image-url="avatarImageUrl"
+  :display-name="displayName"
+  :time="time"
+  :is-active="isActive"
+  :state="state"
+  @on-hover="onHover"
+  @on-focus="onFocus"
+>
+  <!-- default content / Rich media -->
+  <template
+    #threading
+  >
+    <!-- threads component -->
+  </template>
+  <template
+    #reactions
+  >
+    <!-- reactions component -->
+  </template>
+  <template
+    #menu
+  >
+    <!-- Menu component which appears when isActive is true -->
+  </template>
+</dt-recipe-feed-item-row>
+`},void 0,!1,{fileName:"<source.js>",lineNumber:51,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:51,columnNumber:1},this)]},void 0,!0,{fileName:"<source.js>",lineNumber:1,columnNumber:1},this)}function Y(i={}){const{wrapper:n}=Object.assign({},m(),i.components);return n?e.jsxDEV(n,Object.assign({},i,{children:e.jsxDEV(t,i,void 0,!1,{fileName:"<source.js>"},this)}),void 0,!1,{fileName:"<source.js>"},this):t(i)}export{Y as default};
+//# sourceMappingURL=feed_item_row-96b293a0.js.map
