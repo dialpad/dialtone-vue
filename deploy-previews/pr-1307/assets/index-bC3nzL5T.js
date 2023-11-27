c87a1f1f1af9f396ac477e94dac3bcb72150dabd
iropolo <iropolo@users.noreply.github.com>
2023-11-27 16:41:22 +0000

    Deploying to gh-pages from @ dialpad/dialtone-vue@61bc1892078437d83ec535bb0b9bac1447a16a53 🚀

---
+++ deploy-previews/pr-1307/assets/index-bC3nzL5T.js
@@ -0,0 +1,2 @@
+import{M as n,a as M,u as D,w as X}from"./index-FcYvG9Ar.js";import"./index-tYW7vkaV.js";import"./iframe-ZrEVmpC3.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-5-cIlDoe.js";import"./index-aKxH7ZY0.js";import"./index-1QZrJT68.js";import"./index-PPLHz8o0.js";export{n as MDXContext,M as MDXProvider,D as useMDXComponents,X as withMDXComponents};
+//# sourceMappingURL=index-bC3nzL5T.js.map
---
+++ deploy-previews/pr-1307/assets/item_layout-I5HOrKBz.js
@@ -0,0 +1,13 @@
+import{j as e}from"./jsx-dev-runtime-czqLhJ0S.js";import{M as r,b as m,C as t,e as o}from"./index-tYW7vkaV.js";import{D as l,a}from"./item_layout.stories-J-ZklZN8.js";import{u}from"./index-FcYvG9Ar.js";import"./iframe-ZrEVmpC3.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-5-cIlDoe.js";import"./index-aKxH7ZY0.js";import"./index-1QZrJT68.js";import"./index-PPLHz8o0.js";import"./storybook_utils-XrTpeJKJ.js";import"./item_layout-wI78zaM5.js";import"./_plugin-vue2_normalizer-XBmYDgEh.js";function i(n){const s=Object.assign({h1:"h1",p:"p",code:"code",h2:"h2",h3:"h3",pre:"pre"},u(),n.components);return e.jsxDEV(e.Fragment,{children:[e.jsxDEV(r,{of:l},void 0,!1,{fileName:"<source.js>",lineNumber:4,columnNumber:1},this),`
+`,e.jsxDEV(s.h1,{id:"item-layout",children:"Item Layout"},void 0,!1,{fileName:"<source.js>",lineNumber:6,columnNumber:1},this),`
+`,e.jsxDEV(m,{children:e.jsxDEV(s.p,{children:[`Custom layout to enable developer to use list-item like stack.
+It is used as base for `,e.jsxDEV(s.code,{children:"dt-list-item"},void 0,!1,{fileName:"<source.js>",lineNumber:10,columnNumber:26},this)," component"]},void 0,!0,{fileName:"<source.js>",lineNumber:9,columnNumber:3},this)},void 0,!1,{fileName:"<source.js>",lineNumber:8,columnNumber:1},this),`
+`,e.jsxDEV(s.h2,{id:"base-style",children:"Base Style"},void 0,!1,{fileName:"<source.js>",lineNumber:13,columnNumber:1},this),`
+`,e.jsxDEV(t,{of:a},void 0,!1,{fileName:"<source.js>",lineNumber:15,columnNumber:1},this),`
+`,e.jsxDEV(s.h2,{id:"slots-props--events",children:"Slots, Props & Events"},void 0,!1,{fileName:"<source.js>",lineNumber:17,columnNumber:1},this),`
+`,e.jsxDEV(o,{},void 0,!1,{fileName:"<source.js>",lineNumber:19,columnNumber:1},this),`
+`,e.jsxDEV(s.h2,{id:"usage",children:"Usage"},void 0,!1,{fileName:"<source.js>",lineNumber:21,columnNumber:1},this),`
+`,e.jsxDEV(s.h3,{id:"import",children:"Import"},void 0,!1,{fileName:"<source.js>",lineNumber:23,columnNumber:1},this),`
+`,e.jsxDEV(s.pre,{children:e.jsxDEV(s.code,{className:"language-jsx",children:`import { DtItemLayout } from '@dialpad/dialtone-vue';
+`},void 0,!1,{fileName:"<source.js>",lineNumber:25,columnNumber:1},this)},void 0,!1,{fileName:"<source.js>",lineNumber:25,columnNumber:1},this)]},void 0,!0,{fileName:"<source.js>",lineNumber:1,columnNumber:1},this)}function y(n={}){const{wrapper:s}=Object.assign({},u(),n.components);return s?e.jsxDEV(s,Object.assign({},n,{children:e.jsxDEV(i,n,void 0,!1,{fileName:"<source.js>"},this)}),void 0,!1,{fileName:"<source.js>"},this):i(n)}export{y as default};
+//# sourceMappingURL=item_layout-I5HOrKBz.js.map
